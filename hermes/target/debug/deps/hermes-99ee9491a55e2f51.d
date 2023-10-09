23c8d3db9264e247f1d8374f699f0ac53fa354c3
Oleksandr Prokhorenko <djminikin@gmail.com>
2023-10-09 15:32:22 +0200

    chore: delete hermes/target directory

--- hermes/target/debug/deps/hermes-99ee9491a55e2f51.d
+++
@@ -1,5 +0,0 @@
-/Users/minikin/IOG/Code/hermes/hermes/target/debug/deps/hermes-99ee9491a55e2f51.rmeta: src/main.rs
-
-/Users/minikin/IOG/Code/hermes/hermes/target/debug/deps/hermes-99ee9491a55e2f51.d: src/main.rs
-
-src/main.rs:
--- hermes/target/debug/deps/hermes-bece2cffe57a61bd.d
+++
@@ -1,5 +0,0 @@
-/Users/minikin/IOG/Code/hermes/hermes/target/debug/deps/hermes-bece2cffe57a61bd.rmeta: src/main.rs
-
-/Users/minikin/IOG/Code/hermes/hermes/target/debug/deps/hermes-bece2cffe57a61bd.d: src/main.rs
-
-src/main.rs:
