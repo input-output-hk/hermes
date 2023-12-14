ac1777e5cc24167bef00427bbbfd6515c8e21e34
Apisit Ritreungroj <apisit_lon@hotmail.com>
2023-12-15 00:04:46 +0700

    fix: lints

--- hermes/crates/wasi-component-adapter/src/macros.rs
+++ hermes/crates/wasi-component-adapter/src/macros.rs
@@ -1,3 +1,5 @@
+#![allow(let_underscore_drop)]
+
 //! Minimal versions of standard-library panicking and printing macros.
 //!
 //! We're avoiding static initializers, so we can't have things like string
