45dfa8f57cd372fa76b200284e43205bbe800d3f
Apisit Ritreungroj <apisit_lon@hotmail.com>
2024-01-15 16:53:22 +0700

    feat: error display for testing multiple files

--- hermes/crates/cbork/cddl-parser/tests/cddl.rs
+++ hermes/crates/cbork/cddl-parser/tests/cddl.rs
@@ -6,6 +6,8 @@ use cddl_parser::{parse_cddl, Extension};
 fn parse_cddl_files() -> Result<()> {
   let entries = fs::read_dir("tests/cddl")?;
 
+  let mut error_results = vec![];
+
   for entry in entries {
     let file_path = entry?.path();
 
@@ -15,8 +17,14 @@ fn parse_cddl_files() -> Result<()> {
 
     let mut content = fs::read_to_string(&file_path)?;
 
-    parse_cddl(&mut content, &Extension::RFC8610Parser)
-      .unwrap_or_else(|err| panic!("{file_path:?} {err}"));
+    if let Err(e) = parse_cddl(&mut content, &Extension::RFC8610Parser) {
+      error_results.push(format!("{}) {file_path:?} {e}", error_results.len() + 1));
+    }
+  }
+
+  let err_msg = error_results.join("\n\n");
+  if !err_msg.is_empty() {
+    panic!("{err_msg}")
   }
 
   Ok(())
