a3410f6ad566f15b65ab83cf2a2e61a2ec341cfb
Apisit Ritreungroj <apisit_lon@hotmail.com>
2024-01-22 20:15:02 +0700

    feat: type1 test cases

--- hermes/crates/cbork/cddl-parser/tests/type_declarations.rs
+++ hermes/crates/cbork/cddl-parser/tests/type_declarations.rs
@@ -49,19 +49,60 @@ pub const RANGEOP_FAILS: &[&str] = &[
 ];
 
 pub const TYPE2_PASSES: &[&str] = &[
-
+    "#",
+    "#1",
+    "#1.1",
+    "#1.1",
+    "#6",
+    "#6.11",
+    "#6.11(tstr)",
+    "#6.11(\tstr\n)",
+    "#6.11({ foo })",
+    "#6.11([ foo ])",
+    "#6.11(#3.1)",
+    "&foo",
+    "& foo<bar>",
+    "&((+ a) // (b / c))",
+    "&\t( foo: bar )",
+    "~foo",
+    "~ foo<bar>",
+    "foo<bar>",
+    "[ foo bar ]",
+    "{ foo bar }",
+    "(a)",
+    "(a / b)",
+    "(#)",
+    "((a))",
+    "1",
+    "h'1111'",
+    "true",
+    "foo",
 ];
 
 pub const TYPE2_FAILS: &[&str] = &[
-
+    "",
+    "##",
+    "#1.",
+    "#6.11 (tstr)",
+    "#6.11(( foo: uint ))",
+    "&",
+    "& foo <bar>",
+    "(foo bar)",
 ];
 
 pub const TYPE1_PASSES: &[&str] = &[
-
+    "1..2",
+    "1...2",
+    "0..10.0", // BAD range 1
+    "0.0..10", // BAD range 2
+    "0..max-byte",
+    "1.0..2.0",
+    "1.0...2.0",
+    "foo.bar",
 ];
 
 pub const TYPE1_FAILS: &[&str] = &[
-
+    ""
 ];
 
 pub const TYPE_PASSES: &[&str] = &[
@@ -141,6 +182,27 @@ fn check_type1() {
     for test in fails {
         let parse = CDDLTestParser::parse(Rule::type1_TEST, test);
         assert!(parse.is_err());
+    }
+}
+
+#[test]
+/// Test if the `type1` rule passes properly based on composition of type2 test cases.
+fn check_type1_composition() {
+    // type2 composition testing
+    for (i, test_i) in [TYPE2_PASSES, TYPE_FAILS].into_iter().flatten().enumerate() {
+        for (j, test_j) in [CTLOP_PASSES, RANGEOP_PASSES].into_iter().flatten().enumerate() {
+            for (k, test_k) in [TYPE2_PASSES, TYPE_FAILS].into_iter().flatten().enumerate() {
+                let input = [test_i.to_owned(), test_j.to_owned(), test_k.to_owned()].join(" ");
+                let parse = CDDLTestParser::parse(Rule::type1_TEST, &input);
+                if (0..TYPE2_PASSES.len()).contains(&i)
+                && (0..(CTLOP_PASSES.len() + RANGEOP_PASSES.len())).contains(&j)
+                && (0..TYPE2_PASSES.len()).contains(&k) {
+                    assert!(parse.is_ok());
+                } else {
+                    assert!(parse.is_err());
+                }
+            }
+        }
     }
 }
 
