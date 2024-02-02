61045cb92b262056128c472f33a3bf37a9bd7f70
cong-or <conchubhar.gannon@gmail.com>
2024-02-02 20:48:05 +0000

    clippy

--- hermes/crates/cardano-chain-follower/src/lib.rs
+++ hermes/crates/cardano-chain-follower/src/lib.rs
@@ -119,7 +119,8 @@ impl From<Network> for u64 {
 }
 
 /// Return genesis values for given network
-pub fn network_genesis_values(network: Network) -> Option<GenesisValues> {
+#[must_use]
+pub fn network_genesis_values(network: &Network) -> Option<GenesisValues> {
     match network {
         Network::Mainnet => GenesisValues::from_magic(MAINNET_MAGIC),
         Network::Preprod => GenesisValues::from_magic(PRE_PRODUCTION_MAGIC),
