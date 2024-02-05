02cd06aefad7d6812a7f587a4f4d4c7ac7231dc2
Apisit Ritreungroj <apisit_lon@hotmail.com>
2024-02-05 17:52:27 +0700

    feat: test modules

---
+++ hermes/crates/cbork/abnf-parser/tests/testsuite/character_sets.rs
@@ -0,0 +1,144 @@
+use abnf_parser::{
+    self,
+    abnf_test::Rule,
+};
+
+use crate::common::*;
+
+#[test]
+/// Test if the `WHITESPACE` rule passes properly.
+fn check_whitespace() {
+    let passes = vec![" ", "\t"];
+    let fails = vec!["not", "\r", "\n", "\r\n"];
+
+    check_tests_rule(Rule::WHITESPACE, &passes, &fails)
+}
+
+/* #[test]
+/// Test if the `PCHAR` rule passes properly.
+fn check_pchar() {
+    for x in ('\u{0}'..='\u{ff}').map(char::from) {
+        let test = format!("{x}");
+        let parse = ABNFTestParser::parse(Rule::PCHAR, &test);
+        if x < ' ' || x == '\u{7f}' {
+            assert!(parse.is_err());
+        } else {
+            assert!(parse.is_ok());
+        }
+    }
+
+    let parse = ABNFTestParser::parse(Rule::ASCII_VISIBLE, "\r");
+    assert!(parse.is_err());
+}
+
+#[test]
+/// Test if the `BCHAR` rule passes properly.
+fn check_bchar() {
+    for x in ('\u{0}'..='\u{ff}').map(char::from) {
+        let test = format!("{x}");
+        let parse = ABNFTestParser::parse(Rule::BCHAR, &test);
+        if !matches!(x, '\n' | '\r') && x < ' ' || matches!(x, '\t' | '\'' | '\\' | '\u{7f}') {
+            assert!(parse.is_err());
+        } else {
+            assert!(parse.is_ok());
+        }
+    }
+
+    let parse = ABNFTestParser::parse(Rule::ASCII_VISIBLE, "\r");
+    assert!(parse.is_err());
+}
+
+#[test]
+/// Test if the `SESC` rule passes properly.
+fn check_sesc() {
+    for x in (' '..='\u{ff}').map(char::from) {
+        let test = format!("\\{x}");
+        let parse = ABNFTestParser::parse(Rule::SESC, &test);
+        if x == '\u{7f}' {
+            assert!(parse.is_err());
+        } else {
+            assert!(parse.is_ok());
+        }
+    }
+
+    let parse = ABNFTestParser::parse(Rule::ASCII_VISIBLE, "\r");
+    assert!(parse.is_err());
+}
+
+#[test]
+/// Test if the `ASCII_VISIBLE` rule passes properly.
+fn check_ascii_visible() {
+    for x in (b' '..=b'~').map(char::from) {
+        let test = x.to_string();
+        let parse = ABNFTestParser::parse(Rule::ASCII_VISIBLE, &test);
+        assert!(parse.is_ok());
+    }
+
+    let parse = ABNFTestParser::parse(Rule::ASCII_VISIBLE, "\r");
+    assert!(parse.is_err());
+
+    let parse = ABNFTestParser::parse(Rule::ASCII_VISIBLE, "\u{80}");
+    assert!(parse.is_err());
+}
+
+#[test]
+/// Test if the `SCHAR_ASCII_VISIBLE` rule passes properly.
+fn check_schar_ascii_visible() {
+    let invalids = "\"\\";
+    for x in (b' '..=b'~').map(char::from) {
+        let test = x.to_string();
+        let parse = ABNFTestParser::parse(Rule::SCHAR_ASCII_VISIBLE, &test);
+        if invalids.contains(x) {
+            assert!(parse.is_err());
+        } else {
+            assert!(parse.is_ok());
+        }
+    }
+
+    let parse = ABNFTestParser::parse(Rule::SCHAR_ASCII_VISIBLE, "\r");
+    assert!(parse.is_err());
+
+    let parse = ABNFTestParser::parse(Rule::SCHAR_ASCII_VISIBLE, "\u{80}");
+    assert!(parse.is_err());
+}
+
+#[test]
+/// Test if the `BCHAR_ASCII_VISIBLE` rule passes properly.
+fn check_bchar_ascii_visible() {
+    let invalids = "'\\";
+    for x in (b' '..=b'~').map(char::from) {
+        let test = x.to_string();
+        let parse = ABNFTestParser::parse(Rule::BCHAR_ASCII_VISIBLE, &test);
+        if invalids.contains(x) {
+            assert!(parse.is_err());
+        } else {
+            assert!(parse.is_ok());
+        }
+    }
+
+    let parse = ABNFTestParser::parse(Rule::BCHAR_ASCII_VISIBLE, "\r");
+    assert!(parse.is_err());
+
+    let parse = ABNFTestParser::parse(Rule::BCHAR_ASCII_VISIBLE, "\u{80}");
+    assert!(parse.is_err());
+}
+
+#[test]
+/// Test if the `UNICODE_CHAR` rule passes properly.
+fn check_unicode() {
+    let parse = ABNFTestParser::parse(Rule::UNICODE_CHAR, "\r");
+    assert!(parse.is_err());
+
+    let parse = ABNFTestParser::parse(Rule::UNICODE_CHAR, "\u{80}");
+    assert!(parse.is_ok());
+
+    let parse = ABNFTestParser::parse(Rule::UNICODE_CHAR, "\u{10fffd}");
+    assert!(parse.is_ok());
+
+    let parse = ABNFTestParser::parse(Rule::UNICODE_CHAR, "\u{7ffff}");
+    assert!(parse.is_ok());
+
+    let parse = ABNFTestParser::parse(Rule::UNICODE_CHAR, "\u{10fffe}");
+    assert!(parse.is_err());
+}
+ */
---
+++ hermes/crates/cbork/abnf-parser/tests/testsuite/main.rs
@@ -0,0 +1,3 @@
+mod common;
+mod abnf;
+mod character_sets;
