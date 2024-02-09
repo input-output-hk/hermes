4b0dd14ac974899e815e27bbb4a3b01a979b0491
bkioshn <bkioshn@gmail.com>
2024-02-09 16:25:51 +0700

    fix: remove unwrap and add test

--- hermes/bin/src/runtime/host/hermes/hash/mod.rs
+++ hermes/bin/src/runtime/host/hermes/hash/mod.rs
@@ -1,10 +1,7 @@
 //! Host - Hash implementations
 #![allow(unused_variables)]
 
-// use blake2::digest::{consts::{U32}, FixedOutput, Update, VariableOutput};
-// use blake2::{Blake2bVar};
 use blake2_rfc::blake2b::Blake2b;
-use hex_literal::hex;
 
 use crate::runtime::extensions::{
     hermes::{
@@ -24,34 +21,19 @@ impl Stateful for State {
 }
 
 fn blake2b_impl(buf: Bstr, outlen: Option<u8>, key: Option<Bstr>) -> Result<Bstr, Errno> {
-    let outlen = outlen.unwrap_or(64) as usize;
-    // let mut output = vec![0u8; outlen];
+    let outlen = match outlen {
+        Some(len) => len as usize,
+        None => 64, // Default output length is 64 bytes, blake2b-512
+    };
 
     let key = match key {
         Some(k) => k,
         None => (&[]).to_vec(),
     };
 
-    let mut ctx = Blake2b::with_key(outlen, &key);
-    ctx.update(&buf);
-    return Ok(ctx.finalize().as_bytes().to_vec());
-
-    // Default to 64 bytes Blake2b-512
-    // let outlen = outlen.unwrap_or(64) as usize;
-    // let mut output = vec![0u8; outlen];
-    // Key is specified, use MAC
-    // if let Some(k) = key {
-    // let mut hasher = Blake2bMac::<U32>::new_with_salt_and_personal(&k, &[], &[]).unwrap();
-    // hasher.update(&buf);
-    // output = hasher.finalize_fixed().to_vec();
-    //     let ctx = Blake2::with_key(outlen, key);
-    //     ctx.update(buf);
-    //     let output = ctx.finalize();
-    // } else {
-    //     let mut hasher = Blake2bVar::new(outlen).unwrap();
-    //     hasher.update(&buf);
-    //     hasher.finalize_variable(&mut output).unwrap();
-    // };
+    let mut hasher = Blake2b::with_key(outlen, &key);
+    hasher.update(&buf);
+    Ok(hasher.finalize().as_bytes().to_vec())
 }
 
 impl Host for HermesState {
@@ -66,7 +48,7 @@ impl Host for HermesState {
     fn blake2b(
         &mut self, buf: Bstr, outlen: Option<u8>, key: Option<Bstr>,
     ) -> wasmtime::Result<Result<Bstr, Errno>> {
-        todo!()
+        Ok(blake2b_impl(buf, outlen, key))
     }
 
     /// Hash a binary buffer with BLAKE3
@@ -77,63 +59,77 @@ impl Host for HermesState {
     }
 }
 
-#[test]
-fn test_blake2b() {
-    // Setup
-    let context = crate::wasm::context::Context::new("Test".to_string());
-    let state = crate::runtime::host::hermes::State::new(&context);
-    let mut hermes_state = HermesState::new(&context);
+#[cfg(test)]
+mod tests_blake2b {
+    use super::*;
+    use hex_literal::hex;
 
-    let buf = Bstr::from("test test");
-    let key = Bstr::from("key");
-    let outlen = Some(64);
+    #[test]
+    fn blake2b_512_with_key() {
+        // Setup
+        let buf = Bstr::from("test test");
+        let key = Bstr::from("key");
+        let outlen = Some(64);
 
-    let result = hermes_state.blake2b(buf, outlen, Some(key));
+        let result =
+            blake2b_impl(buf, outlen, Some(key)).expect("Failed to hash blake2b-512 with key");
 
-    // Assert the result
-    match result {
-        Ok(Ok(result_buf)) => {
-            // Hash without key, should work properly
-            assert_ne!(
-                result_buf.as_ref(),
-                hex!("c28029cbab4e11d759e971d7e2a13dbe9ef60d2fa539cc03138b0432c3fdb2757b6c87383bd1074f5533c0c2ad2a5d2ac71bbd96f0f8fbb4c3ba0d4abb309115")
-            );
-        },
-        Ok(Err(errno)) => {
-            panic!("Error returned: {:?}", errno);
-        },
-        Err(err) => {
-            panic!("Function failed with error: {:?}", err);
-        },
+        assert_eq!(
+            result.as_ref(),
+            hex!("c28029cbab4e11d759e971d7e2a13dbe9ef60d2fa539cc03138b0432c3fdb2757b6c87383bd1074f5533c0c2ad2a5d2ac71bbd96f0f8fbb4c3ba0d4abb309115")
+        );
     }
-}
-#[test]
-fn test_blake2b_ja() {
-    // Setup
-    let context = crate::wasm::context::Context::new("Test".to_string());
-    let state = crate::runtime::host::hermes::State::new(&context);
-    let mut hermes_state = HermesState::new(&context);
 
-    let buf = Bstr::from("test test");
-    let key = Bstr::from("key");
-    let outlen = Some(64);
+    #[test]
+    fn blake2b_512_with_no_key() {
+        // Setup
+        let buf = Bstr::from("test test");
+        let outlen = Some(64);
 
-    let result = hermes_state.blake2b(buf, outlen, None);
+        let result =
+            blake2b_impl(buf, outlen, None).expect("Failed to hash blake2b-512 without key");
 
-    // Assert the result
-    match result {
-        Ok(Ok(result_buf)) => {
-            // Hash without key, should work properly
-            assert_eq!(
-                result_buf.as_ref(),
-                hex!("8e27b2481dd1fe73d598104c03b1f67da60725abb73cf66e400177d73aee01e74b93f55adda27b0ad92e22e284b5e0cc95ad81b04b496bd58c4ae6bca5f56196")
-            );
-        },
-        Ok(Err(errno)) => {
-            panic!("Error returned: {:?}", errno);
-        },
-        Err(err) => {
-            panic!("Function failed with error: {:?}", err);
-        },
+        assert_eq!(
+            result.as_ref(),
+            hex!("8e27b2481dd1fe73d598104c03b1f67da60725abb73cf66e400177d73aee01e74b93f55adda27b0ad92e22e284b5e0cc95ad81b04b496bd58c4ae6bca5f56196")
+        );
+    }
+
+    #[test]
+    fn blake2b_256_with_no_key() {
+        // Setup
+        let buf = Bstr::from("test test");
+        let outlen = Some(32);
+
+        let result =
+            blake2b_impl(buf, outlen, None).expect("Failed to hash blake2b-256 without key");
+
+        assert_eq!(
+            result.as_ref(),
+            hex!("7f3dc1170e7017a1643d84d102429c4c7aec4ca99c016c32af18af997fed51f1")
+        );
+    }
+    #[test]
+    fn blake2b_512_with_default_outlen_no_key() {
+        // Setup
+        let buf = Bstr::from("test test");
+
+        let result = blake2b_impl(buf, None, None)
+            .expect("Failed to hash blake2b-512 default outlen without key");
+
+        assert_eq!(
+            result.as_ref(),
+            hex!("8e27b2481dd1fe73d598104c03b1f67da60725abb73cf66e400177d73aee01e74b93f55adda27b0ad92e22e284b5e0cc95ad81b04b496bd58c4ae6bca5f56196")
+        );
+    }
+
+    #[test]
+    fn blake2b_0_outlen() {
+        // Setup
+        let buf = Bstr::from("test test");
+        let outlen = Some(0);
+
+        let result =
+            blake2b_impl(buf, outlen, None).expect("");
     }
 }
