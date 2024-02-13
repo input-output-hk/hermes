a15fc37d60e82bbc50dd52f316782e5a2a9992d2
bkioshn <bkioshn@gmail.com>
2024-02-13 09:31:06 +0700

    feat: implement blake2bmac_impl (not work)

--- hermes/bin/src/runtime/host/hermes/hash/mod.rs
+++ hermes/bin/src/runtime/host/hermes/hash/mod.rs
@@ -1,7 +1,7 @@
 //! Host - Hash implementations
 #![allow(unused_variables)]
 
-use blake2::digest::{Update, VariableOutput};
+use blake2::digest::{consts::U64, FixedOutput, Update, VariableOutput};
 use blake2::Blake2bVar;
 
 use crate::runtime::extensions::{
@@ -42,6 +42,29 @@ fn blake2b_impl(buf: Bstr, outlen: Option<u8>) -> Result<Bstr, Errno> {
     return Ok(Bstr::from(output));
 }
 
+fn blake2bmac_impl(
+    buf: Bstr, outlen: Option<u8>, key: Bstr, salt: Option<Bstr>, persona: Option<Bstr>,
+) -> Result<Bstr, Errno> {
+    // Convert outlen to usize
+    let outlen = outlen.unwrap_or(64) as usize;
+
+    // outlen is set
+    // invalid when == 0 or > 64
+    if outlen == 0 {
+        return Err(Errno::InvalidDigestByteLength);
+    } else if outlen > 64 {
+        return Err(Errno::HashTooBig);
+    }
+
+    let salt = salt.unwrap_or_default();
+    let persona = persona.unwrap_or_default();
+    let mut hasher =
+        blake2::Blake2bMac::<U64>::new_with_salt_and_personal(&key, &salt, &persona).unwrap();
+    hasher.update(&buf);
+
+    return Ok(Bstr::from(hasher.finalize_fixed().to_vec()));
+}
+
 impl Host for HermesState {
     /// Hash a binary buffer with BLAKE2s
     fn blake2s(&mut self, buf: Bstr, outlen: Option<u8>) -> wasmtime::Result<Result<Bstr, Errno>> {
@@ -50,7 +73,7 @@ impl Host for HermesState {
 
     /// Hash a binary buffer with `BLAKE2s` with `MAC` (Message Authentication Code) mode
     fn blake2smac(
-        &mut self, buf: Bstr, outlen: Option<u8>, key: Option<Bstr>, salt: Option<Bstr>,
+        &mut self, buf: Bstr, outlen: Option<u8>, key: Bstr, salt: Option<Bstr>,
         persona: Option<Bstr>,
     ) -> wasmtime::Result<Result<Bstr, Errno>> {
         todo!()
@@ -67,10 +90,14 @@ impl Host for HermesState {
 
     /// Hash a binary buffer with `BLAKE2b` with `MAC` (Message Authentication Code) mode
     fn blake2bmac(
-        &mut self, buf: Bstr, outlen: Option<u8>, key: Option<Bstr>, salt: Option<Bstr>,
+        &mut self, buf: Bstr, outlen: Option<u8>, key: Bstr, salt: Option<Bstr>,
         persona: Option<Bstr>,
     ) -> wasmtime::Result<Result<Bstr, Errno>> {
-        todo!()
+        let hash = blake2bmac_impl(buf, outlen, key, salt, persona);
+        match hash {
+            Ok(hash) => Ok(Ok(hash)),
+            Err(err) => Err(err.into()),
+        }
     }
 
     /// Hash a binary buffer with BLAKE3
@@ -91,7 +118,7 @@ mod tests_blake2b {
         let buf = Bstr::from("test test");
         let outlen = Some(64);
 
-        let result = blake2b_impl(buf, outlen).expect("Failed to hash blake2b-512 without key");
+        let result = blake2b_impl(buf, outlen).expect("Failed to hash blake2b-512");
 
         assert_eq!(
             result.as_ref(),
@@ -104,7 +131,7 @@ mod tests_blake2b {
         let buf = Bstr::from("test test");
         let outlen = Some(32);
 
-        let result = blake2b_impl(buf, outlen).expect("Failed to hash blake2b-256 without key");
+        let result = blake2b_impl(buf, outlen).expect("Failed to hash blake2b-256");
 
         assert_eq!(
             result.as_ref(),
@@ -116,7 +143,7 @@ mod tests_blake2b {
         let buf = Bstr::from("test test");
 
         let result =
-            blake2b_impl(buf, None).expect("Failed to hash blake2b-512 default outlen without key");
+            blake2b_impl(buf, None).expect("Failed to hash blake2b-512 default outlen");
 
         assert_eq!(
             result.as_ref(),
@@ -140,5 +167,51 @@ mod tests_blake2b {
 
         let result = blake2b_impl(buf, outlen);
         assert_eq!(result.unwrap_err(), Errno::HashTooBig)
+    }
+    #[test]
+    fn blake2bmac_512() {
+        let buf = Bstr::from("test test");
+        let key = Bstr::from("key");
+        let outlen = Some(64);
+
+        let result =
+            blake2bmac_impl(buf, outlen, key, None, None).expect("Failed to hash blake2bmac-512");
+
+        assert_eq!(
+        result.as_ref(),
+        hex!("c28029cbab4e11d759e971d7e2a13dbe9ef60d2fa539cc03138b0432c3fdb2757b6c87383bd1074f5533c0c2ad2a5d2ac71bbd96f0f8fbb4c3ba0d4abb309115")
+    );
+    }
+
+    #[test]
+    fn blake2bmac_512_salt_persona() {
+        let buf = Bstr::from("test test");
+        let key = Bstr::from("key");
+        let salt = Bstr::from("salt");
+        let persona = Bstr::from("persona");
+        let outlen = Some(64);
+
+        let result =
+            blake2bmac_impl(buf, outlen, key, Some(salt), Some(persona)).expect("Failed to hash blake2bmac-512");
+
+        assert_eq!(
+        result.as_ref(),
+        hex!("c28029cbab4e11d759e971d7e2a13dbe9ef60d2fa539cc03138b0432c3fdb2757b6c87383bd1074f5533c0c2ad2a5d2ac71bbd96f0f8fbb4c3ba0d4abb309115")
+    );
+    }
+
+    #[test]
+    fn blake2bmac_512_0_key() {
+        let buf = Bstr::from("test test");
+        let key = vec![];
+        let outlen = Some(64);
+
+        let result =
+            blake2bmac_impl(buf, outlen, key, None, None).expect("Failed to hash blake2bmac-512 with emprty key");
+
+        assert_eq!(
+            result.as_ref(),
+            hex!("8e27b2481dd1fe73d598104c03b1f67da60725abb73cf66e400177d73aee01e74b93f55adda27b0ad92e22e284b5e0cc95ad81b04b496bd58c4ae6bca5f56196")
+        );
     }
 }
