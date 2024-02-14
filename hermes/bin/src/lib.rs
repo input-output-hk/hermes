d73d4b129c2c6168703aeb365d86e51a8ffb1b49
Mr-Leshiy <leshiy12345678@gmail.com>
2024-02-14 16:16:55 +0200

    remove reactor

--- hermes/bin/src/lib.rs
+++ hermes/bin/src/lib.rs
@@ -2,8 +2,6 @@
 //! This file exists, so that doc tests can be used inside binary crates.
 
 mod event_queue;
-#[allow(missing_docs, clippy::missing_docs_in_private_items, dead_code)]
-mod reactor;
 mod runtime_extensions;
 mod state;
 mod wasm;
