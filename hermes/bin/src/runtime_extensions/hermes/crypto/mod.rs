20e54b67a0f30588f403c882ff5fd99a086e98d7
bkioshn <bkioshn@gmail.com>
2024-02-16 20:31:49 +0700

    feat: implement state and resource holder

--- hermes/bin/src/runtime_extensions/hermes/crypto/mod.rs
+++ hermes/bin/src/runtime_extensions/hermes/crypto/mod.rs
@@ -1,14 +1,52 @@
 //! Crypto runtime extension implementation.
 
-use crate::runtime_extensions::state::{Context, Stateful};
+use std::collections::HashMap;
+
+use crate::runtime_extensions::{
+    bindings::hermes::crypto::api::{Ed25519Bip32PrivateKey},
+    state::{Context, Stateful},
+};
 
 mod host;
 
+#[derive(Default, Clone)]
+struct ResHolder<T> {
+    resources: HashMap<u32, T>,
+    next_id: u32,
+}
+
+impl<T> ResHolder<T> {
+    fn new(&mut self, item: T) -> u32 {
+        let id = self.next_id;
+        self.resources.insert(id, item);
+        self.next_id += 1;
+        id
+    }
+
+    fn _get(&self, id: u32) -> Option<&T> {
+        self.resources.get(&id)
+    }
+
+    fn _drop(&mut self, id: u32) -> Result<(), ()> {
+        self.resources.remove(&id).map(|_| ()).ok_or(())
+    }
+}
+
+#[derive(Default)]
+#[allow(dead_code)]
+struct Ed25519Bip32Struct {
+    private_key: Ed25519Bip32PrivateKey,
+}
+
 /// State
-pub(crate) struct State {}
+pub(crate) struct State {
+    private_key: ResHolder<Option<Ed25519Bip32Struct>>,
+}
 
 impl Stateful for State {
     fn new(_ctx: &Context) -> Self {
-        State {}
+        State {
+            private_key: ResHolder::default(),
+        }
     }
 }
