d4803d8cebe2ae7da9152c9bd6fe5a5c06837184
bkioshn <bkioshn@gmail.com>
2024-02-22 16:22:17 +0700

    fix: restructure crypto resource and states

--- hermes/bin/src/runtime_extensions/hermes/crypto/mod.rs
+++ hermes/bin/src/runtime_extensions/hermes/crypto/mod.rs
@@ -1,37 +1,40 @@
 //! Crypto runtime extension implementation.
 
-use std::collections::HashMap;
+use dashmap::DashMap;
+use rusty_ulid::Ulid;
 
-use crate::runtime_extensions::{
-    bindings::hermes::crypto::api::Ed25519Bip32PrivateKey,
-    state::{Context, Stateful},
-};
+use crate::runtime_extensions::state::{Context, Stateful};
 
 mod host;
-
+type XPriv = [u8; 96];
 #[derive(Default, Clone)]
 
 /// Resource holder: a simple resource manager.
-struct ResHolder<T> {
+struct ResHolder<XPriv> {
     /// Map of resources.
-    resources: HashMap<u32, T>,
-    /// The next id that will be used.
-    next_id: u32,
+    resources: DashMap<u32, XPriv>,
 }
 
-impl<T> ResHolder<T> {
+impl<XPriv> ResHolder<XPriv> {
     /// Insert new Resource where item is added with the id.
     /// Id will be incremented by 1 each time.
-    fn add(&mut self, item: T) -> u32 {
-        let id = self.next_id;
+    fn add(&mut self, item: XPriv) -> u32 {
+        // Get the highest key and increment it by 1
+        // Can't rely on the length since item can be removed.
+        let id = self
+            .resources
+            .iter()
+            .map(|entry| entry.key())
+            .max()
+            .unwrap_or(&0)
+            + 1;
         self.resources.insert(id, item);
-        self.next_id += 1;
         id
     }
 
     /// Get the item from resources using id if possible.
-    fn get(&self, id: u32) -> Option<&T> {
-        self.resources.get(&id)
+    fn get(&self, id: u32) -> Option<&XPriv> {
+        self.resources.get(&id).map(|entry| entry.value().clone())
     }
 
     /// Drop the item from resources using id if possible.
@@ -40,16 +43,31 @@ impl<T> ResHolder<T> {
     }
 }
 
+struct ExtendedKeyMap {
+    priv_to_index: DashMap<XPriv, u32>,
+    index_to_priv: DashMap<u32, XPriv>,
+}
+
+struct CryptoState {
+    current_exec_count: u64,
+    resource_holder: ResHolder<XPriv>,
+}
+
+pub(crate) type Storage = DashMap<String, DashMap<Ulid, DashMap<Option<String>, CryptoState>>>;
 /// State
 pub(crate) struct State {
-    /// Resource of Ed25519-Bip32 private key.
-    private_key: ResHolder<Ed25519Bip32PrivateKey>,
+    storage: Storage,
+    extended_key_map: ExtendedKeyMap,
 }
 
 impl Stateful for State {
-    fn new(_ctx: &Context) -> Self {
+    fn new(ctx: &Context) -> Self {
         State {
-            private_key: ResHolder::default(),
+            storage: DashMap::default(),
+            extended_key_map: ExtendedKeyMap {
+                priv_to_index: DashMap::default(),
+                index_to_priv: DashMap::default(),
+            },
         }
     }
 }
