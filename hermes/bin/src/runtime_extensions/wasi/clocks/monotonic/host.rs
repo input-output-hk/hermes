ffd8b026d79b1ffced03706fe47715624d1ffce8
Joaquín Rosales <joaquin.rosales@iohk.io>
2024-02-21 18:00:57 -0600

    feat(wip): monotonic clock resolution

--- hermes/bin/src/runtime_extensions/wasi/clocks/monotonic/host.rs
+++ hermes/bin/src/runtime_extensions/wasi/clocks/monotonic/host.rs
@@ -1,5 +1,6 @@
 //! Monotonic clock host implementation for WASM runtime.
 
+use super::{now_impl, resolution_impl};
 use crate::{
     runtime_extensions::bindings::wasi::clocks::monotonic_clock::{Duration, Host, Instant},
     state::HermesState,
@@ -11,12 +12,12 @@ impl Host for HermesState {
     /// The clock is monotonic, therefore calling this function repeatedly will
     /// produce a sequence of non-decreasing values.
     fn now(&mut self) -> wasmtime::Result<Instant> {
-        todo!()
+        now_impl()
     }
 
     /// Query the resolution of the clock. Returns the duration of time
     /// corresponding to a clock tick.
     fn resolution(&mut self) -> wasmtime::Result<Duration> {
-        todo!()
+        resolution_impl()
     }
 }
