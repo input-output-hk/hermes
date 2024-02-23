927658ec38e7d48051c1e14978762c222a600629
bkioshn <bkioshn@gmail.com>
2024-02-23 16:50:09 +0700

    fix: extract state to its own mod

--- hermes/bin/src/runtime_extensions/hermes/crypto/mod.rs
+++ hermes/bin/src/runtime_extensions/hermes/crypto/mod.rs
@@ -1,73 +1,19 @@
 //! Crypto runtime extension implementation.
 
-use dashmap::DashMap;
-use rusty_ulid::Ulid;
-
 use crate::runtime_extensions::state::{Context, Stateful};
 
 mod host;
-type XPriv = [u8; 96];
-#[derive(Default, Clone)]
-
-/// Resource holder: a simple resource manager.
-struct ResHolder<XPriv> {
-    /// Map of resources.
-    resources: DashMap<u32, XPriv>,
-}
-
-impl<XPriv> ResHolder<XPriv> {
-    /// Insert new Resource where item is added with the id.
-    /// Id will be incremented by 1 each time.
-    fn add(&mut self, item: XPriv) -> u32 {
-        // Get the highest key and increment it by 1
-        // Can't rely on the length since item can be removed.
-        let id = self
-            .resources
-            .iter()
-            .map(|entry| entry.key())
-            .max()
-            .unwrap_or(&0)
-            + 1;
-        self.resources.insert(id, item);
-        id
-    }
-
-    /// Get the item from resources using id if possible.
-    fn get(&self, id: u32) -> Option<&XPriv> {
-        self.resources.get(&id).map(|entry| entry.value().clone())
-    }
-
-    /// Drop the item from resources using id if possible.
-    fn drop(&mut self, id: u32) -> Result<(), ()> {
-        self.resources.remove(&id).map(|_| ()).ok_or(())
-    }
-}
-
-struct ExtendedKeyMap {
-    priv_to_index: DashMap<XPriv, u32>,
-    index_to_priv: DashMap<u32, XPriv>,
-}
-
-struct CryptoState {
-    current_exec_count: u64,
-    resource_holder: ResHolder<XPriv>,
-}
-
-pub(crate) type Storage = DashMap<String, DashMap<Ulid, DashMap<Option<String>, CryptoState>>>;
-/// State
-pub(crate) struct State {
-    storage: Storage,
-    extended_key_map: ExtendedKeyMap,
-}
+pub(crate) struct State {}
 
 impl Stateful for State {
     fn new(ctx: &Context) -> Self {
+        // pass app_name .... to function in state.rs (initialize state)
         State {
-            storage: DashMap::default(),
-            extended_key_map: ExtendedKeyMap {
-                priv_to_index: DashMap::default(),
-                index_to_priv: DashMap::default(),
-            },
+            // storage: DashMap::default(),
+            // extended_key_map: ExtendedKeyMap {
+            //     priv_to_index: DashMap::default(),
+            //     index_to_priv: DashMap::default(),
+            // },
         }
     }
 }
---
+++ hermes/bin/src/runtime_extensions/hermes/crypto/state.rs
@@ -0,0 +1,68 @@
+//! Crypto state
+
+use dashmap::DashMap;
+use once_cell::sync::Lazy;
+use rusty_ulid::Ulid;
+
+pub(crate) type Storage = DashMap<String, DashMap<Ulid, DashMap<Option<String>, CryptoState>>>;
+type XPriv = [u8; 96];
+
+/// Map of XPriv to index and index to XPriv.
+struct ExtendedKeyMap {
+    priv_to_index: DashMap<XPriv, u32>,
+    index_to_priv: DashMap<u32, XPriv>,
+}
+
+struct CryptoState {
+    current_exec_count: u64,
+    resource_holder: ResourceHolder<XPriv>,
+}
+
+/// Crypto internal state
+struct InternalState {
+    storage: Storage,
+    extended_key_map: ExtendedKeyMap,
+}
+
+static CRYPTO_INTERNAL_STATE: Lazy<InternalState> = Lazy::new(|| InternalState {
+    storage: Storage::default(),
+    extended_key_map: ExtendedKeyMap {
+        priv_to_index: DashMap::new(),
+        index_to_priv: DashMap::new(),
+    },
+});
+
+#[derive(Default, Clone)]
+/// A resource manager.
+struct ResourceHolder<XPriv> {
+    /// Map of resources.
+    resources: DashMap<u32, XPriv>,
+}
+
+impl<XPriv> ResourceHolder<XPriv> {
+    /// Insert new Resource where item is added with the id.
+    /// Id will be incremented by 1 each time.
+    fn add(&mut self, item: XPriv) -> u32 {
+        // Get the highest key and increment it by 1
+        // Can't rely on the length since item can be removed.
+        let id = self
+            .resources
+            .iter()
+            .map(|entry| entry.key())
+            .max()
+            .unwrap_or(&0)
+            + 1;
+        self.resources.insert(id, item);
+        id
+    }
+
+    /// Get the item from resources using id if possible.
+    fn get(&self, id: u32) -> Option<&XPriv> {
+        self.resources.get(&id).map(|entry| entry.value().clone())
+    }
+
+    /// Drop the item from resources using id if possible.
+    fn drop(&mut self, id: u32) -> Result<(), ()> {
+        self.resources.remove(&id).map(|_| ()).ok_or(())
+    }
+}
