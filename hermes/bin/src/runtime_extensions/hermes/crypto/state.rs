3279506f2c01ba31f230f72a707356b59354a589
bkioshn <bkioshn@gmail.com>
2024-02-25 20:53:41 +0700

    fix: crypto state implementation

--- hermes/bin/src/runtime_extensions/hermes/crypto/state.rs
+++ hermes/bin/src/runtime_extensions/hermes/crypto/state.rs
@@ -1,68 +1,116 @@
 //! Crypto state
 
+use std::sync::Mutex;
+
 use dashmap::DashMap;
+//  std::sync::LazyLock is still unstable
 use once_cell::sync::Lazy;
 use rusty_ulid::Ulid;
 
-pub(crate) type Storage = DashMap<String, DashMap<Ulid, DashMap<Option<String>, CryptoState>>>;
+type Storage = DashMap<String, DashMap<Ulid, DashMap<Option<String>, CryptoState>>>;
 type XPriv = [u8; 96];
 
-/// Map of XPriv to index and index to XPriv.
-struct ExtendedKeyMap {
-    priv_to_index: DashMap<XPriv, u32>,
-    index_to_priv: DashMap<u32, XPriv>,
-}
-
+#[allow(dead_code)]
+#[derive(Default, Clone, Debug)]
 struct CryptoState {
     current_exec_count: u64,
-    resource_holder: ResourceHolder<XPriv>,
+    resources: DashMap<u32, XPriv>,
 }
 
 /// Crypto internal state
 struct InternalState {
     storage: Storage,
-    extended_key_map: ExtendedKeyMap,
 }
 
-static CRYPTO_INTERNAL_STATE: Lazy<InternalState> = Lazy::new(|| InternalState {
-    storage: Storage::default(),
-    extended_key_map: ExtendedKeyMap {
-        priv_to_index: DashMap::new(),
-        index_to_priv: DashMap::new(),
-    },
+#[allow(dead_code)]
+// CryptoState should be mutable so Mutex is used.
+static CRYPTO_INTERNAL_STATE: Lazy<Mutex<InternalState>> = Lazy::new(|| {
+    Mutex::new(InternalState {
+        storage: Storage::default(),
+    })
 });
 
-#[derive(Default, Clone)]
-/// A resource manager.
-struct ResourceHolder<XPriv> {
-    /// Map of resources.
-    resources: DashMap<u32, XPriv>,
+impl InternalState {
+    #[allow(dead_code)]
+    fn new_storage(&mut self, app_name: String, module_id: Ulid, event_name: Option<String>) {
+        let event_map = DashMap::new();
+        event_map.insert(
+            event_name,
+            CryptoState {
+                current_exec_count: 0,
+                resources: DashMap::new(),
+            },
+        );
+
+        let module_map = DashMap::new();
+        module_map.insert(module_id, event_map);
+
+        let app_map = DashMap::new();
+        app_map.insert(app_name, module_map);
+        self.storage = app_map;
+    }
+
+    #[allow(dead_code)]
+    /// Get the storage.
+    fn _get_storage(&self) -> &Storage {
+        &self.storage
+    }
+
+    #[allow(dead_code)]
+    /// Check context
+    fn check_context(
+        &self, app_name: &String, module_id: &Ulid, event_name: Option<String>,
+    ) -> bool {
+        self.storage.contains_key(app_name)
+            && self.storage.get(app_name).unwrap().contains_key(module_id)
+            && self
+                .storage
+                .get(app_name)
+                .unwrap()
+                .get(module_id)
+                .unwrap()
+                .contains_key(&event_name)
+    }
 }
 
-impl ResourceHolder<XPriv> {
+impl CryptoState {
     /// Insert new Resource where item is added with the id.
     /// Id will be incremented by 1 each time.
-    fn add(&mut self, item: XPriv) -> u32 {
+    fn _add_resource(&mut self, item: XPriv) -> u32 {
         // Get the highest key and increment it by 1
         // Can't rely on the length since item can be removed.
         let id = self
             .resources
             .iter()
-            .map(|entry| entry.key())
+            .map(|entry| entry.key().clone())
             .max()
-            .unwrap_or(&0)
+            .unwrap_or(0)
             + 1;
+        let _ = self.current_exec_count + 1;
         self.resources.insert(id, item);
         id
     }
 
+
     /// Get the item from resources using id if possible.
-    fn get(&self, id: u32) -> Option<&XPriv> {
-        self.resources.get(&id).map(|entry| entry.value().clone())
+    fn _get(&self, id: u32) -> Option<XPriv> {
+        self.resources.get(&id).map(|x| x.clone())
     }
 
     /// Drop the item from resources using id if possible.
-    fn drop(&mut self, id: u32) -> Result<(), ()> {
+    fn _drop(&mut self, id: u32) -> Result<(), ()> {
         self.resources.remove(&id).map(|_| ()).ok_or(())
+    }
+}
+
+
+#[cfg(test)]
+mod tests_crypto_state {
+    use super::*;
+    #[test]
+    fn test_storage_context() {
+        let ulid: Ulid = 1.into();
+        CRYPTO_INTERNAL_STATE.lock().unwrap().new_storage("Test".to_string(), ulid, Some("Test Event".to_string()));
+        debug_assert_eq!(CRYPTO_INTERNAL_STATE.lock().unwrap().check_context(&"Test".to_string(), &ulid, Some("Test Event".to_string())), true);
     }
 }
