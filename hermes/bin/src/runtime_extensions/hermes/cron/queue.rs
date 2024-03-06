d69660316ac5ca74f162034a9bac4cf07502f9bd
Joaquín Rosales <joaquin.rosales@iohk.io>
2024-03-05 19:35:37 -0600

    feat: implement cron queue storage functionality

--- hermes/bin/src/runtime_extensions/hermes/cron/queue.rs
+++ hermes/bin/src/runtime_extensions/hermes/cron/queue.rs
@@ -44,3 +44,161 @@ pub(crate) struct CronEventQueue {
     /// Send events to the crontab queue.
     sender: mpsc::Sender<CronJob>,
 }
+
+impl CronEventQueue {
+    /// Create a new `CronQueueTask`.
+    pub(crate) fn new(sender: mpsc::Sender<CronJob>) -> Self {
+        Self {
+            events: DashMap::default(),
+            sender,
+        }
+    }
+
+    /// Spawn a new cron job.
+    pub(crate) fn spawn_cron_job(&self, cron_job: CronJob) -> anyhow::Result<()> {
+        Ok(self.sender.blocking_send(cron_job)?)
+    }
+
+    /// Add a new crontab entry.
+    fn add_event(&self, app_name: AppName, timestamp: CronTimestamp, on_cron_event: OnCronEvent) {
+        self.events
+            .entry(app_name)
+            .and_modify(|e| {
+                e.entry(timestamp)
+                    .and_modify(|e| {
+                        e.push(on_cron_event.clone());
+                    })
+                    .or_insert_with(|| vec![on_cron_event.clone()]);
+            })
+            .or_insert_with(|| BTreeMap::from([(timestamp, vec![on_cron_event])]));
+    }
+
+    /// List all the crontab entries for the given app.
+    fn ls_events(
+        &self, app_name: &AppName, cron_tagged: &Option<CronEventTag>,
+    ) -> Vec<(CronTagged, bool)> {
+        if let Some(app) = self.events.get(app_name) {
+            app.iter().fold(vec![], |mut v, (_, cron_events)| {
+                //
+                if let Some(tag) = cron_tagged {
+                    for OnCronEvent { tag, last } in cron_events
+                        .iter()
+                        .filter(|event| event.tag.tag == tag.clone())
+                    {
+                        v.push((tag.clone(), *last));
+                    }
+                } else {
+                    for OnCronEvent { tag, last } in cron_events {
+                        v.push((tag.clone(), *last));
+                    }
+                };
+                v
+            })
+        } else {
+            vec![]
+        }
+    }
+
+    /// Remove a crontab entry for the given app.
+    fn rm_event(&self, app_name: &AppName, cron_tagged: &CronTagged) -> bool {
+        let mut response = false;
+        if let Some(mut app) = self.events.get_mut(app_name) {
+            app.retain(|_ts, events| {
+                let start = events.len();
+                // Keep `OnCronEvent`s that do not include `cron_tagged`.
+                events.retain(|e| e.tag != *cron_tagged);
+                let end = events.len();
+                // Check if `events` has changed in length, if so, set the `response` to true.
+                if start != end {
+                    response = true;
+                }
+                // retain if `events` is not empty
+                !events.is_empty()
+            });
+        }
+        response
+    }
+}
+
+/// The crontab queue task runs in the background.
+pub(crate) async fn cron_queue_task(mut queue_rx: mpsc::Receiver<CronJob>) {
+    while let Some(cron_job) = queue_rx.recv().await {
+        match cron_job {
+            CronJob::Add(app_name, on_cron_event, response_tx) => {
+                //
+                handle_add_cron_job(app_name, on_cron_event, response_tx);
+            },
+            CronJob::List(app_name, tag, response_tx) => {
+                //
+                handle_ls_cron_job(&app_name, &tag, response_tx);
+            },
+            CronJob::Delay(app_name, cron_job_delay, response_tx) => {
+                handle_delay_cron_job(app_name, cron_job_delay, response_tx);
+            },
+            CronJob::Remove(app_name, cron_tagged, response_tx) => {
+                //
+                handle_rm_cron_job(&app_name, &cron_tagged, response_tx);
+            },
+        }
+    }
+}
+
+/// Handle the `CronJob::Add` command.
+#[allow(clippy::expect_used)]
+fn handle_add_cron_job(
+    app_name: AppName, on_cron_event: OnCronEvent, response_tx: oneshot::Sender<bool>,
+) {
+    let response = if let Some(timestamp) = on_cron_event.next_tick(None) {
+        CRON_INTERNAL_STATE
+            .cron_queue
+            .add_event(app_name, timestamp, on_cron_event);
+        true
+    } else {
+        false
+    };
+    response_tx
+        .send(response)
+        .expect("tokio channel should work.");
+}
+
+/// Handle the `CronJob::List` command.
+#[allow(clippy::expect_used)]
+fn handle_ls_cron_job(
+    app_name: &AppName, cron_tagged: &Option<CronEventTag>,
+    response_tx: oneshot::Sender<Vec<(CronTagged, bool)>>,
+) {
+    let response = CRON_INTERNAL_STATE
+        .cron_queue
+        .ls_events(app_name, cron_tagged);
+    response_tx
+        .send(response)
+        .expect("tokio channel should work.");
+}
+
+/// Handle the `CronJob::Delay` command.
+#[allow(clippy::expect_used)]
+fn handle_delay_cron_job(
+    app_name: AppName, CronJobDelay { timestamp, event }: CronJobDelay,
+    response_tx: oneshot::Sender<bool>,
+) {
+    CRON_INTERNAL_STATE
+        .cron_queue
+        .add_event(app_name, timestamp, event);
+    let response = true;
+    response_tx
+        .send(response)
+        .expect("tokio channel should work.");
+}
+
+/// Handle the `CronJob::Remove` command.
+#[allow(clippy::expect_used)]
+fn handle_rm_cron_job(
+    app_name: &AppName, cron_tagged: &CronTagged, response_tx: oneshot::Sender<bool>,
+) {
+    let response = CRON_INTERNAL_STATE
+        .cron_queue
+        .rm_event(app_name, cron_tagged);
+    response_tx
+        .send(response)
+        .expect("tokio channel should work.");
+}
