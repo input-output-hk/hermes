9d6643d7edb17d0f6a12ac719b81a81ba6368386
Joaquín Rosales <joaquin.rosales@iohk.io>
2024-03-05 19:39:08 -0600

    refactor: state is managed in threaded async queue

--- hermes/bin/src/runtime_extensions/hermes/cron/state.rs
+++ hermes/bin/src/runtime_extensions/hermes/cron/state.rs
@@ -1,154 +1,57 @@
 //! Cron State.
 /// The crontabs hash map.
-use std::{
-    collections::BTreeSet,
-    hash::{Hash, Hasher},
-    process::exit,
-    sync::{mpsc, Arc, Mutex},
-    thread::JoinHandle,
+use std::hash::{Hash, Hasher};
+
+use once_cell::sync::Lazy;
+use tokio::{
+    runtime::Builder,
+    sync::{mpsc, oneshot},
 };
 
-use chrono::Utc;
-use dashmap::DashMap;
-//  std::sync::LazyLock is still unstable
-use once_cell::sync::Lazy;
-use saffron::Cron;
-
-use super::event::OnCronEvent;
+use super::{
+    event::OnCronEvent,
+    queue::{cron_queue_task, CronEventQueue, CronJob},
+};
 use crate::runtime_extensions::{
     bindings::hermes::cron::api::{CronEventTag, CronTagged, Instant},
     hermes::cron::mkdelay_crontab,
 };
 
 /// Cron Internal State
-pub(crate) static CRON_INTERNAL_STATE: Lazy<Mutex<InternalState>> =
-    Lazy::new(|| Mutex::new(InternalState::new()));
+#[allow(clippy::expect_used)]
+pub(crate) static CRON_INTERNAL_STATE: Lazy<InternalState> = Lazy::new(|| {
+    //
+    let (sender, receiver) = mpsc::channel(1);
+
+    let runtime = Builder::new_current_thread()
+        .enable_all()
+        .build()
+        .expect("tokio runtime should work.");
+
+    let _handle = std::thread::spawn(move || {
+        runtime.block_on(async move {
+            let h = tokio::spawn(cron_queue_task(receiver));
+            drop(tokio::join!(h));
+        });
+        std::process::exit(0);
+    });
+    InternalState::new(sender)
+});
 
 /// Name of the Application that owns the `OnCronEvent`s.
-type AppName = String;
-
-/// Storage for application-specific crontabs.
-type AppCronState = DashMap<CronTagged, OnCronEvent>;
-
-/// Storage for the crontabs.
-type CronTabStorage = DashMap<AppName, AppCronState>;
-
-/// Timestamp for when to run the cron.
-type CronTimestamp = u64;
-
-/// Scheduled Date and Time for sending a cron event.
-struct ScheduledCron {
-    /// Scheduled time for running the event.
-    _timestamp: CronTimestamp,
-    /// The crontab event.
-    _event: OnCronEvent,
-}
-
-impl OnCronEvent {
-    /// Get the next scheduled cron event after (excluding) an optional start timestamp.
-    ///
-    /// # Parameters
-    ///
-    /// * `start: Option<CronTimestamp>` - The optional start timestamp. If `None`, the
-    ///   current time is used.
-    ///
-    /// # Returns
-    ///
-    /// * `Some(CronTimestamp)` - The next timestamp for the `OnCronEvent`.
-    /// * `None` if the timestamp could not be calculated.
-    fn _after(&self, start: Option<CronTimestamp>) -> Option<CronTimestamp> {
-        let cron = self._cron()?;
-        if cron.any() {
-            let datetime = Self::_start_datetime(start)?;
-            let cdt = cron.iter_after(datetime).next()?;
-            let timestamp = cdt.timestamp_nanos_opt()?;
-            timestamp.try_into().ok()
-        } else {
-            None
-        }
-    }
-
-    /// Get the next scheduled cron event from (including) an optional start timestamp.
-    ///
-    /// # Parameters
-    ///
-    /// * `start: Option<CronTimestamp>` - The optional start timestamp. If `None`, the
-    ///   current time is used.
-    ///
-    /// # Returns
-    ///
-    /// * `Some(CronTimestamp)` - The next timestamp for the `OnCronEvent`.
-    /// * `None` if the timestamp could not be calculated.
-    fn _from(&self, start: Option<CronTimestamp>) -> Option<CronTimestamp> {
-        let cron = self._cron()?;
-        if cron.any() {
-            let datetime = Self::_start_datetime(start)?;
-            let cdt = cron.iter_from(datetime).next()?;
-            let timestamp = cdt.timestamp_nanos_opt()?;
-            timestamp.try_into().ok()
-        } else {
-            None
-        }
-    }
-
-    /// Get the cron.
-    fn _cron(&self) -> Option<Cron> {
-        let when = &self.tag.when;
-        when.parse::<Cron>().ok()
-    }
-
-    /// Get the UTC datetime from an optional start timestamp.
-    ///
-    /// Use the `start` timestamp if provided, otherwise use the current time.
-    ///
-    /// Returns `None` if the datetime could not be calculated.:w
-    fn _start_datetime(start: Option<CronTimestamp>) -> Option<chrono::DateTime<Utc>> {
-        let datetime = match start {
-            None => Utc::now(),
-            Some(dt) => {
-                let dt = chrono::NaiveDateTime::from_timestamp_nanos(dt.try_into().ok()?)?;
-                chrono::DateTime::from_naive_utc_and_offset(dt, Utc)
-            },
-        };
-        Some(datetime)
-    }
-}
+pub type AppName = String;
 
 /// Internal State.
 pub(crate) struct InternalState {
-    /// The storage for crontabs.
-    storage: CronTabStorage,
     /// The send events to the crontab queue.
-    _cron_queue: mpsc::Sender<ScheduledCron>,
-    /// The crontab queue task runs in the background.
-    _task: CronQueueTask,
-}
-
-/// The crontab queue task runs in the background.
-struct CronQueueTask {
-    /// Handle to the crontab queue task.
-    _task: JoinHandle<()>,
-}
-
-impl CronQueueTask {
-    /// Create a new `CronQueueTask`.
-    fn new(_receiver: &Arc<Mutex<mpsc::Receiver<ScheduledCron>>>) -> Self {
-        let handle = std::thread::spawn(|| {
-            println!("wip: cron queue task goes here");
-            exit(0);
-        });
-        Self { _task: handle }
-    }
+    pub(crate) cron_queue: CronEventQueue,
 }
 
 impl InternalState {
     /// Create a new `InternalState`.
-    pub(crate) fn new() -> Self {
-        let (sender, receiver) = mpsc::channel();
+    pub(crate) fn new(sender: mpsc::Sender<CronJob>) -> Self {
         Self {
-            storage: CronTabStorage::default(),
-            _cron_queue: sender,
-            _task: CronQueueTask::new(&Arc::new(Mutex::new(receiver))),
+            cron_queue: CronEventQueue::new(sender),
         }
     }
 
@@ -170,20 +73,21 @@ impl InternalState {
     /// - `true`: Crontab added successfully.
     /// - `false`: Crontab failed to be added.
     pub(crate) fn add_crontab(&self, app_name: &str, entry: CronTagged, retrigger: bool) -> bool {
-        let tagged = entry.clone();
         let crontab = OnCronEvent {
             tag: entry,
             last: retrigger,
         };
-        if let Some(app_cron) = self.storage.get_mut(app_name) {
-            app_cron.insert(tagged, crontab);
+        let (cmd_tx, cmd_rx) = oneshot::channel();
+        drop(
+            self.cron_queue
+                .spawn_cron_job(CronJob::Add(app_name.into(), crontab, cmd_tx)),
+        );
+        // TODO(saibatizoku): deal with errors
+        if let Ok(resp) = cmd_rx.blocking_recv() {
+            resp
         } else {
-            let app_cron: DashMap<CronTagged, OnCronEvent> = DashMap::new();
-            app_cron.insert(tagged, crontab);
-            self.storage.insert(AppName::from(app_name), app_cron);
+            false
         }
-        // todo!("implement cron event queue")
-        true
     }
 
     /// Schedule a single cron event after a fixed delay.
@@ -206,10 +110,18 @@ impl InternalState {
     pub(crate) fn delay_crontab(
         &self, app_name: &str, duration: Instant, tag: CronEventTag,
     ) -> wasmtime::Result<bool> {
-        let crontagged = mkdelay_crontab(duration, tag)?;
-        self.add_crontab(app_name, crontagged, false);
-        // todo!("implement cron event queue")
-        Ok(true)
+        let cron_delay = mkdelay_crontab(duration, tag)?;
+        let (cmd_tx, cmd_rx) = oneshot::channel();
+        drop(
+            self.cron_queue
+                .spawn_cron_job(CronJob::Delay(app_name.into(), cron_delay, cmd_tx)),
+        );
+        // TODO(saibatizoku): deal with errors
+        if let Ok(resp) = cmd_rx.blocking_recv() {
+            Ok(resp)
+        } else {
+            Ok(false)
+        }
     }
 
     /// List crontabs for an application.
@@ -232,23 +144,14 @@ impl InternalState {
     pub(crate) fn ls_crontabs(
         &self, app_name: &str, tag: Option<CronEventTag>,
     ) -> Vec<(CronTagged, bool)> {
-        if let Some(app_cron) = self.storage.get_mut(app_name) {
-            if let Some(tag) = tag {
-                app_cron
-                    .iter()
-                    .filter(|cron| cron.tag.tag == tag)
-                    .map(|cron| (cron.tag.clone(), cron.last))
-                    .collect::<BTreeSet<(CronTagged, bool)>>()
-                    .into_iter()
-                    .collect()
-            } else {
-                app_cron
-                    .iter()
-                    .map(|cron| (cron.tag.clone(), cron.last))
-                    .collect::<BTreeSet<(CronTagged, bool)>>()
-                    .into_iter()
-                    .collect()
-            }
+        let (cmd_tx, cmd_rx) = oneshot::channel();
+        drop(
+            self.cron_queue
+                .spawn_cron_job(CronJob::List(app_name.into(), tag, cmd_tx)),
+        );
+        // TODO(saibatizoku): deal with errors
+        if let Ok(resp) = cmd_rx.blocking_recv() {
+            resp
         } else {
             vec![]
         }
@@ -267,9 +170,15 @@ impl InternalState {
     ///
     /// - `true`: The requested crontab was deleted and will not trigger.
     /// - `false`: The requested crontab does not exist.
-    pub(crate) fn rm_crontab(&self, app_name: &str, entry: &CronTagged) -> bool {
-        if let Some(app_cron) = self.storage.get_mut(app_name) {
-            app_cron.remove(entry).is_some()
+    pub(crate) fn rm_crontab(&self, app_name: &str, entry: CronTagged) -> bool {
+        let (cmd_tx, cmd_rx) = oneshot::channel();
+        drop(
+            self.cron_queue
+                .spawn_cron_job(CronJob::Remove(app_name.into(), entry, cmd_tx)),
+        );
+        // TODO(saibatizoku): deal with errors
+        if let Ok(resp) = cmd_rx.blocking_recv() {
+            resp
         } else {
             false
         }
@@ -283,86 +192,92 @@ impl Hash for CronTagged {
     }
 }
 
-impl PartialEq for CronTagged {
-    fn eq(&self, other: &Self) -> bool {
-        self.tag == other.tag && self.when == other.when
-    }
-}
-
-impl Eq for CronTagged {}
-
-impl PartialOrd for CronTagged {
-    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-        Some(std::cmp::Ord::cmp(self, other))
-    }
-}
-
-#[allow(clippy::expect_used)]
-impl Ord for CronTagged {
-    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-        self.partial_cmp(other)
-            .expect("CronTagged should always be comparable")
-    }
-}
-
 #[cfg(test)]
+#[allow(dead_code)]
 mod tests {
     use super::*;
 
+    const APP_NAME: &str = "test";
+    const EVERY_MINUTE_WHEN: &str = "* * * * *";
+    const EVERY_DAY_WHEN: &str = "0 0 * * *";
+    const EVERY_MONTH_WHEN: &str = "0 0 1 * *";
+    const EXAMPLE_TAG: &str = "ExampleTag";
+    const OTHER_TAG: &str = "OtherTag";
+
+    // triggers every minute
+    fn crontab_example_1() -> CronTagged {
+        CronTagged {
+            when: EVERY_MINUTE_WHEN.into(),
+            tag: EXAMPLE_TAG.into(),
+        }
+    }
+    // triggers every minute
+    fn crontab_example_2() -> CronTagged {
+        CronTagged {
+            when: EVERY_MONTH_WHEN.into(),
+            tag: EXAMPLE_TAG.into(),
+        }
+    }
+    // triggers every minute
+    fn crontab_example_3() -> CronTagged {
+        CronTagged {
+            when: EVERY_DAY_WHEN.into(),
+            tag: EXAMPLE_TAG.into(),
+        }
+    }
+    // triggers every minute
+    fn crontab_other_1() -> CronTagged {
+        CronTagged {
+            when: EVERY_MINUTE_WHEN.into(),
+            tag: OTHER_TAG.into(),
+        }
+    }
+    const RETRIGGER_YES: bool = true;
+    const RETRIGGER_NO: bool = false;
+
     #[test]
-    #[allow(clippy::unwrap_used)]
     fn test_cron_state() {
-        let state = CRON_INTERNAL_STATE.lock().unwrap();
-        let app_name = "test";
+        let state = &CRON_INTERNAL_STATE;
         // Initial state for any AppName is always empty
-        assert!(state.ls_crontabs(app_name, None).is_empty());
+        assert!(state.ls_crontabs(APP_NAME, None).is_empty());
     }
 
     #[test]
-    #[allow(clippy::unwrap_used)]
-    fn test_cron_state_add_crontab() {
-        let state = CRON_INTERNAL_STATE.lock().unwrap();
-        let app_name = "test";
-        let every_minute_when = "* * * * *";
-        let every_day_when = "0 0 * * *";
-        let every_month_when = "0 0 1 * *";
-        let example_tag = "ExampleTag";
-        let other_tag = "OtherTag";
-        let crontab_example_1 = CronTagged {
-            // triggers every minute
-            when: every_minute_when.into(),
-            tag: example_tag.into(),
-        };
-        let crontab_example_2 = CronTagged {
-            // triggers every minute
-            when: every_month_when.into(),
-            tag: example_tag.into(),
-        };
-        let _crontab_example_3 = CronTagged {
-            // triggers every minute
-            when: every_day_when.into(),
-            tag: example_tag.into(),
-        };
-        let _crontab_other_1 = CronTagged {
-            // triggers every minute
-            when: every_minute_when.into(),
-            tag: other_tag.into(),
-        };
-        let retrigger_yes = true;
-        let retrigger_no = false;
+    fn test_cron_state_add_and_list_crontabs() {
         // Initial state for any AppName is always empty
-        assert!(state.ls_crontabs(app_name, None).is_empty());
-        assert!(state.add_crontab(app_name, crontab_example_1.clone(), retrigger_yes));
+
+        let state = &CRON_INTERNAL_STATE;
+        assert!(state.ls_crontabs(APP_NAME, None).is_empty());
+
+        // inserting returns true
+        assert!(state.add_crontab(APP_NAME, crontab_example_1(), RETRIGGER_YES));
         // re-inserting returns true
-        assert!(state.add_crontab(app_name, crontab_example_1.clone(), retrigger_no));
-        assert_eq!(state.ls_crontabs(app_name, None), vec![(
-            crontab_example_1.clone(),
-            retrigger_no
-        )]);
-        assert!(state.add_crontab(app_name, crontab_example_2.clone(), retrigger_yes));
-        assert_eq!(state.ls_crontabs(app_name, None), vec![
-            (crontab_example_1, retrigger_no),
-            (crontab_example_2, retrigger_yes),
+        assert!(state.add_crontab(APP_NAME, crontab_example_1(), RETRIGGER_NO));
+
+        assert_eq!(state.ls_crontabs(APP_NAME, None), vec![
+            (crontab_example_1(), RETRIGGER_YES),
+            (crontab_example_1(), RETRIGGER_NO),
+        ]);
+
+        assert!(state.add_crontab(APP_NAME, crontab_example_2(), RETRIGGER_YES));
+
+        assert!(state.add_crontab(APP_NAME, crontab_example_2(), RETRIGGER_YES));
+
+        assert_eq!(state.ls_crontabs(APP_NAME, None), vec![
+            (crontab_example_1(), RETRIGGER_YES),
+            (crontab_example_1(), RETRIGGER_NO),
+            (crontab_example_2(), RETRIGGER_YES),
+            (crontab_example_2(), RETRIGGER_YES),
+        ]);
+
+        assert!(state.add_crontab(APP_NAME, crontab_other_1(), RETRIGGER_YES));
+
+        assert_eq!(state.ls_crontabs(APP_NAME, None), vec![
+            (crontab_example_1(), RETRIGGER_YES),
+            (crontab_example_1(), RETRIGGER_NO),
+            (crontab_other_1(), RETRIGGER_YES),
+            (crontab_example_2(), RETRIGGER_YES),
+            (crontab_example_2(), RETRIGGER_YES),
         ]);
     }
 }
