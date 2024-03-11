30ef0253d0d7d1a21fa9a6ba652e834f6072654f
Apisit Ritreungroj <apisit_lon@hotmail.com>
2024-03-11 20:00:13 +0700

    refactor: remove testsuite for now

--- hermes/bin/tests/testsuite/main.rs
+++
@@ -1,5 +0,0 @@
-//! Import test modules here.
-//! Follows the example of a unified integration test hierarchy from:
-//!   [Cargo TestSuite](https://github.com/rust-lang/cargo/tree/master/tests/testsuite)
-
-mod example;
