37a075d6d08d32ea8211a792dcca67b5b5fca2b0
Joaquín Rosales <joaquin.rosales@iohk.io>
2024-03-11 13:23:07 -0600

    feat: cleanup localtime API

--- hermes/bin/src/runtime_extensions/hermes/localtime/host.rs
+++ hermes/bin/src/runtime_extensions/hermes/localtime/host.rs
@@ -1,12 +1,15 @@
 //! Localtime host implementation for WASM runtime.
 
+use chrono::{Local, TimeZone};
+use chrono_tz::Tz;
+
 use crate::{
     runtime_extensions::{
         bindings::{
             hermes::localtime::api::{Errno, Host, Localtime, Timezone},
             wasi::clocks::wall_clock::Datetime,
         },
-        hermes::localtime::{alt_localtime_impl, get_datetime_impl, get_localtime_impl},
+        hermes::localtime::get_tz,
     },
     state::HermesState,
 };
@@ -28,7 +31,21 @@ impl Host for HermesState {
     fn get_localtime(
         &mut self, when: Option<Datetime>, tz: Option<Timezone>,
     ) -> wasmtime::Result<Result<Localtime, Errno>> {
-        get_localtime_impl(when, tz)
+        let timezone = get_tz(tz)?;
+        let local_naive = match when {
+            Some(Datetime {
+                seconds,
+                nanoseconds,
+            }) => {
+                let seconds = seconds.try_into().map_err(|_| Errno::InvalidLocaltime)?;
+                let utc_dt = chrono::DateTime::from_timestamp(seconds, nanoseconds)
+                    .ok_or(Errno::InvalidLocaltime)?;
+                utc_dt.naive_utc()
+            },
+            None => Local::now().naive_utc(),
+        };
+        let local_date_time = timezone.from_utc_datetime(&local_naive);
+        Ok(local_date_time.try_into())
     }
 
     /// Get a new localtime from a localtime, by recalculating time for a new timezone.
@@ -46,7 +63,15 @@ impl Host for HermesState {
     fn alt_localtime(
         &mut self, time: Localtime, tz: Option<Timezone>,
     ) -> wasmtime::Result<Result<Localtime, Errno>> {
-        alt_localtime_impl(time, tz)
+        let local_date_time: chrono::DateTime<Tz> = time.try_into()?;
+        let alt_local_date_time = match tz {
+            Some(alt_tz) => {
+                let tz = get_tz(Some(alt_tz))?;
+                tz.from_utc_datetime(&local_date_time.naive_utc())
+            },
+            None => local_date_time,
+        };
+        Ok(alt_local_date_time.try_into())
     }
 
     /// Get a datetime from a localtime.
@@ -60,6 +85,6 @@ impl Host for HermesState {
     /// `datetime`  : the converted time.
     /// `errno`     : An error indicating why conversion failed.
     fn get_datetime(&mut self, time: Localtime) -> wasmtime::Result<Result<Datetime, Errno>> {
-        get_datetime_impl(time)
+        Ok(time.try_into())
     }
 }
--- hermes/bin/src/runtime_extensions/hermes/localtime/mod.rs
+++ hermes/bin/src/runtime_extensions/hermes/localtime/mod.rs
@@ -1,7 +1,7 @@
 //! Localtime runtime extension implementation.
 
-use chrono::{Datelike, Local, LocalResult, NaiveDate, TimeZone, Timelike};
-use chrono_tz::Tz;
+use chrono::{Datelike, NaiveDate, TimeZone, Timelike};
+use chrono_tz::{OffsetName, Tz};
 
 use crate::runtime_extensions::{
     bindings::{
@@ -22,134 +22,53 @@ impl Stateful for State {
     }
 }
 
-/// Get localtime from a datetime or now.
+/// Get `Tz` from an optional `Timezone`.
 ///
-/// **Parameters**
-/// * `when`: `Option<Datetime>`.
-/// * `tz`: `Option<Timezone>`.
+/// If present, `tz` is parsed into `Tz`, otherwise the system timezone is
+/// returned.
 ///
-/// **Returns**
-/// `wasmtime::Result<Result<Localtime, Errno>>`.
-pub(crate) fn get_localtime_impl(
-    when: Option<Datetime>, tz: Option<Timezone>,
-) -> wasmtime::Result<Result<Localtime, Errno>> {
-    let date_time = get_dt(when)?;
-    let timezone = get_tz(tz)?;
-    let dt = date_time.with_timezone(&timezone);
-    let localtime = offset_datetime_to_localtime(dt, timezone.name());
-    Ok(localtime)
-}
-
-/// Get alt localtime from a datetime or now.
-///
-/// **Parameters**
-/// * `time`: `Localtime`.
-/// * `tz`: `Option<Timezone>`.
-///
-/// **Returns**
-/// `wasmtime::Result<Result<Localtime, Errno>>`.
-pub(crate) fn alt_localtime_impl(
-    time: Localtime, tz: Option<Timezone>,
-) -> wasmtime::Result<Result<Localtime, Errno>> {
-    let alt_tz = get_tz(tz)?;
-    let date_time: chrono::DateTime<Tz> = time.try_into()?;
-    let dt = date_time.with_timezone(&alt_tz);
-    let alt_time = offset_datetime_to_localtime(dt, alt_tz.name());
-    Ok(alt_time)
-}
-
-/// Get datetime from a localtime.
-///
-/// **Parameters**
-/// * `time`: `Localtime`.
-///
-/// **Returns**
-/// `wasmtime::Result<Result<Datetime, Errno>>`.
-pub(crate) fn get_datetime_impl(time: Localtime) -> wasmtime::Result<Result<Datetime, Errno>> {
-    let dt: chrono::DateTime<Tz> = time.try_into()?;
-    Ok(dt.try_into())
-}
-
-/// Get `chrono::DateTime<Utc>` from a `Datetime` or now.
-///
-/// **Parameters**
-/// * `when`: `Option<Datetime>`.
-///
-/// **Returns**
-/// `wasmtime::Result<Result<chrono::DateTime<Utc>, Errno>>`.
-fn get_dt(dt: Option<Datetime>) -> Result<chrono::DateTime<Tz>, Errno> {
-    match dt {
-        Some(Datetime {
-            seconds,
-            nanoseconds,
-        }) => {
-            let seconds = seconds.try_into().map_err(|_| Errno::InvalidLocaltime)?;
-            let utc_dt = chrono::DateTime::from_timestamp(seconds, nanoseconds)
-                .ok_or(Errno::InvalidLocaltime)?;
-            Ok(Tz::UTC.from_utc_datetime(&utc_dt.naive_utc()))
-        },
-        None => {
-            if let LocalResult::Single(dt) =
-                Tz::UTC.from_local_datetime(&Local::now().naive_local())
-            {
-                Ok(dt)
-            } else {
-                Err(Errno::InvalidLocaltime)
-            }
-        },
-    }
-}
-
-/// Get `Tz` from a `Timezone`.
+/// This function returns `Errno::UnknownTimezone` if `tz` cannot be parsed.
 ///
 /// **Parameters**
 /// * `tz`: `Option<Timezone>`.
 ///
 /// **Returns**
 /// `wasmtime::Result<Result<Tz, Errno>>`.
-fn get_tz(tz: Option<Timezone>) -> Result<Tz, Errno> {
-    let timezone = if let Some(tz_str) = tz {
-        tz_str.parse().map_err(|_| Errno::UnknownTimezone)?
-    } else {
-        let dt = Tz::UTC.from_utc_datetime(&Local::now().naive_local());
-        dt.timezone()
+pub(crate) fn get_tz(tz: Option<Timezone>) -> Result<Tz, Errno> {
+    let timezone_str = match tz {
+        Some(tz_str) => tz_str,
+        None => iana_time_zone::get_timezone().map_err(|_| Errno::UnknownTimezone)?,
     };
-    Ok(timezone)
+    timezone_str.parse().map_err(|_| Errno::UnknownTimezone)
 }
 
-/// Convert `chrono::DateTime<Utc>` and timezone name,  to `Localtime`.
-///
-/// **Parameters**
-/// * `dt`: `chrono::DateTime<Utc>`.
-/// * `tz_name`: `&str`.
-///
-/// **Returns**
-/// `wasmtime::Result<Result<Localtime, Errno>>`.
-fn offset_datetime_to_localtime(
-    dt: chrono::DateTime<Tz>, tz_name: &str,
-) -> Result<Localtime, Errno> {
-    let localtime = Localtime {
-        year: dt.year().try_into().map_err(|_| Errno::YearOutOfRange)?,
-        month: dt.month().try_into().map_err(|_| Errno::InvalidLocaltime)?,
-        dow: dt
-            .weekday()
-            .number_from_monday()
-            .try_into()
-            .map_err(|_| Errno::YearOutOfRange)?,
-        day: dt.day().try_into().map_err(|_| Errno::InvalidLocaltime)?,
-        hh: dt.hour().try_into().map_err(|_| Errno::InvalidLocaltime)?,
-        mm: dt
-            .minute()
-            .try_into()
-            .map_err(|_| Errno::InvalidLocaltime)?,
-        ss: dt
-            .second()
-            .try_into()
-            .map_err(|_| Errno::InvalidLocaltime)?,
-        ns: dt.nanosecond(),
-        tz: tz_name.to_string(),
-    };
-    Ok(localtime)
+impl TryFrom<chrono::DateTime<Tz>> for Localtime {
+    type Error = Errno;
+
+    fn try_from(dt: chrono::DateTime<Tz>) -> Result<Self, Self::Error> {
+        let localtime = Localtime {
+            year: dt.year().try_into().map_err(|_| Errno::YearOutOfRange)?,
+            month: dt.month().try_into().map_err(|_| Errno::InvalidLocaltime)?,
+            dow: dt
+                .weekday()
+                .number_from_monday()
+                .try_into()
+                .map_err(|_| Errno::YearOutOfRange)?,
+            day: dt.day().try_into().map_err(|_| Errno::InvalidLocaltime)?,
+            hh: dt.hour().try_into().map_err(|_| Errno::InvalidLocaltime)?,
+            mm: dt
+                .minute()
+                .try_into()
+                .map_err(|_| Errno::InvalidLocaltime)?,
+            ss: dt
+                .second()
+                .try_into()
+                .map_err(|_| Errno::InvalidLocaltime)?,
+            ns: dt.nanosecond(),
+            tz: dt.offset().tz_id().to_string(),
+        };
+        Ok(localtime)
+    }
 }
 
 impl TryInto<chrono::DateTime<Tz>> for Localtime {
@@ -197,5 +116,14 @@ impl TryFrom<chrono::DateTime<Tz>> for Datetime {
             seconds,
             nanoseconds,
         })
+    }
+}
+
+impl TryFrom<Localtime> for Datetime {
+    type Error = Errno;
+
+    fn try_from(value: Localtime) -> Result<Self, Self::Error> {
+        let dt: chrono::DateTime<Tz> = value.try_into()?;
+        dt.try_into()
     }
 }
