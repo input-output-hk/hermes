b4a89a0ac2ac47fe0c07f4d0bab4ce60d8695608
bkioshn <bkioshn@gmail.com>
2024-03-14 18:30:04 +0700

    feat: add set state

--- hermes/bin/src/runtime_extensions/hermes/crypto/mod.rs
+++ hermes/bin/src/runtime_extensions/hermes/crypto/mod.rs
@@ -1,6 +1,13 @@
 //! Crypto runtime extension implementation.
 
+use self::state::set_state;
+
 mod host;
+mod bip32_ed25519;
+mod bip39;
+mod state;
 
 /// Advise Runtime Extensions of a new context
-pub(crate) fn new_context(_ctx: &crate::runtime_context::HermesRuntimeContext) {}
+pub(crate) fn new_context(ctx: &crate::runtime_context::HermesRuntimeContext) {
+    set_state(ctx.app_name().clone(), ctx.module_id().clone(),ctx.event_name(), ctx.exc_counter());
+}
