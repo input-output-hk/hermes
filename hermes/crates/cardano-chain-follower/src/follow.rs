dec436eacf5d0b145c59d9546594fc233c861f44
Felipe Rosa <felipe.rosa@iohk.io>
2024-03-14 17:10:57 -0300

    chore(cardano-chain-follower): fix fmt

--- hermes/crates/cardano-chain-follower/src/follow.rs
+++ hermes/crates/cardano-chain-follower/src/follow.rs
@@ -72,9 +72,7 @@ impl FollowerConfigBuilder {
     /// * `from`: Sync starting point.
     #[must_use]
     pub fn follow_from<P>(mut self, from: P) -> Self
-    where
-        P: Into<PointOrTip>,
-    {
+    where P: Into<PointOrTip> {
         self.follow_from = from.into();
         self
     }
@@ -280,9 +278,7 @@ impl Follower {
     ///
     /// Returns Err if something went wrong while communicating with the producer.
     pub async fn set_read_pointer<P>(&self, at: P) -> Result<Option<Point>>
-    where
-        P: Into<PointOrTip>,
-    {
+    where P: Into<PointOrTip> {
         let (response_tx, response_rx) = oneshot::channel();
 
         let req = task::SetReadPointerRequest {
@@ -305,9 +301,7 @@ impl Follower {
     /// * `at`: Point at which to read the block.
     #[must_use]
     pub fn read_block<P>(&self, at: P) -> ReadBlock
-    where
-        P: Into<PointOrTip>,
-    {
+    where P: Into<PointOrTip> {
         ReadBlock(
             at.into(),
             self.client_connect_info.clone(),
@@ -323,9 +317,7 @@ impl Follower {
     /// * `to`: Block range end.
     #[must_use]
     pub fn read_block_range<P>(&self, from: Point, to: P) -> ReadBlockRange
-    where
-        P: Into<PointOrTip>,
-    {
+    where P: Into<PointOrTip> {
         ReadBlockRange(
             from,
             to.into(),
@@ -841,24 +833,30 @@ mod task {
 /// Sets the N2N remote client's read pointer.
 async fn set_client_read_pointer(client: &mut PeerClient, at: PointOrTip) -> Result<Option<Point>> {
     match at {
-        PointOrTip::Point(Point::Origin) => client
-            .chainsync()
-            .intersect_origin()
-            .await
-            .map(Some)
-            .map_err(Error::Chainsync),
-        PointOrTip::Point(p @ Point::Specific(..)) => client
-            .chainsync()
-            .find_intersect(vec![p])
-            .await
-            .map(|(point, _)| point)
-            .map_err(Error::Chainsync),
-        PointOrTip::Tip => client
-            .chainsync()
-            .intersect_tip()
-            .await
-            .map(Some)
-            .map_err(Error::Chainsync),
+        PointOrTip::Point(Point::Origin) => {
+            client
+                .chainsync()
+                .intersect_origin()
+                .await
+                .map(Some)
+                .map_err(Error::Chainsync)
+        },
+        PointOrTip::Point(p @ Point::Specific(..)) => {
+            client
+                .chainsync()
+                .find_intersect(vec![p])
+                .await
+                .map(|(point, _)| point)
+                .map_err(Error::Chainsync)
+        },
+        PointOrTip::Tip => {
+            client
+                .chainsync()
+                .intersect_tip()
+                .await
+                .map(Some)
+                .map_err(Error::Chainsync)
+        },
     }
 }
 
