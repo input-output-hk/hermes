3a82d46bf94f6a53915c55f0565af5b1d41d88a9
bkioshn <35752733+bkioshn@users.noreply.github.com>
2024-03-19 16:13:01 +0700

    feat(api): Implement Ed25519-BIP32 (#127)
    * feat: add ed25519-bip32 crate
    
    * fix: change key type to bstr
    
    * feat: impl new for Ed25519Bip32
    
    * feat: implement state and resource holder
    
    * chore: calling crypto state
    
    * feat: implement get pub key
    
    * fix: state
    
    * feat: implement sign data
    
    * feat: implement check signature
    
    * feat: implement resource drop crypto
    
    * feat: implement derive
    
    * fix: remove unnecessary struct
    
    * fix: syntax cleanup
    
    * fix: cspell
    
    * fix: format
    
    * chore: add dashmap crate
    
    * fix: change to type with fix size
    
    * fix: change signature length to 512
    
    * fix: restructure crypto resource and states
    
    * fix: crypto api implementation
    
    * feat: add once_cell crate
    
    * fix: extract state to its own mod
    
    * fix: remove generic from impl
    
    * fix: crypto state implementation
    
    * fix: crypto state
    
    * fix: crypto state
    
    * fix: counter and map in crypto state
    
    * fix: use XPrv from lib
    
    * fix: create wrap Xprv
    
    * fix: crypto state
    
    * fix: remove FIXME and fix From impl
    
    * fix: add thread safe test and comments
    
    * chore: add println for esier debug
    
    * chore: comment out old functionality
    
    * fix: add resource and test
    
    * feat: add bip39 lib
    
    * feat: add bip32-ed25519 implementation
    
    * fix: constructor to accept bip39
    
    * fix: mnemonic to xprv
    
    * fix: change mnemonic and passphrase to list
    
    * feat: add rand and rand_core lib
    
    * feat: start implement generating mnemonic
    
    * feat: implement mnemonic generating with prefix
    
    * chore: add bip39 support lang
    
    * feat: add crypto error
    
    * fix: add mnemonic to priv test and refactor
    
    * fix: move bip39 to its own module + write doc and comment
    
    * chore: fix func name and add comment
    
    * feat: add bip32-ed25519 implementation
    
    * fix: bip32-ed25519 name + add path to derive
    
    * feat: add bip32 crate
    
    * feat: implement test
    
    * fix: host crypto
    
    * fix: test vector
    
    * fix: add doc
    
    * feat: implement derive, sign data, check sig, and pubkey
    
    * fix: state type
    
    * fix: wit file function and add err
    
    * fix: bip39 type
    
    * fix: bip32_ed25519 type
    
    * fix: host implementation
    
    * chore: fix type and comment
    
    * feat: add delete resource
    
    * fix: error handling
    
    * fix: import
    
    * feat: implement drop resource
    
    * chore: cleanup bip32-ed25519
    
    * fix: remove unused crate
    
    * fix: bip39 doc
    
    * fix: add error crypto wit file
    
    * fix: syntax in bip32-ed25519
    
    * fix: bip39 syntax
    
    * fix: state syntax
    
    * fix: host syntax
    
    * fix: add error
    
    * feat: add set state
    
    * fix: spelling
    
    * fix: nightly format
    
    * fix: spelling
    
    * fix: try no cache
    
    * fix: delete no cache
    
    * fix: add CC0-1 license to deny toml
    
    * fix: implement new state, app -> resource
    
    * fix: bip39 gen mnemonic implementation
    
    * fix: move gen mnemonic and fix constructor
    
    * fix: bip39 types
    
    * fix: crypto host
    
    * fix: remove errno
    
    * fix: add state check
    
    * fix: todo comment
    
    * fix: add project dict
    
    * fix: typo
    
    * fix: formatter
    
    * fix: update rust ci version
    
    * fix: try without cache
    
    * fix: syntax
    
    * fix: remove no cache
    
    * fix: rust ci
    
    * fix: add license
    
    * fix: bip39 jap test
    
    * fix: unicode not nfc problem
    
    * fix: rust base version
    
    * chore: cleanup
    
    * fix: pr comment
    
    * fix: spelling
    
    ---------
    
    Co-authored-by: Steven Johnson <stevenj@users.noreply.github.com>

---
+++ hermes/bin/src/runtime_extensions/hermes/crypto/bip39.rs
@@ -0,0 +1,403 @@
+//! Implementation of BIP39.
+
+use std::vec;
+
+use bip39::{Language, Mnemonic};
+use cryptoxide::{
+    digest::Digest,
+    hmac::Hmac,
+    pbkdf2::pbkdf2,
+    sha2::{Sha256, Sha512},
+};
+use ed25519_bip32::XPrv;
+use rand::RngCore;
+
+use crate::runtime_extensions::bindings::hermes::crypto::api::Errno;
+
+/// Generate a new extended private key (`XPrv`) from a mnemonic and passphrase.
+///
+/// # Arguments
+///
+/// - `mnemonic`: A string representing the mnemonic.
+/// - `passphrase`: An optional string representing the passphrase.
+///
+/// # Returns
+///
+/// Returns the `XPrv` extended private key as a `Result`.
+/// If the conversion is successful, it returns `Ok` with the extended private key
+/// (`XPrv`).
+///
+/// # Errors
+///
+/// - `InvalidMnemonic`: If the mnemonic should be either 12, 15, 18, 21, or 24.
+pub(crate) fn mnemonic_to_xprv(mnemonic: &str, passphrase: &str) -> Result<XPrv, Errno> {
+    /// 4096 is the number of iterations for PBKDF2.
+    const ITER: u32 = 4096;
+
+    // Parse will detect language and check mnemonic valid length
+    // 12, 15, 18, 21, 24 are valid mnemonic length
+    let Ok(mnemonic) = Mnemonic::parse(mnemonic) else {
+        return Err(Errno::InvalidMnemonic);
+    };
+
+    let entropy = mnemonic.to_entropy();
+
+    // This implementation follows SLIP-0023 - Cardano HD master node derivation
+    // from a master seed.
+    // https://github.com/satoshilabs/slips/blob/master/slip-0023.md
+    let mut pbkdf2_result = [0; 96];
+    let passphrase_byte: &[u8] = passphrase.as_bytes();
+    let mut mac = Hmac::new(Sha512::new(), passphrase_byte);
+    pbkdf2(&mut mac, &entropy, ITER, &mut pbkdf2_result);
+
+    Ok(XPrv::normalize_bytes_force3rd(pbkdf2_result))
+}
+
+/// Generate a new mnemonic.
+/// Number of entropy required is calculated by `word_count` / 3 * 4.
+/// eg. 24 words will have 32 entropy or 256 bits entropy.
+/// Number of checksum required is calculated by `word_count` / 3 * 4 * 8 / 32.
+/// eg. 24 words will have 8 checksum bits.
+///
+/// # Arguments
+///
+/// - `word_count`: The number of words in the mnemonic. Must be a multiple of
+/// 3 and in the range of 12 - 24.
+/// - `prefix`: A vector of strings representing the prefix, empty if no prefix.
+/// - `language`: The language of the mnemonic.
+///
+/// # Returns
+///
+/// Returns the mnemonic of type `String` as a `Result`.
+/// If the conversion is successful, it returns `Ok` with the mnemonic.
+/// If there is an error during the computation, it returns `Err` with an `Errno`.
+///
+/// # Errors
+///
+/// - `InvalidMnemonicLength`: If the word count is not a multiple of 3 or not in the
+///   range of 12 - 24.
+/// - `PrefixTooLong`: If the prefix is longer than the maximum allowed length, max is 3.
+/// - `WordNotFound`: If a word in the mnemonic is not found in the word list.
+pub(crate) fn generate_new_mnemonic(
+    word_count: usize, prefix: Vec<String>, language: Option<String>,
+) -> Result<Vec<String>, Errno> {
+    // Check word count
+    if is_invalid_word_count(word_count) {
+        return Err(Errno::InvalidMnemonicLength);
+    }
+
+    // Number of prefix word should be <= 3.
+    if prefix.len() > 3 {
+        return Err(Errno::PrefixTooLong);
+    }
+
+    let language = language.map_or_else(
+        || string_to_language("English"),
+        |lang| string_to_language(&lang),
+    )?;
+
+    let prefix_index_bits = match get_prefix_index_bits(prefix, language) {
+        Ok(prefix_index_bits) => prefix_index_bits,
+        Err(e) => return Err(e),
+    };
+
+    // Create an vec that will hold binary conversion of entropy.
+    let mut bits_entropy = Vec::new();
+    // Add the prefix index bit to the bit entropy.
+    bits_entropy.extend_from_slice(&prefix_index_bits);
+
+    let entropy = match generate_entropy(word_count) {
+        Ok(entropy) => entropy,
+        Err(e) => return Err(e),
+    };
+
+    // Convert bytes entropy to bits.
+    byte_to_bit(entropy, &mut bits_entropy, word_count);
+
+    let check_sum_bits = get_check_sum_bits(&bits_entropy, word_count);
+    // Add the checksum bits to the end of bit entropy.
+    bits_entropy.extend_from_slice(&check_sum_bits);
+
+    let word_indices = get_word_indices(&bits_entropy, word_count);
+    let mnemonic_list = get_mnemonic_from_indices(word_indices, language);
+
+    Ok(mnemonic_list)
+}
+
+/// Check if the word count is invalid.
+/// Valid word count is a multiple of 3 and in the range of 12 - 24.
+/// Returns true if the word count is invalid, otherwise false.
+fn is_invalid_word_count(word_count: usize) -> bool {
+    word_count < 12 || word_count % 3 != 0 || word_count > 24
+}
+
+/// Get the index bits of the prefix words from a BIP39 dictionary.
+fn get_prefix_index_bits(prefix_list: Vec<String>, language: Language) -> Result<Vec<u8>, Errno> {
+    let mut prefix_index: Vec<u8> = Vec::new();
+    for word in prefix_list {
+        match language.find_word(&word) {
+            Some(index) => {
+                for b in decimal_to_binary_array(index) {
+                    prefix_index.push(b);
+                }
+            },
+            None => return Err(Errno::WordNotFound),
+        }
+    }
+    Ok(prefix_index)
+}
+
+/// Generate entropies and return the value.
+fn generate_entropy(word_count: usize) -> Result<Vec<u8>, Errno> {
+    // Number of entropy calculate from mnemonic word.
+    let entropy_num = (word_count / 3) * 4;
+    // Maximum length of mnemonic is 24 words which is 32 entropy.
+    let mut entropy_num_max = [0u8; 32];
+    // Random number
+    let mut rng = rand::thread_rng();
+    // Fill the random number into entropy_num.
+    if let Some(slice) = entropy_num_max.get_mut(0..entropy_num) {
+        rng.fill_bytes(slice);
+    }
+
+    if let Some(slice) = entropy_num_max.get(0..entropy_num) {
+        Ok(slice.to_vec())
+    } else {
+        Err(Errno::GenerateEntropyFailed)
+    }
+}
+
+/// Generate checksum bits from entropy bits.
+fn get_check_sum_bits(entropy_bits: &[u8], word_count: usize) -> Vec<u8> {
+    // Number of checksum bits to be added.
+    let check_sum_num = word_count / 3 * 4 * 8 / 32;
+    let mut hash_result = [0u8; 32];
+    let mut hasher = Sha256::new();
+    // Convert bits_entropy to bytes, so it works with SHA256 hasher.
+    let bytes_entropy = bits_to_bytes(entropy_bits);
+    hasher.input(&bytes_entropy);
+
+    hasher.result(&mut hash_result);
+
+    // Retrieve the first `check_sum_num` check sum bits from the hash result.
+    let mut check_sum_bits = Vec::new();
+    for i in 0..check_sum_num {
+        check_sum_bits.push(hash_result[0] >> (7 - i) & 1);
+    }
+    check_sum_bits
+}
+
+/// Get the word indices from the entropy bits.
+fn get_word_indices(bits_entropy: &[u8], word_count: usize) -> Vec<u16> {
+    let mut word_index_vec = Vec::new();
+
+    // Separate entropy bits into 11 bits and convert to decimal.
+    // This decimal will be used to get the word index.
+    for i in 0..word_count {
+        let mut idx = 0;
+        for j in 0..11 {
+            if let Some(value) = bits_entropy.get(i * 11 + j) {
+                if *value > 0 {
+                    idx += 1 << (10 - j);
+                }
+            }
+        }
+        word_index_vec.push(idx);
+    }
+    word_index_vec
+}
+
+/// Get the mnemonic from the BIP39 dictionary using word indices.
+fn get_mnemonic_from_indices(word_index_vec: Vec<u16>, language: Language) -> Vec<String> {
+    let word_list = language.word_list();
+    let mut mnemonic: Vec<String> = vec![];
+    for word in word_index_vec {
+        if let Some(word) = word_list.get(word as usize) {
+            mnemonic.push((*(word)).to_string());
+        }
+    }
+    mnemonic
+}
+
+/// Turns decimal into binary array of length 11.
+fn decimal_to_binary_array(decimal: u16) -> [u8; 11] {
+    let mut binary = [0u8; 11];
+    let mut n = decimal;
+    let mut index = 0;
+
+    while n > 0 {
+        if let Some(value) = binary.get_mut(index) {
+            let bit = n % 2;
+            index += 1;
+            *value = bit as u8;
+            n /= 2;
+        }
+    }
+
+    // If the number of bits is less than 11, fill the remaining bits with 0.
+    while index < 11 {
+        if let Some(value) = binary.get_mut(index) {
+            *value = 0;
+        }
+        index += 1;
+    }
+    binary.reverse();
+    binary
+}
+
+/// Turns bit of array to bytes of arrays
+fn bits_to_bytes(bits: &[u8]) -> Vec<u8> {
+    bits.chunks(8)
+        .map(|chunk| {
+            chunk
+                .iter()
+                .enumerate()
+                .fold(0, |acc, (i, &bit)| acc | ((bit) << (7 - i)))
+        })
+        .collect()
+}
+
+/// Convert string to BIP39 language.
+fn string_to_language(s: &str) -> Result<Language, Errno> {
+    match s {
+        "English" => Ok(Language::English),
+        "SimplifiedChinese" => Ok(Language::SimplifiedChinese),
+        "TraditionalChinese" => Ok(Language::TraditionalChinese),
+        "Czech" => Ok(Language::Czech),
+        "French" => Ok(Language::French),
+        "Italian" => Ok(Language::Italian),
+        "Japanese" => Ok(Language::Japanese),
+        "Korean" => Ok(Language::Korean),
+        "Spanish" => Ok(Language::Spanish),
+        _ => Err(Errno::UnsupportedLanguage),
+    }
+}
+
+/// Convert bytes entropy to bits.
+fn byte_to_bit(entropy: Vec<u8>, bits_entropy: &mut Vec<u8>, word_count: usize) {
+    for byte in entropy {
+        for j in (0..8).rev() {
+            // Should not exceed the word_count / 3 * 4 * 8
+            // which is number of entropy bits for the mnemonic word count.
+            if bits_entropy.len() >= word_count / 3 * 4 * 8 {
+                break;
+            }
+            bits_entropy.push((byte >> j) & 1);
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests_bip39 {
+    use super::*;
+
+    // English test vector is from https://cips.cardano.org/cip/CIP-0011
+    // Entropy can be checked from https://iancoleman.io/bip39
+    const MNEMONIC_ENG: &str = "prevent company field green slot measure chief hero apple task eagle sunset endorse dress seed";
+    // Japanese test is from https://github.com/rust-bitcoin/rust-bip39/blob/master/src/lib.rs
+    const MNEMONIC_JAPANESE: &str = "こころ いどう きあつ そうがんきょう へいあん せつりつ ごうせい はいち  いびき きこく あんい おちつく きこえる けんとう たいこ すすめる はっけん ていど はんおん いんさつ うなぎ しねま れいぼう みつかる";
+
+    #[test]
+    fn test_eng_mnemonic_to_xprv() {
+        // Valid mnemonic, shouldn't fail.
+        mnemonic_to_xprv(MNEMONIC_ENG, "").expect("Failed to convert English mnemonic to xprv");
+    }
+
+    #[test]
+    fn test_jap_mnemonic_to_xprv() {
+        // Valid mnemonic, shouldn't fail.
+        mnemonic_to_xprv(MNEMONIC_JAPANESE, "")
+            .expect("Failed to convert Japanese mnemonic to xprv");
+    }
+
+    #[test]
+    fn test_mnemonic_with_passphrase_to_xprv() {
+        let passphrase = "test cat";
+        // Valid mnemonic with passphrase, shouldn't fail.
+        mnemonic_to_xprv(MNEMONIC_ENG, passphrase)
+            .expect("Failed to convert English mnemonic with passphrase to xprv");
+    }
+
+    #[test]
+    fn test_mnemonic_to_xprv_invalid_length() {
+        let mnemonic = "project cat test";
+        mnemonic_to_xprv(mnemonic, "").expect_err(&bip39::Error::BadWordCount(2).to_string());
+    }
+
+    #[test]
+    fn test_generate_mnemonic_prefix() {
+        let mnemonic = generate_new_mnemonic(12, vec![], Some("English".to_string()))
+            .expect("Failed to generate mnemonic");
+        Mnemonic::parse(mnemonic.join(" ")).expect("Fail to parse mnemonic");
+        let mnemonic =
+            generate_new_mnemonic(12, vec!["project".to_string()], Some("English".to_string()))
+                .expect("Failed to generate mnemonic");
+        Mnemonic::parse(mnemonic.join(" ")).expect("Fail to parse mnemonic");
+        let mnemonic = generate_new_mnemonic(
+            12,
+            vec!["project".to_string(), "cat".to_string()],
+            Some("English".to_string()),
+        )
+        .expect("Failed to generate mnemonic");
+        Mnemonic::parse(mnemonic.join(" ")).expect("Fail to parse mnemonic");
+        let mnemonic = generate_new_mnemonic(
+            12,
+            vec!["project".to_string(), "cat".to_string(), "test".to_string()],
+            Some("English".to_string()),
+        )
+        .expect("Failed to generate mnemonic");
+        Mnemonic::parse(mnemonic.join(" ")).expect("Fail to parse mnemonic");
+        let mnemonic = generate_new_mnemonic(
+            12,
+            vec!["project".to_string(), "cat".to_string(), "test".to_string()],
+            None,
+        )
+        .expect("Failed to generate mnemonic");
+        Mnemonic::parse(mnemonic.join(" ")).expect("Fail to parse mnemonic");
+    }
+    #[test]
+    // Disable unicode error for linter
+    #[allow(clippy::unicode_not_nfc)]
+    fn test_generate_mnemonic_prefix_japanese() {
+        let mnemonic = generate_new_mnemonic(
+            12,
+            vec!["たいみんぐ".to_string(), "うけたまわる".to_string()],
+            Some("Japanese".to_string()),
+        )
+        .expect("Failed to generate mnemonic");
+        Mnemonic::parse(mnemonic.join(" ")).expect("Fail to parse mnemonic");
+    }
+
+    #[test]
+    fn test_generate_mnemonic_validity() {
+        for _ in 0..20 {
+            let prefix = vec!["project".to_string(), "cat".to_string()];
+            let mnemonic = generate_new_mnemonic(12, prefix, Some("English".to_string()))
+                .expect("Failed to generate mnemonic");
+            Mnemonic::parse(mnemonic.join(" ")).expect("Fail to parse mnemonic");
+        }
+    }
+
+    #[test]
+    fn test_generate_mnemonic_with_prefix_too_long() {
+        let prefix = vec![
+            "project".to_string(),
+            "cat".to_string(),
+            "test".to_string(),
+            "long".to_string(),
+        ];
+        generate_new_mnemonic(12, prefix, Some("English".to_string()))
+            .expect_err(&format!("{:?}", Errno::PrefixTooLong));
+    }
+
+    #[test]
+    fn test_generate_mnemonic_invalid_length() {
+        generate_new_mnemonic(3, vec![], Some("English".to_string()))
+            .expect_err(&format!("{:?}", Errno::InvalidMnemonicLength));
+    }
+    #[test]
+    fn test_generate_mnemonic_prefix_word_not_found() {
+        generate_new_mnemonic(12, vec!["abc".to_string()], Some("English".to_string()))
+            .expect_err(&format!("{:?}", Errno::WordNotFound));
+    }
+}
--- hermes/bin/src/runtime_extensions/hermes/crypto/host.rs
+++ hermes/bin/src/runtime_extensions/hermes/crypto/host.rs
@@ -1,33 +1,65 @@
 //! Crypto host implementation for WASM runtime.
 
+use wasmtime::component::Resource;
+
+use super::bip32_ed25519::check_signature;
 use crate::{
     runtime_context::HermesRuntimeContext,
-    runtime_extensions::bindings::hermes::{
-        binary::api::Bstr,
-        crypto::api::{
-            Ed25519Bip32, Ed25519Bip32PrivateKey, Ed25519Bip32PublicKey, Ed25519Bip32Signature,
-            Host, HostEd25519Bip32,
+    runtime_extensions::{
+        bindings::hermes::{
+            binary::api::Bstr,
+            crypto::api::{
+                Bip32Ed25519, Bip32Ed25519PublicKey, Bip32Ed25519Signature, Errno, Host,
+                HostBip32Ed25519, MnemonicPhrase, Passphrase, Path,
+            },
+        },
+        hermes::crypto::{
+            bip32_ed25519::{derive_new_private_key, get_public_key, sign_data},
+            bip39::{generate_new_mnemonic, mnemonic_to_xprv},
+            state::{add_resource, delete_resource, get_resource},
         },
     },
 };
 
-impl HostEd25519Bip32 for HermesRuntimeContext {
+impl HostBip32Ed25519 for HermesRuntimeContext {
     /// Create a new ED25519-BIP32 Crypto resource
     ///
     /// **Parameters**
     ///
-    /// - `private_key` : The key to use, if not supplied one is RANDOMLY generated.
+    /// - `mnemonic-phrase` : BIP39 mnemonic.
+    /// - `passphrase` : Optional BIP39 passphrase.
     fn new(
-        &mut self, _private_key: Option<Ed25519Bip32PrivateKey>,
-    ) -> wasmtime::Result<wasmtime::component::Resource<Ed25519Bip32>> {
-        todo!()
+        &mut self, mnemonic: MnemonicPhrase, passphrase: Option<Passphrase>,
+    ) -> wasmtime::Result<wasmtime::component::Resource<Bip32Ed25519>> {
+        let passphrase = passphrase.unwrap_or_default();
+        let xprv = mnemonic_to_xprv(&mnemonic.join(" "), &passphrase.join(" "));
+        match xprv {
+            Ok(xprv) => {
+                if let Some(id) = add_resource(self.app_name(), xprv) {
+                    return Ok(Resource::new_own(id));
+                }
+            },
+            Err(e) => return Err(wasmtime::Error::msg(e.to_string())),
+        };
+        // TODO(bkioshn): https://github.com/input-output-hk/hermes/issues/183
+        Err(wasmtime::Error::msg("Error creating new resource"))
     }
 
     /// Get the public key for this private key.
     fn public_key(
-        &mut self, _resource: wasmtime::component::Resource<Ed25519Bip32>,
-    ) -> wasmtime::Result<Ed25519Bip32PublicKey> {
-        todo!()
+        &mut self, resource: wasmtime::component::Resource<Bip32Ed25519>,
+    ) -> wasmtime::Result<Bip32Ed25519PublicKey> {
+        let private_key = get_resource(self.app_name(), resource.rep());
+        match private_key {
+            Some(private_key) => {
+                let public_key = get_public_key(&private_key);
+                Ok(public_key)
+            },
+            None => {
+                // TODO(bkioshn): https://github.com/input-output-hk/hermes/issues/183
+                Ok((0, 0, 0, 0))
+            },
+        }
     }
 
     /// Sign data with the Private key, and return it.
@@ -36,9 +68,17 @@ impl HostEd25519Bip32 for HermesRuntimeContext {
     ///
     /// - `data` : The data to sign.
     fn sign_data(
-        &mut self, _resource: wasmtime::component::Resource<Ed25519Bip32>, _data: Bstr,
-    ) -> wasmtime::Result<Ed25519Bip32Signature> {
-        todo!()
+        &mut self, resource: wasmtime::component::Resource<Bip32Ed25519>, data: Bstr,
+    ) -> wasmtime::Result<Bip32Ed25519Signature> {
+        let private_key = get_resource(self.app_name(), resource.rep());
+        match private_key {
+            Some(private_key) => {
+                let sig = sign_data(&private_key, &data);
+                Ok(sig)
+            },
+            // TODO(bkioshn): https://github.com/input-output-hk/hermes/issues/183
+            None => Ok((0, 0, 0, 0, 0, 0, 0, 0)),
+        }
     }
 
     /// Check a signature on a set of data.
@@ -53,32 +93,74 @@ impl HostEd25519Bip32 for HermesRuntimeContext {
     /// - `true` : Signature checked OK.
     /// - `false` : Signature check failed.
     fn check_sig(
-        &mut self, _resource: wasmtime::component::Resource<Ed25519Bip32>, _data: Bstr,
-        _sig: Ed25519Bip32Signature,
+        &mut self, resource: wasmtime::component::Resource<Bip32Ed25519>, data: Bstr,
+        sig: Bip32Ed25519Signature,
     ) -> wasmtime::Result<bool> {
-        todo!()
+        let private_key = get_resource(self.app_name(), resource.rep());
+        match private_key {
+            Some(private_key) => {
+                let check_sig = check_signature(&private_key, &data, sig);
+                Ok(check_sig)
+            },
+            // TODO(bkioshn): https://github.com/input-output-hk/hermes/issues/183
+            None => Ok(false),
+        }
     }
 
     /// Derive a new private key from the current private key.
     ///
+    /// **Parameters**
+    ///
+    /// - `path` : Derivation path.
+    ///
     /// Note: uses BIP32 HD key derivation.
     fn derive(
-        &mut self, _resource: wasmtime::component::Resource<Ed25519Bip32>,
-    ) -> wasmtime::Result<wasmtime::component::Resource<Ed25519Bip32>> {
-        todo!()
+        &mut self, resource: wasmtime::component::Resource<Bip32Ed25519>, path: Path,
+    ) -> wasmtime::Result<wasmtime::component::Resource<Bip32Ed25519>> {
+        get_resource(self.app_name(), resource.rep())
+            .and_then(|private_key| derive_new_private_key(private_key, &path).ok())
+            .and_then(|derived_private_key| add_resource(self.app_name(), derived_private_key))
+            .map(Resource::new_own)
+            // TODO(bkioshn): https://github.com/input-output-hk/hermes/issues/183
+            .ok_or_else(|| wasmtime::Error::msg("Error deriving new private key"))
     }
 
-    /// Create a new RANDOM private key.
-    ///
-    /// Note, this does not need to be used, as the constructor will do this
-    /// automatically.
-    fn gen_private_key(&mut self) -> wasmtime::Result<Ed25519Bip32PrivateKey> {
-        todo!()
-    }
-
-    fn drop(&mut self, _rep: wasmtime::component::Resource<Ed25519Bip32>) -> wasmtime::Result<()> {
-        todo!()
+    fn drop(&mut self, res: wasmtime::component::Resource<Bip32Ed25519>) -> wasmtime::Result<()> {
+        // If the state deletion is successful, drop the resource.
+        if delete_resource(self.app_name(), res.rep()).is_some() {
+            let _unused = self.drop(res);
+        }
+        Ok(())
     }
 }
 
-impl Host for HermesRuntimeContext {}
+impl Host for HermesRuntimeContext {
+    /// # Generate BIP39 Mnemonic Function
+    ///
+    /// Generate a new BIP39 mnemonic phrase with the given
+    /// size, prefix and language.
+    ///
+    /// ## Parameters
+    ///
+    /// `size` : The size of the mnemonic. Must be a multiple of 3 and in the range of 12
+    /// - 24.
+    /// `prefix` : The prefix for the mnemonic. Must be a list of 1 - 3 words.
+    /// `language` : Optional. The language to use for the mnemonic.
+    ///              If not provided, the default language is used.
+    ///
+    /// ## Returns
+    ///
+    /// - Either a list of mnemonic words.
+    /// - Or an error if the mnemonic could not be generated:
+    ///     - `prefix-too-long` : The prefix is longer than the maximum allowed length,
+    ///       max is 3.
+    ///     - `invalid-mnemonic-length` : The mnemonic length is not a multiple of 3 or
+    ///       not in the range of 12 - 24.
+    ///     - `word-not-found` : A word in the mnemonic is not found in the word list.
+    ///     - `generate-entropy-failed` : Failed to generate entropy.
+    fn generate_mnemonic(
+        &mut self, size: u8, prefix: Vec<String>, language: Option<String>,
+    ) -> wasmtime::Result<Result<Vec<String>, Errno>> {
+        Ok(generate_new_mnemonic(size.into(), prefix, language))
+    }
+}
