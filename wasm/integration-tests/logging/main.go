8bc547078da177a8c77578540bdd8fa1f47025b1
bkioshn <bkioshn@gmail.com>
2024-04-01 22:53:05 +0700

    fix: add more export function

--- wasm/integration-tests/logging/main.go
+++ wasm/integration-tests/logging/main.go
@@ -1,5 +1,6 @@
 package main
 
+// FIXME: Rename this file
 import (
 	hermes "test/hermes"
 )
@@ -11,6 +12,16 @@ func init() {
 	hermes.SetExportsHermesCardanoEventOnBlock(b)
 	c := HermesCardanoEventOnTxn{}
 	hermes.SetExportsHermesCardanoEventOnTxn(c)
+	d := HermesCardanoEventOnRollback{}
+	hermes.SetExportsHermesCardanoEventOnRollback(d)
+	e := HermesCronEvent{}
+	hermes.SetExportsHermesCronEvent(e)
+	f := HermesInitEvent{}
+	hermes.SetExportsHermesInitEvent(f)
+	g := HermesKvStoreEvent{}
+	hermes.SetExportsHermesKvStoreEvent(g)
+	h := HermesIntegrationTestEvent{}
+	hermes.SetExportsHermesIntegrationTestEvent(h)
 }
 
 type WasiHttp0_2_0_IncomingHandler struct {
@@ -29,7 +40,36 @@ type HermesCardanoEventOnTxn struct{}
 func (h HermesCardanoEventOnTxn) OnCardanoTxn(blockchain hermes.ExportsHermesCardanoEventOnTxnCardanoBlockchainId, slot uint64, txn_index uint32, txn hermes.ExportsHermesCardanoEventOnTxnCardanoTxn) {
 }
 
-//go:generate wit-bindgen tiny-go ../wit --out-dir=hermes
-func main() {
-	// hermes.HermesLoggingApiLog(hermes.HermesLoggingApiLevelInfo(), hermes.None[string](), hermes.None[string](), hermes.None[uint32](), hermes.None[uint32](), hermes.None[string](), "Hello, world!", hermes.None[string]())
+type HermesCardanoEventOnRollback struct{}
+
+func (h HermesCardanoEventOnRollback) OnCardanoRollback(blockchain hermes.ExportsHermesCardanoEventOnRollbackCardanoBlockchainId, slot uint64) {
 }
+
+type HermesCronEvent struct{}
+
+func (h HermesCronEvent) OnCron(event hermes.ExportsHermesCronEventCronTagged, last bool) bool {
+	return false
+}
+
+type HermesInitEvent struct{}
+
+func (h HermesInitEvent) Init() bool {
+	return false
+}
+
+type HermesKvStoreEvent struct{}
+
+func (h HermesKvStoreEvent) KvUpdate(key string, value hermes.ExportsHermesKvStoreEventKvValues) {
+}
+
+type HermesIntegrationTestEvent struct{}
+
+func (h HermesIntegrationTestEvent) Test(test uint32, run bool) hermes.Option[hermes.ExportsHermesIntegrationTestEventTestResult] {
+	return hermes.Option[hermes.ExportsHermesIntegrationTestEventTestResult]{}
+}
+func (h HermesIntegrationTestEvent) Bench(test uint32, run bool) hermes.Option[hermes.ExportsHermesIntegrationTestEventTestResult] {
+	return hermes.Option[hermes.ExportsHermesIntegrationTestEventTestResult]{}
+}
+
+//go:generate wit-bindgen tiny-go ../../wasi/wit --out-dir=hermes
+func main() {}
