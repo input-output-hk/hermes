c09d4f98337c186fe601dac09487a42c575b9ae8
Steven Johnson <sakurainds@gmail.com>
2024-04-04 17:31:43 +0700

    fix(wasm): Fix running wasm integration tests

--- hermes/bin/tests/wasm-integration/main.rs
+++ hermes/bin/tests/wasm-integration/main.rs
@@ -30,94 +30,100 @@ fn main() -> Result<(), Box<dyn Error>> {
     libtest_mimic::run(&args, tests).exit();
 }
 
+/// Collect all the tests to run from a specified directory
+fn visit_dir(path: &Path, tests: &mut Vec<Trial>) -> Result<(), Box<dyn Error>> {
+    let args = Arguments::from_args();
+
+    let n_test: u32 = env::var(ENV_N_TEST)
+        .unwrap_or_else(|_| DEFAULT_ENV_N_TEST.to_owned())
+        .parse()?;
+    let n_bench: u32 = env::var(ENV_N_BENCH)
+        .unwrap_or_else(|_| DEFAULT_ENV_N_BENCH.to_owned())
+        .parse()?;
+
+    let raw_entries = match fs::read_dir(path) {
+        Ok(entries) => entries,
+        Err(_e) => return Ok(()), // If the directory of modules can not be found, just skip it.
+    };
+
+    let entries: Vec<_> = raw_entries
+        .collect::<Result<Vec<_>, _>>()?
+        .into_iter()
+        .map(|entry| entry.file_type().map(|file_type| (file_type, entry.path())))
+        .collect::<Result<_, _>>()?;
+
+    let wasm_file_paths: Vec<_> = entries
+        .iter()
+        .filter_map(|(file_type, path)| {
+            (file_type.is_file() && path.extension() == Some(OsStr::new("wasm"))).then_some(path)
+        })
+        .collect();
+
+    let dir_paths: Vec<_> = entries
+        .iter()
+        .filter_map(|(file_type, path)| file_type.is_dir().then_some(path))
+        .collect();
+
+    // process `.wasm` files
+    for file_path in wasm_file_paths {
+        let name = file_path.strip_prefix(path)?.display().to_string();
+
+        // Execute the wasm tests to get their name
+        // Load WASM module in the executor.
+        let wasm_buf = fs::read(file_path)?;
+        let mut module = Module::new(&wasm_buf)?;
+
+        let mut collect = |event_type: EventType, n: u32| -> Result<(), Box<dyn Error>> {
+            // Collect the cases in a loop until no more cases.
+            for i in 0..n {
+                match execute_event(&mut module, i, false, event_type)? {
+                    Some(result) => {
+                        let path_string = file_path.to_string_lossy().to_string();
+
+                        let test = match event_type {
+                            EventType::Test => {
+                                Trial::test(result.name, move || {
+                                    execute_test(i, path_string, event_type)
+                                })
+                            },
+                            EventType::Bench => {
+                                Trial::bench(result.name, move |test_mode| {
+                                    execute_bench(test_mode, i, path_string, event_type)
+                                })
+                            },
+                        }
+                        .with_kind(name.clone());
+
+                        tests.push(test);
+                    },
+                    _ => {
+                        break;
+                    },
+                }
+            }
+
+            Ok(())
+        };
+
+        if args.test || args.list {
+            collect(EventType::Test, n_test)?;
+        }
+        if args.bench || args.list {
+            collect(EventType::Bench, n_bench)?;
+        }
+    }
+
+    // process items inside directories
+    for path in dir_paths {
+        visit_dir(path, tests)?;
+    }
+
+    Ok(())
+}
+
 /// Creates as many tests as required for each `.wasm` file in the current directory or
 /// sub-directories of the current directory.
 fn collect_tests() -> Result<Vec<Trial>, Box<dyn Error>> {
-    #[allow(clippy::missing_docs_in_private_items)]
-    fn visit_dir(path: &Path, tests: &mut Vec<Trial>) -> Result<(), Box<dyn Error>> {
-        let args = Arguments::from_args();
-
-        let n_test: u32 = env::var(ENV_N_TEST)
-            .unwrap_or_else(|_| DEFAULT_ENV_N_TEST.to_owned())
-            .parse()?;
-        let n_bench: u32 = env::var(ENV_N_BENCH)
-            .unwrap_or_else(|_| DEFAULT_ENV_N_BENCH.to_owned())
-            .parse()?;
-
-        let entries: Vec<_> = fs::read_dir(path)?
-            .collect::<Result<Vec<_>, _>>()?
-            .into_iter()
-            .map(|entry| entry.file_type().map(|file_type| (file_type, entry.path())))
-            .collect::<Result<_, _>>()?;
-        let wasm_file_paths: Vec<_> = entries
-            .iter()
-            .filter_map(|(file_type, path)| {
-                (file_type.is_file() && path.extension() == Some(OsStr::new("wasm")))
-                    .then_some(path)
-            })
-            .collect();
-        let dir_paths: Vec<_> = entries
-            .iter()
-            .filter_map(|(file_type, path)| file_type.is_dir().then_some(path))
-            .collect();
-
-        // process `.wasm` files
-        for file_path in wasm_file_paths {
-            let name = file_path.strip_prefix(path)?.display().to_string();
-
-            // Execute the wasm tests to get their name
-            // Load WASM module in the executor.
-            let wasm_buf = fs::read(file_path)?;
-            let mut module = Module::new(&wasm_buf)?;
-
-            let mut collect = |event_type: EventType, n: u32| -> Result<(), Box<dyn Error>> {
-                // Collect the cases in a loop until no more cases.
-                for i in 0..n {
-                    match execute_event(&mut module, i, false, event_type)? {
-                        Some(result) => {
-                            let path_string = file_path.to_string_lossy().to_string();
-
-                            let test = match event_type {
-                                EventType::Test => {
-                                    Trial::test(result.name, move || {
-                                        execute_test(i, path_string, event_type)
-                                    })
-                                },
-                                EventType::Bench => {
-                                    Trial::bench(result.name, move |test_mode| {
-                                        execute_bench(test_mode, i, path_string, event_type)
-                                    })
-                                },
-                            }
-                            .with_kind(name.clone());
-
-                            tests.push(test);
-                        },
-                        _ => {
-                            break;
-                        },
-                    }
-                }
-
-                Ok(())
-            };
-
-            if args.bench {
-                collect(EventType::Bench, n_bench)?;
-            }
-            if args.test {
-                collect(EventType::Test, n_test)?;
-            }
-        }
-
-        // process items inside directories
-        for path in dir_paths {
-            visit_dir(path, tests)?;
-        }
-
-        Ok(())
-    }
-
     // Read wasm components to be test in a directory.
     let mut tests = Vec::new();
     let test_module_dir =
@@ -137,8 +143,11 @@ fn execute(test_case: u32, path: String, event_type: EventType) -> Result<(), Fa
 
     match execute_event(&mut module, test_case, true, event_type)? {
         Some(result) => {
-            assert!(result.status);
-            Ok(())
+            if result.status {
+                Ok(())
+            } else {
+                Err(Failed::from("Failed"))
+            }
         },
         _ => Err(Failed::from("result unexpected")),
     }
