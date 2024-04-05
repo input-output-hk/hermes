80cb887cf74e4f56a677f9b10d85936045beac36
Steven Johnson <sakurainds@gmail.com>
2024-04-05 13:54:19 +0700

    fix(wasm): remove commented obsolete code

--- hermes/bin/tests/wasm-integration/main.rs
+++ hermes/bin/tests/wasm-integration/main.rs
@@ -36,7 +36,6 @@ fn init_logger() -> Result<(), SetGlobalDefaultError> {
         .with_file(true)
         .with_line_number(true)
         .with_timer(time::UtcTime::rfc_3339())
-        //.with_span_events(FmtSpan::CLOSE)
         .with_max_level(LevelFilter::INFO)
         .finish();
 
@@ -106,16 +105,12 @@ fn visit_dir(path: &Path, tests: &mut Vec<Trial>) -> Result<(), Box<dyn Error>> 
                         let path_string = file_path.to_string_lossy().to_string();
 
                         let test = match event_type {
-                            EventType::Test => {
-                                Trial::test(result.name, move || {
-                                    execute_test(i, path_string, event_type)
-                                })
-                            },
-                            EventType::Bench => {
-                                Trial::bench(result.name, move |test_mode| {
-                                    execute_bench(test_mode, i, path_string, event_type)
-                                })
-                            },
+                            EventType::Test => Trial::test(result.name, move || {
+                                execute_test(i, path_string, event_type)
+                            }),
+                            EventType::Bench => Trial::bench(result.name, move |test_mode| {
+                                execute_bench(test_mode, i, path_string, event_type)
+                            }),
                         }
                         .with_kind(name.clone());
 
