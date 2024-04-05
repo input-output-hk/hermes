36e64e99585b5cfad9a19e8b49c7480c2df50848
Felipe Rosa <felipe.rosa@iohk.io>
2024-04-05 00:06:56 -0300

    feat(wasm): Use pallas to parse blocks in Cardano RTE integration test component

--- wasm/integration-test/cardano/src/lib.rs
+++ wasm/integration-test/cardano/src/lib.rs
@@ -14,8 +14,49 @@ use bindings::{
     },
     wasi::http::types::{IncomingRequest, ResponseOutparam},
 };
+use pallas_traverse::MultiEraBlock;
 
 struct TestComponent;
+
+fn test_fetch_block() -> bool {
+    let block_slot = 56581108;
+    let block_hash =
+        hex::decode("e095cca24865b85812109a8a0af01a8a80c6a74d1f56cfe1830631423a806b46")
+            .expect("valid block hash hex");
+
+    let slot = Slot::Point((block_slot, block_hash.clone()));
+
+    let Ok(block_cbor) = cardano::api::fetch_block(CardanoBlockchainId::Preprod, &slot) else {
+        return false;
+    };
+
+    let Ok(block) = MultiEraBlock::decode(&block_cbor) else {
+        return false;
+    };
+
+    block_slot == block.slot() && block_hash == block.hash().to_vec()
+}
+
+impl bindings::exports::hermes::integration_test::event::Guest for TestComponent {
+    fn test(test: u32, run: bool) -> Option<TestResult> {
+        match test {
+            0 => {
+                let status = if run { test_fetch_block() } else { true };
+
+                Some(TestResult {
+                    name: "Fetch block".to_string(),
+                    status,
+                })
+            }
+
+            _ => None,
+        }
+    }
+
+    fn bench(_test: u32, _run: bool) -> Option<TestResult> {
+        None
+    }
+}
 
 impl bindings::exports::hermes::cardano::event_on_block::Guest for TestComponent {
     fn on_cardano_block(_blockchain: CardanoBlockchainId, _block: CardanoBlock, _source: BlockSrc) {
@@ -54,37 +95,6 @@ impl bindings::exports::hermes::kv_store::event::Guest for TestComponent {
 
 impl bindings::exports::wasi::http::incoming_handler::Guest for TestComponent {
     fn handle(_request: IncomingRequest, _response_out: ResponseOutparam) {}
-}
-
-fn test_fetch_block() -> bool {
-    let slot = Slot::Point((
-        56581108,
-        hex::decode("e095cca24865b85812109a8a0af01a8a80c6a74d1f56cfe1830631423a806b46")
-            .expect("decoded"),
-    ));
-
-    cardano::api::fetch_block(CardanoBlockchainId::Preprod, &slot).is_ok()
-}
-
-impl bindings::exports::hermes::integration_test::event::Guest for TestComponent {
-    fn test(test: u32, run: bool) -> Option<TestResult> {
-        match test {
-            0 => {
-                let status = if run { test_fetch_block() } else { true };
-
-                Some(TestResult {
-                    name: "Fetch block".to_string(),
-                    status,
-                })
-            }
-
-            _ => None,
-        }
-    }
-
-    fn bench(_test: u32, _run: bool) -> Option<TestResult> {
-        None
-    }
 }
 
 bindings::export!(TestComponent);
