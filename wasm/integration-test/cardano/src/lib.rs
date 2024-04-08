9719e753dfe17537483d6a232a735df003705690
Steven Johnson <sakurainds@gmail.com>
2024-04-08 19:37:24 +0700

    fix(wasm): remove generated bindings, try and use autogenerated ones

--- wasm/integration-test/cardano/src/lib.rs
+++ wasm/integration-test/cardano/src/lib.rs
@@ -1,8 +1,8 @@
 // Allow everything since this is generated code.
 #[allow(clippy::all, unused)]
-mod bindings;
+mod hermes;
 
-use bindings::{
+use hermes::{
     exports::hermes::integration_test::event::TestResult,
     hermes::{
         cardano::{
@@ -37,7 +37,7 @@ fn test_fetch_block() -> bool {
     block_slot == block.slot() && block_hash == block.hash().to_vec()
 }
 
-impl bindings::exports::hermes::integration_test::event::Guest for TestComponent {
+impl hermes::exports::hermes::integration_test::event::Guest for TestComponent {
     fn test(test: u32, run: bool) -> Option<TestResult> {
         match test {
             0 => {
@@ -58,16 +58,16 @@ impl bindings::exports::hermes::integration_test::event::Guest for TestComponent
     }
 }
 
-impl bindings::exports::hermes::cardano::event_on_block::Guest for TestComponent {
+impl hermes::exports::hermes::cardano::event_on_block::Guest for TestComponent {
     fn on_cardano_block(_blockchain: CardanoBlockchainId, _block: CardanoBlock, _source: BlockSrc) {
     }
 }
 
-impl bindings::exports::hermes::cardano::event_on_rollback::Guest for TestComponent {
+impl hermes::exports::hermes::cardano::event_on_rollback::Guest for TestComponent {
     fn on_cardano_rollback(_blockchain: CardanoBlockchainId, _slot: u64) {}
 }
 
-impl bindings::exports::hermes::cardano::event_on_txn::Guest for TestComponent {
+impl hermes::exports::hermes::cardano::event_on_txn::Guest for TestComponent {
     fn on_cardano_txn(
         _blockchain: CardanoBlockchainId,
         _slot: u64,
@@ -77,24 +77,24 @@ impl bindings::exports::hermes::cardano::event_on_txn::Guest for TestComponent {
     }
 }
 
-impl bindings::exports::hermes::cron::event::Guest for TestComponent {
+impl hermes::exports::hermes::cron::event::Guest for TestComponent {
     fn on_cron(_event: CronTagged, _last: bool) -> bool {
         false
     }
 }
 
-impl bindings::exports::hermes::init::event::Guest for TestComponent {
+impl hermes::exports::hermes::init::event::Guest for TestComponent {
     fn init() -> bool {
         true
     }
 }
 
-impl bindings::exports::hermes::kv_store::event::Guest for TestComponent {
+impl hermes::exports::hermes::kv_store::event::Guest for TestComponent {
     fn kv_update(_key: String, _value: KvValues) {}
 }
 
-impl bindings::exports::wasi::http::incoming_handler::Guest for TestComponent {
+impl hermes::exports::wasi::http::incoming_handler::Guest for TestComponent {
     fn handle(_request: IncomingRequest, _response_out: ResponseOutparam) {}
 }
 
-bindings::export!(TestComponent);
+hermes::export!(TestComponent);
