38942816567c1da1997165e7a023db97f5512854
Apisit Ritreungroj <38898766+apskhem@users.noreply.github.com>
2024-04-08 18:14:57 +0700

    test: time module (#200)
    * feat: initial commit
    
    * feat: simple function
    
    * ci: add test
    
    * test: restructure and add test to localtime
    
    * fix: minor format
    
    * refactor: remove localtime name
    
    * fix: localtime issue
    
    * feat: clocks test
    
    * fix: test warms
    
    * fix: use existing tests
    
    * chore: earthfile tmp
    
    * fix: builder
    
    * fix: remove wall test
    
    * chore: fmtfix

---
+++ hermes/bin/src/runtime_extensions/hermes/localtime/time.rs
@@ -0,0 +1,60 @@
+//! Localtime host implementation for WASM runtime.
+
+use chrono::{Local, TimeZone};
+use chrono_tz::Tz;
+
+use crate::runtime_extensions::{
+    bindings::{
+        hermes::localtime::api::{Errno, Localtime, Timezone},
+        wasi::clocks::wall_clock::Datetime,
+    },
+    hermes::localtime::get_tz,
+};
+
+/// (Implementation) Get localtime from a datetime or now.
+pub(super) fn get_localtime(
+    when: Option<Datetime>, tz: Option<Timezone>,
+) -> Result<Localtime, Errno> {
+    let timezone = get_tz(tz)?;
+    let local_naive = match when {
+        Some(Datetime {
+            seconds,
+            nanoseconds,
+        }) => {
+            let seconds = seconds.try_into().map_err(|_| Errno::InvalidLocaltime)?;
+            let utc_dt = chrono::DateTime::from_timestamp(seconds, nanoseconds)
+                .ok_or(Errno::InvalidLocaltime)?;
+            utc_dt.naive_utc()
+        },
+        None => Local::now().naive_utc(),
+    };
+    let local_date_time = timezone.from_utc_datetime(&local_naive);
+
+    local_date_time.try_into()
+}
+
+/// (Implementation) Get a new localtime from a localtime, by recalculating time for a new
+/// timezone.
+pub(super) fn alt_localtime(time: Localtime, tz: Option<Timezone>) -> Result<Localtime, Errno> {
+    let local_date_time: chrono::DateTime<Tz> = time.try_into()?;
+    let alt_local_date_time = match tz {
+        Some(alt_tz) => {
+            let tz = get_tz(Some(alt_tz))?;
+            tz.from_utc_datetime(&local_date_time.naive_utc())
+        },
+        None => local_date_time,
+    };
+
+    alt_local_date_time.try_into()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_get_localtime_with_utc_offset() {
+        let result = get_localtime(None, Some(String::from("Europe/London")));
+        assert!(result.is_ok()); // Check if the function call was successful
+    }
+}
---
+++ wasm/integration-test/localtime/localtime.c
@@ -0,0 +1,81 @@
+#include "bindings_src/hermes.h"
+#include <string.h>
+
+// Exported Functions from `wasi:http/incoming-handler@0.2.0`
+void exports_wasi_http_incoming_handler_handle(exports_wasi_http_incoming_handler_own_incoming_request_t request, exports_wasi_http_incoming_handler_own_response_outparam_t response_out)
+{
+}
+
+// Exported Functions from `hermes:cardano/event-on-block`
+void exports_hermes_cardano_event_on_block_on_cardano_block(exports_hermes_cardano_event_on_block_cardano_blockchain_id_t blockchain, exports_hermes_cardano_event_on_block_cardano_block_t *block, exports_hermes_cardano_event_on_block_block_src_t source)
+{
+}
+
+// Exported Functions from `hermes:cardano/event-on-txn`
+void exports_hermes_cardano_event_on_txn_on_cardano_txn(exports_hermes_cardano_event_on_txn_cardano_blockchain_id_t blockchain, uint64_t slot, uint32_t txn_index, exports_hermes_cardano_event_on_txn_cardano_txn_t *txn)
+{
+}
+
+// Exported Functions from `hermes:cardano/event-on-rollback`
+void exports_hermes_cardano_event_on_rollback_on_cardano_rollback(exports_hermes_cardano_event_on_rollback_cardano_blockchain_id_t blockchain, uint64_t slot)
+{
+}
+
+// Exported Functions from `hermes:cron/event`
+bool exports_hermes_cron_event_on_cron(exports_hermes_cron_event_cron_tagged_t *event, bool last)
+{
+    return false;
+}
+
+// Exported Functions from `hermes:init/event`
+bool exports_hermes_init_event_init(void)
+{
+    return false;
+}
+
+// Exported Functions from `hermes:kv-store/event`
+void exports_hermes_kv_store_event_kv_update(hermes_string_t *key, exports_hermes_kv_store_event_kv_values_t *value)
+{
+}
+
+#define HERMES_STRING(x) \
+    (hermes_string_t) { (uint8_t *)x, strlen(x) }
+#define HERMES_JSON_STRING(x) \
+    (hermes_json_api_json_t) { (uint8_t *)x, strlen(x) }
+
+// Localtime test function
+bool test_localtime_function()
+{
+    hermes_localtime_api_localtime_t ret;
+    hermes_localtime_api_errno_t err;
+    
+    if (hermes_localtime_api_get_localtime(NULL, &HERMES_STRING("Europe/London"), &ret, &err)) {
+        return true;
+    }
+    
+    return false;
+}
+
+// Exported Functions from `hermes:integration-test/event`
+bool exports_hermes_integration_test_event_test(uint32_t test, bool run, exports_hermes_integration_test_event_test_result_t *ret)
+{
+    ret->status = true;
+    switch (test)
+    {
+    case 0:
+        hermes_string_dup(&ret->name, "get_localtime");
+        if (run)
+            ret->status = test_localtime_function();
+        break;
+    
+    default:
+        return false;
+    }
+
+    return true;
+}
+
+bool exports_hermes_integration_test_event_bench(uint32_t test, bool run, exports_hermes_integration_test_event_test_result_t *ret)
+{
+    return false;
+}
