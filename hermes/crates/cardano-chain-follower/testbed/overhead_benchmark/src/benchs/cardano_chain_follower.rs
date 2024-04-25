433a72b4bb64dfb9953c0edb29ad0a69071b569e
Felipe Rosa <felipe.rosa@iohk.io>
2024-04-25 07:21:02 -0300

    feat(cardano-chain-follower): Overhead benchmark implementations (pallas + cardano-chain-follower) (#211)
    * feat(cardano-chain-follower): Basic benchmark implementations (pallas + cardano-chain-follower)
    
    Co-authored-by: Joaquín Rosales <joaquin.rosales@iohk.io>
    
    * feat(cardano-chain-follower): Working snapshot_tip function and some output improvements
    
    * feat(cardano-chain-follower): Change directory structure to match hermes crates conventions
    
    * feat(cardano-chain-follower): Revert Earthfile to version 0.7 to make CI pass
    
    * feat(cardano-chain-follower): Fix spelling and benchmark Earthfile
    
    * chore(cardano-chain-follower): Add some READMEs to the cardano-chain-follower overhead benchmark
    
    ---------
    
    Co-authored-by: Joaquín Rosales <joaquin.rosales@iohk.io>
    Co-authored-by: Oleksandr Prokhorenko <djminikin@gmail.com>

---
+++ hermes/crates/cardano-chain-follower/testbed/overhead_benchmark/src/benchs/cardano_chain_follower.rs
@@ -0,0 +1,58 @@
+//! Benchmark implementation that uses the chain-follower-crate to read all the
+//! blocks from the specified Mithril snapshot.
+
+use cardano_chain_follower::{ChainUpdate, Follower, FollowerConfigBuilder, Network};
+use pallas_traverse::MultiEraBlock;
+
+use super::{monitor, snapshot_tip, BenchmarkParams};
+
+/// Executes the benchmark.
+pub async fn run(params: BenchmarkParams) -> anyhow::Result<()> {
+    let mithril_snapshot_tip_data = snapshot_tip(&params.mithril_snapshot_path)?
+        .ok_or(anyhow::anyhow!("Failed to find snapshot tip"))?;
+    let mithril_snapshot_tip_block = MultiEraBlock::decode(&mithril_snapshot_tip_data)?;
+
+    let monitor_task_handle = monitor::spawn_task(mithril_snapshot_tip_block.number());
+
+    let config = FollowerConfigBuilder::default()
+        .follow_from(cardano_chain_follower::PointOrTip::Point(
+            cardano_chain_follower::Point::Origin,
+        ))
+        .mithril_snapshot_path(params.mithril_snapshot_path)
+        .build();
+
+    let mut follower = Follower::connect(
+        "relays-new.cardano-mainnet.iohk.io:3001",
+        Network::Mainnet,
+        config,
+    )
+    .await?;
+
+    loop {
+        let update = follower.next().await?;
+
+        match update {
+            ChainUpdate::Block(raw_block_data) => {
+                let block_data = raw_block_data.decode()?;
+
+                monitor_task_handle.send_update(monitor::BenchmarkStats {
+                    blocks_read: 1,
+                    block_bytes_read: raw_block_data.as_ref().len() as u64,
+                    current_block: block_data.number(),
+                    current_slot: block_data.slot(),
+                })?;
+
+                if block_data.number() >= mithril_snapshot_tip_block.number() {
+                    break;
+                }
+            },
+            ChainUpdate::Rollback(_) => {
+                anyhow::bail!("Unexpected rollback: benchmark should not receive rollback events");
+            },
+        }
+    }
+
+    monitor_task_handle.close().await;
+
+    Ok(())
+}
