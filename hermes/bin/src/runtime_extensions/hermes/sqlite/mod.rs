9b32d32029130b9a897f9a01c4b78425d8870a22
Apisit Ritreungroj <apisit_lon@hotmail.com>
2024-05-01 18:38:25 +0700

    fix: duplicate file name

--- hermes/bin/src/runtime_extensions/hermes/sqlite/mod.rs
+++ hermes/bin/src/runtime_extensions/hermes/sqlite/mod.rs
@@ -1,11 +1,11 @@
 //! `SQLite` runtime extension implementation.
 
 mod host;
-mod sqlite;
+mod connection;
 mod statement;
 
 /// Advise Runtime Extensions of a new context
 pub(crate) fn new_context(ctx: &crate::runtime_context::HermesRuntimeContext) {
-    sqlite::new_context(ctx);
+    connection::new_context(ctx);
     statement::new_context(ctx);
 }
