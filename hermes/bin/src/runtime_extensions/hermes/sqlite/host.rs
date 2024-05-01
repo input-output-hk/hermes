a21e1f323ab8705f7590b0dc2ab738ba643e2113
Apisit Ritreungroj <apisit_lon@hotmail.com>
2024-05-01 21:35:31 +0700

    feat: initial commit

--- hermes/bin/src/runtime_extensions/hermes/sqlite/host.rs
+++ hermes/bin/src/runtime_extensions/hermes/sqlite/host.rs
@@ -1,5 +1,7 @@
 //! `SQLite` host implementation for WASM runtime.
 
+use libsqlite3_sys::*;
+
 use crate::{
     runtime_context::HermesRuntimeContext,
     runtime_extensions::bindings::hermes::sqlite::api::{Errno, Host, Sqlite},
@@ -19,8 +21,35 @@ impl Host for HermesRuntimeContext {
     /// If the database is opened (and/or created) successfully, then the `sqlite3` object
     /// is returned. Otherwise an error code is returned.
     fn open(
-        &mut self, _readonly: bool, _memory: bool,
+        &mut self, readonly: bool, memory: bool,
     ) -> wasmtime::Result<Result<wasmtime::component::Resource<Sqlite>, Errno>> {
-        todo!()
+        let mut db: *mut sqlite3 = std::ptr::null_mut();
+        let db_path = if memory {
+            ":memory:"
+        } else {
+            "your_database_path.db"
+        };
+        let flags = if readonly {
+            SQLITE_OPEN_READONLY
+        } else {
+            SQLITE_OPEN_CREATE | SQLITE_OPEN_READWRITE
+        };
+
+        let result = unsafe {
+            sqlite3_open_v2(
+                db_path.as_ptr() as *const _,
+                &mut db,
+                flags,
+                std::ptr::null(),
+            )
+        };
+
+        if result != SQLITE_OK {
+            return Ok(Err(result.into()));
+        } else if db.is_null() {
+            return Err(wasmtime::Error::msg("Error opening a connection to the database"));
+        } else {
+            return Ok(Ok(wasmtime::component::Resource::new_own(db as u32)))
+        }
     }
 }
--- hermes/bin/src/runtime_extensions/hermes/sqlite/mod.rs
+++ hermes/bin/src/runtime_extensions/hermes/sqlite/mod.rs
@@ -9,3 +9,44 @@ pub(crate) fn new_context(ctx: &crate::runtime_context::HermesRuntimeContext) {
     connection::new_context(ctx);
     statement::new_context(ctx);
 }
+
+use libsqlite3_sys::*;
+
+use crate::runtime_extensions::bindings::hermes::sqlite::api::Errno;
+
+impl From<i32> for Errno {
+    fn from(value: i32) -> Self {
+        match value {
+            SQLITE_ERROR => Errno::Error,
+            SQLITE_INTERNAL => Errno::Internal,
+            SQLITE_PERM => Errno::Perm,
+            SQLITE_ABORT => Errno::Abort,
+            SQLITE_BUSY => Errno::Busy,
+            SQLITE_LOCKED => Errno::Locked,
+            SQLITE_NOMEM => Errno::Nomem,
+            SQLITE_READONLY => Errno::Readonly,
+            SQLITE_INTERRUPT => Errno::Interrupt,
+            SQLITE_IOERR => Errno::Ioerr,
+            SQLITE_CORRUPT => Errno::Corrupt,
+            SQLITE_NOTFOUND => Errno::Notfound,
+            SQLITE_FULL => Errno::Full,
+            SQLITE_CANTOPEN => Errno::Cantopen,
+            SQLITE_PROTOCOL => Errno::Protocol,
+            SQLITE_EMPTY => Errno::Empty,
+            SQLITE_SCHEMA => Errno::Schema,
+            SQLITE_TOOBIG => Errno::Toobig,
+            SQLITE_CONSTRAINT => Errno::Constraint,
+            SQLITE_MISMATCH => Errno::Mismatched,
+            SQLITE_MISUSE => Errno::Misuse,
+            SQLITE_NOLFS => Errno::Nolfs,
+            SQLITE_AUTH => Errno::Auth,
+            SQLITE_FORMAT => Errno::Format,
+            SQLITE_RANGE => Errno::Range,
+            SQLITE_NOTADB => Errno::Notadb,
+            SQLITE_NOTICE => Errno::Notice,
+            SQLITE_WARNING => Errno::Warning,
+            _ => Errno::Error,
+        }
+    }
+}
+
