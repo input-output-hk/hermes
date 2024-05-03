d6b342ef0fb5f1ec76475695a1d92111c2ed26ad
Apisit Ritreungroj <apisit_lon@hotmail.com>
2024-05-03 17:38:41 +0700

    feat: stmt col fn

--- hermes/bin/src/runtime_extensions/hermes/sqlite/statement/host.rs
+++ hermes/bin/src/runtime_extensions/hermes/sqlite/statement/host.rs
@@ -83,9 +83,43 @@ impl HostStatement for HermesRuntimeContext {
     ///
     /// The value of a result column in a specific data format.
     fn column(
-        &mut self, _resource: wasmtime::component::Resource<Statement>, _index: u32,
+        &mut self, resource: wasmtime::component::Resource<Statement>, index: u32,
     ) -> wasmtime::Result<Result<Value, Errno>> {
-        todo!()
+        let stmt_ptr: *mut sqlite3_stmt = resource.rep() as *mut _;
+        let index = index as i32;
+
+        let value = unsafe {
+            let column_type = sqlite3_column_type(stmt_ptr, index);
+
+            match column_type {
+                SQLITE_BLOB => {
+                    let blob_ptr = sqlite3_column_value(stmt_ptr, index);
+                    let blob_len = sqlite3_column_bytes(stmt_ptr, index);
+                    let blob_slice =
+                        std::slice::from_raw_parts(blob_ptr as *const u8, blob_len as usize);
+                    Value::Blob(blob_slice.to_vec())
+                },
+                SQLITE_FLOAT => Value::Double(sqlite3_column_double(stmt_ptr, index)),
+                SQLITE_INTEGER => {
+                    let int_value = sqlite3_column_int64(stmt_ptr, index);
+                    if int_value >= std::i32::MIN as i64 && int_value <= std::i32::MAX as i64 {
+                        Value::Int32(int_value as i32)
+                    } else {
+                        Value::Int64(int_value)
+                    }
+                },
+                SQLITE_NULL => Value::Null,
+                SQLITE_TEXT => {
+                    let text_ptr = sqlite3_column_text(stmt_ptr, index);
+                    let text_slice = std::ffi::CStr::from_ptr(text_ptr as *const i8);
+                    let text_string = text_slice.to_string_lossy().into_owned();
+                    Value::Text(text_string)
+                },
+                _ => return Err(wasmtime::Error::msg("Unsupported column type")),
+            }
+        };
+
+        Ok(Ok(value))
     }
 
     /// Destroys a prepared statement object. If the most recent evaluation of the
