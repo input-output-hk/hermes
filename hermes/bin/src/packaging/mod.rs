3d2cc364e48c9d23472b8a47ba9621f9a6704698
Mr-Leshiy <leshiy12345678@gmail.com>
2024-05-20 11:37:47 +0300

    add WasmModulePackage

--- hermes/bin/src/packaging/mod.rs
+++ hermes/bin/src/packaging/mod.rs
@@ -1,17 +1,96 @@
 //! Hermes packaging.
 
+use std::ops::Deref;
+
+#[allow(dead_code, missing_docs, clippy::missing_docs_in_private_items)]
+pub(crate) mod wasm_module;
+
+/// File not found error.
+#[derive(thiserror::Error, Debug)]
+#[error("File {0} not found")]
+pub(crate) struct FileNotFoundError(String);
+
+/// Copy file to hdf5 package.
+fn copy_file_from_dir_to_package<P, Package>(
+    dir: P, file_name: &str, package: &Package,
+) -> anyhow::Result<()>
+where
+    P: AsRef<std::path::Path>,
+    Package: Deref<Target = hdf5::Group>,
+{
+    let file_path = dir.as_ref().join(file_name);
+
+    let file_data = std::fs::read(file_path).map_err(|err| {
+        if err.kind() == std::io::ErrorKind::NotFound {
+            anyhow::Error::new(FileNotFoundError(file_name.to_string()))
+        } else {
+            anyhow::Error::new(err)
+        }
+    })?;
+
+    package
+        .new_dataset_builder()
+        .with_data(&file_data)
+        .create(file_name)?;
+
+    Ok(())
+}
+
 #[cfg(test)]
 mod tests {
     use hdf5::File;
     use temp_dir::TempDir;
+
+    use super::*;
+
+    #[test]
+    fn copy_file_to_package_test() {
+        let dir = TempDir::new().expect("cannot create temp dir");
+
+        let file_name = dir.child("test.hdf5");
+        let hdf5_file = File::create(file_name).expect("cannot create HDF5 file");
+
+        let metada_json = "metadata.json";
+        let metada_json_data = r#"{ "name": "Alex", "age": 25"}"#;
+        std::fs::write(dir.child(metada_json), metada_json_data)
+            .expect("Cannot write data to metadata.json");
+
+        copy_file_from_dir_to_package(dir.path(), metada_json, &hdf5_file)
+            .expect("Cannot copy metadata.json to hdf5 package");
+
+        let metada_json = hdf5_file
+            .dataset(metada_json)
+            .expect("cannot open metadata.json dataset");
+        let data = String::from_utf8(
+            metada_json
+                .read_raw()
+                .expect("cannot read metadata.json dataset"),
+        )
+        .expect("cannot parse metadata.json dataset");
+        assert_eq!(data, metada_json_data);
+    }
+
+    #[test]
+    fn copy_file_to_package_not_found_test() {
+        let dir = TempDir::new().expect("cannot create temp dir");
+
+        let file_name = dir.child("test.hdf5");
+        let hdf5_file = File::create(file_name).expect("cannot create HDF5 file");
+
+        let metada_json = "metadata.json";
+
+        let err = copy_file_from_dir_to_package(dir.path(), metada_json, &hdf5_file)
+            .expect_err("Should return error");
+
+        assert!(err.is::<FileNotFoundError>());
+    }
 
     #[test]
     fn hdf5_dataset_test() {
         let dir = TempDir::new().expect("cannot create temp dir");
 
         let file_name = dir.child("test.hdf5");
-        let hdf5_file = File::create(file_name).expect("cannot create HDF5 file");
-        let root_group = hdf5_file.as_group().expect("cannot create HDF5 group");
+        let root_group = File::create(file_name).expect("cannot create HDF5 file");
 
         println!(
             "hdf5 group: {root_group:?}, members: {:?}",
---
+++ hermes/bin/src/packaging/wasm_module.rs
@@ -0,0 +1,78 @@
+//! Wasm module package.
+
+use std::path::Path;
+
+use super::{copy_file_from_dir_to_package, FileNotFoundError};
+
+pub(crate) struct WasmModulePackage {
+    package: hdf5::File,
+}
+
+impl WasmModulePackage {
+    const CONFIG_JSON: &'static str = "config.json";
+    const CONFIG_SCHEMA_JSON: &'static str = "config.schema.json";
+    const METDATA_JSON: &'static str = "metadata.json";
+    const MODULE_WASM: &'static str = "module.wasm";
+    const SETTINGS_SCHEMA_JSON: &'static str = "settings.schema.json";
+
+    pub(crate) fn from_dir<P: AsRef<Path>>(path: P) -> anyhow::Result<Self> {
+        let path = path.as_ref();
+
+        let package = hdf5::File::create(path.join("module.hdf5"))?;
+
+        match copy_file_from_dir_to_package(path, Self::CONFIG_SCHEMA_JSON, &package) {
+            Ok(()) => copy_file_from_dir_to_package(path, Self::CONFIG_JSON, &package)?,
+            Err(err) if err.is::<FileNotFoundError>() => {},
+            Err(err) => return Err(err),
+        }
+
+        copy_file_from_dir_to_package(path, Self::METDATA_JSON, &package)?;
+        copy_file_from_dir_to_package(path, Self::MODULE_WASM, &package)?;
+        copy_file_from_dir_to_package(path, Self::SETTINGS_SCHEMA_JSON, &package)
+            .or_else(|err| err.is::<FileNotFoundError>().then_some(()).ok_or(err))?;
+
+        Ok(Self { package })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use temp_dir::TempDir;
+
+    use super::*;
+
+    #[test]
+    fn from_dir_test() {
+        let dir = TempDir::new().expect("cannot create temp dir");
+        println!("{:?}", dir.path());
+
+        let config_json = dir.path().join(WasmModulePackage::CONFIG_JSON);
+        let config_schema_json = dir.path().join(WasmModulePackage::CONFIG_SCHEMA_JSON);
+        let metadata_json = dir.path().join(WasmModulePackage::METDATA_JSON);
+        let module_wasm_path = dir.path().join(WasmModulePackage::MODULE_WASM);
+        let settings_schema_json = dir.path().join(WasmModulePackage::SETTINGS_SCHEMA_JSON);
+
+        std::fs::File::create(config_json).expect("Cannot create config.json file");
+        std::fs::File::create(config_schema_json).expect("Cannot create config.schema.json file");
+        std::fs::File::create(metadata_json).expect("Cannot create metadata.json file");
+        std::fs::File::create(module_wasm_path).expect("Cannot create module.wasm file");
+        std::fs::File::create(settings_schema_json)
+            .expect("Cannot create settings.schema.json file");
+
+        WasmModulePackage::from_dir(dir.path()).expect("Cannot create module package");
+    }
+
+    #[test]
+    fn from_dir_some_files_missing_test() {
+        let dir = TempDir::new().expect("cannot create temp dir");
+        println!("{:?}", dir.path());
+
+        let metadata_json = dir.path().join(WasmModulePackage::METDATA_JSON);
+        let module_wasm_path = dir.path().join(WasmModulePackage::MODULE_WASM);
+
+        std::fs::File::create(metadata_json).expect("Cannot create metadata.json file");
+        std::fs::File::create(module_wasm_path).expect("Cannot create module.wasm file");
+
+        WasmModulePackage::from_dir(dir.path()).expect("Cannot create module package");
+    }
+}
