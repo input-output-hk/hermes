fc6cf0ad1cab7bcff3f509b3198c6402f698e769
Mr-Leshiy <leshiy12345678@gmail.com>
2024-05-21 12:17:52 +0300

    wip

--- hermes/bin/src/main.rs
+++ hermes/bin/src/main.rs
@@ -15,10 +15,31 @@ pub use wasm::module::bench::{
     module_small_component_full_pre_load_bench,
 };
 
+/// A parameter identifier specifying the log level.
+const ENV_LOG_LEVEL: &str = "HERMES_LOG_LEVEL";
+
 #[allow(clippy::exit)]
 fn main() {
+    use std::str::FromStr;
+
     use clap::Parser;
     use tracing::error;
+
+    use crate::logger::{self, LogLevel, LoggerConfigBuilder};
+
+    let log_level = if let Ok(log_level_str) = std::env::var(ENV_LOG_LEVEL) {
+        LogLevel::from_str(&log_level_str)?
+    } else {
+        LogLevel::default()
+    };
+
+    let log_config = LoggerConfigBuilder::default()
+        .log_level(log_level)
+        .with_thread(true)
+        .with_file(true)
+        .with_line_num(true)
+        .build();
+    logger::init(&log_config)?;
 
     if let Err(err) = cli::Cli::parse().exec() {
         error!("{err}");
