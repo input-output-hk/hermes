d9e5d5cf92b2d357d02b885637d4b38432c0822d
Mr-Leshiy <leshiy12345678@gmail.com>
2024-05-22 20:25:33 +0300

    wip

--- hermes/bin/src/packaging/wasm_module/manifest.rs
+++ hermes/bin/src/packaging/wasm_module/manifest.rs
@@ -1,8 +1,18 @@
 //! WASM module package manifet.json struct.
 
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 
 use serde::Deserialize;
+
+/// Manifest file open and read error.
+#[derive(thiserror::Error, Debug)]
+#[error("Cannot open and read WASM module package manifest file at {0}")]
+pub(crate) struct ManifestFileError(PathBuf);
+
+/// WASM module package manifest reading error.
+#[derive(thiserror::Error, Debug)]
+#[error("WASM module manifest json file reading error: {0}")]
+pub(crate) struct ManifestReadingError(String);
 
 /// WASM module package manifet.json definition.
 #[derive(Deserialize, Clone, Debug, PartialEq, Eq)]
@@ -33,10 +43,19 @@ impl Manifest {
     fn default_component_path() -> PathBuf {
         PathBuf::from("module.wasm")
     }
+
+    /// Create a Manifest from a path.
+    pub(crate) fn from_file<P: AsRef<Path>>(path_to_manifest: P) -> anyhow::Result<Self> {
+        let file = std::fs::File::open(&path_to_manifest)
+            .map_err(|_| ManifestFileError(path_to_manifest.as_ref().into()))?;
+        Ok(serde_json::from_reader(file).map_err(|err| ManifestReadingError(err.to_string()))?)
+    }
 }
 
 #[cfg(test)]
 mod tests {
+    use temp_dir::TempDir;
+
     use super::*;
 
     #[test]
@@ -86,5 +105,24 @@ mod tests {
             share: None,
         };
         assert_eq!(manifest, expected);
+    }
+
+    #[test]
+    fn manifest_from_path_test() {
+        let dir = TempDir::new().expect("cannot create temp dir");
+
+        let path = dir.path().join("manifest.json");
+        let manifest_json_data = serde_json::json!({}).to_string();
+        std::fs::write(&path, manifest_json_data).expect("Cannot create manifest.json file");
+
+        let manifest = Manifest::from_file(path).expect("Cannot create manifest");
+        assert_eq!(manifest, Manifest {
+            metadata: PathBuf::from("metadata.json"),
+            component: PathBuf::from("module.wasm"),
+            config: None,
+            config_schema: None,
+            settings_schema: None,
+            share: None,
+        });
     }
 }
