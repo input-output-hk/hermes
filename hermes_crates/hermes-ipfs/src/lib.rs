a11ca584d1e3f843d21683addb16136317149a8e
Joaquín Rosales <joaquin.rosales@iohk.io>
2024-05-31 21:42:46 -0600

    feat: add basic node with default options

--- hermes_crates/hermes-ipfs/src/lib.rs
+++ hermes_crates/hermes-ipfs/src/lib.rs
@@ -1,3 +1,46 @@
 //! Hermes IPFS
 //!
 //! Provides support for storage, and `PubSub` functionality.
+use std::ops::{Deref, DerefMut};
+
+use rust_ipfs::{Ipfs, UninitializedIpfsNoop};
+
+/// Hermes IPFS Node
+#[allow(dead_code)]
+pub struct HermesIpfs {
+    /// IPFS node
+    node: Ipfs,
+}
+
+impl HermesIpfs {
+    /// Start a new node.
+    ///
+    /// # Errors
+    ///
+    /// Returns an error if the IPFS daemon fails to start.
+    pub async fn new() -> anyhow::Result<Self> {
+        // TODO(saibatizoku):
+        let node = UninitializedIpfsNoop::new()
+            .with_default()
+            .with_mdns()
+            .with_relay(true)
+            .default_record_key_validator()
+            .start()
+            .await?;
+        Ok(HermesIpfs { node })
+    }
+}
+
+impl Deref for HermesIpfs {
+    type Target = Ipfs;
+
+    fn deref(&self) -> &Self::Target {
+        &self.node
+    }
+}
+
+impl DerefMut for HermesIpfs {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.node
+    }
+}
