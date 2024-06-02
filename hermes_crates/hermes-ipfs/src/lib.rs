a6affd8fbeafa8370b39aa4040e8042bf4f38699
Joaquín Rosales <joaquin.rosales@iohk.io>
2024-06-01 19:50:51 -0600

    feat: add method to add files to ipfs
    * feat: add thiserror crate to dependencies
    * feat: add Error for Hermes IPFS

--- hermes_crates/hermes-ipfs/src/lib.rs
+++ hermes_crates/hermes-ipfs/src/lib.rs
@@ -1,9 +1,12 @@
 //! Hermes IPFS
 //!
 //! Provides support for storage, and `PubSub` functionality.
-use std::ops::{Deref, DerefMut};
 
-use rust_ipfs::{Ipfs, UninitializedIpfsNoop};
+use futures::StreamExt;
+
+use rust_ipfs::{unixfs::UnixfsStatus, Ipfs, UninitializedIpfsNoop};
+
+pub use rust_ipfs::{path::IpfsPath, unixfs::AddOpt};
 
 /// Hermes IPFS Node
 #[allow(dead_code)]
@@ -20,27 +23,76 @@ impl HermesIpfs {
     /// Returns an error if the IPFS daemon fails to start.
     pub async fn new() -> anyhow::Result<Self> {
         // TODO(saibatizoku):
-        let node = UninitializedIpfsNoop::new()
-            .with_default()
-            .with_mdns()
-            .with_relay(true)
-            .default_record_key_validator()
-            .start()
-            .await?;
+        let node = UninitializedIpfsNoop::new().with_default().start().await?;
         Ok(HermesIpfs { node })
     }
-}
 
-impl Deref for HermesIpfs {
-    type Target = Ipfs;
+    /// Add a file to IPFS.
+    ///
+    /// # Parameters
+    ///
+    /// * `payload` The payload can be specified as a file path, as a stream of bytes, or as a
+    ///     named stream of bytes.
+    ///
+    ///     * File path types
+    ///         * `&str`
+    ///         * `String`
+    ///         * `&std::path::Path`
+    ///         * `std::path::PathBuf`
+    ///
+    ///     * Stream of bytes
+    ///         * `Vec<u8>`
+    ///         * `&static [u8]`
+    ///
+    ///     * Named stream of bytes
+    ///         * `(String, Vec<u8>)`
+    ///         * `(String, &static [u8])`
+    ///
+    /// # Errors
+    ///
+    /// Returns an error if the file fails to upload.
+    pub async fn add_file(&self, payload: impl Into<AddOpt>) -> anyhow::Result<IpfsPath> {
+        let mut stream = self.node.add_unixfs(payload);
+        let mut ipfs_path = None;
 
-    fn deref(&self) -> &Self::Target {
-        &self.node
+        while let Some(status) = stream.next().await {
+            match status {
+                UnixfsStatus::ProgressStatus {
+                    written,
+                    total_size,
+                } => match total_size {
+                    Some(size) => println!("{written} out of {size} stored"),
+                    None => println!("{written} been stored"),
+                },
+                UnixfsStatus::FailedStatus {
+                    written,
+                    total_size,
+                    error,
+                } => {
+                    match total_size {
+                        Some(size) => println!("failed with {written} out of {size} stored"),
+                        None => println!("failed with {written} stored"),
+                    }
+
+                    if let Some(error) = error {
+                        anyhow::bail!(error);
+                    }
+                    return Err(Error::AddFileFailure.into());
+                }
+                UnixfsStatus::CompletedStatus { path, written, .. } => {
+                    println!("{written} bytes stored with path {path}");
+                    ipfs_path = Some(path);
+                }
+            }
+        }
+        ipfs_path.ok_or(Error::AddFileFailure.into())
     }
 }
 
-impl DerefMut for HermesIpfs {
-    fn deref_mut(&mut self) -> &mut Self::Target {
-        &mut self.node
-    }
+/// Hermes IPFS Errors.
+#[derive(thiserror::Error, Debug)]
+enum Error {
+    /// File upload to IPFS failed.
+    #[error = "failed to add file to ipfs"]
+    AddFileFailure,
 }
