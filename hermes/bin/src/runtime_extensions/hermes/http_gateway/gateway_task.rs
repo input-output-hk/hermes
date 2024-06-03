960323827ed93fb28a61ea56cf3b354cd83a1b89
cong-or <conchubhar.gannon@gmail.com>
2024-06-03 15:25:55 +0100

    feat(barebones): http gateway

--- hermes/bin/src/runtime_extensions/hermes/http_gateway/gateway_task.rs
+++ hermes/bin/src/runtime_extensions/hermes/http_gateway/gateway_task.rs
@@ -1,15 +1,23 @@
 //! HTTP Gateway task
 
-use std::convert::Infallible;
+use std::{
+    collections::HashMap,
+    convert::Infallible,
+    sync::{Arc, Mutex},
+};
 
 use hyper::{
-    server::Server,
+    self,
+    server::{conn::AddrStream, Server},
     service::{make_service_fn, service_fn},
-    {self},
 };
 use tracing::{error, info};
 
 use super::routing::router;
+
+struct ConnectionManager {
+    pub connection_context: Mutex<HashMap<String, String>>,
+}
 
 /// Spawns a OS thread running the Tokio runtime task.
 pub fn spawn() {
@@ -20,6 +28,10 @@ pub fn spawn() {
 
 /// Starts the HTTP Gateway
 fn executor() {
+    let shared = Arc::new(ConnectionManager {
+        connection_context: Mutex::new(HashMap::new()),
+    });
+
     let res = tokio::runtime::Builder::new_current_thread()
         .enable_io()
         .enable_time()
@@ -37,8 +49,27 @@ fn executor() {
 
     rt.block_on(async move {
         let addr = ([127, 0, 0, 1], 5000).into();
-        let gateway_service =
-            make_service_fn(|_| async { Ok::<_, Infallible>(service_fn(router)) });
+
+        let gateway_service = make_service_fn(|client: &AddrStream| {
+            let connection_manager = shared.clone();
+
+            match connection_manager.connection_context.try_lock() {
+                Ok(mut context) => {
+                    context.insert(
+                        client.remote_addr().to_string(),
+                        rusty_ulid::generate_ulid_string(),
+                    );
+                },
+                Err(err) => error!(
+                    "Unable to record connection state for {:?} {:?}",
+                    client.remote_addr().to_string(),
+                    err
+                ),
+            }
+
+            async move { Ok::<_, Infallible>(service_fn(router)) }
+        });
+
         Server::bind(&addr)
             .serve(gateway_service)
             .await
