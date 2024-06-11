657a1839a9018ccb4e91457178e5d2a4de0d5f62
Mr-Leshiy <leshiy12345678@gmail.com>
2024-06-11 14:37:38 +0300

    update sign cli command

---
+++ hermes/bin/src/cli/module/sign.rs
@@ -0,0 +1,29 @@
+//! WASM module sign command
+
+use std::path::PathBuf;
+
+use clap::Args;
+use console::Emoji;
+
+/// WASM module package signing
+#[derive(Args)]
+pub(crate) struct SignCommand {
+    /// Defines the location of the builded WASM module package.
+    package: PathBuf,
+
+    /// Defines the location of the ED2559 private key associated with the signing key.
+    private_key: PathBuf,
+
+    /// Defines the location of the x.509 certificate associated with the signing key.
+    cert: PathBuf,
+}
+
+impl SignCommand {
+    /// Run cli command
+    pub(crate) fn exec(self) -> anyhow::Result<()> {
+        println!("{} Sign wasm module package...", Emoji::new("📝", ""));
+
+        println!("{} Done", Emoji::new("✅", ""));
+        Ok(())
+    }
+}
