d240f7ad8fecac2987d833fadb1a02797b210fa5
Mr-Leshiy <leshiy12345678@gmail.com>
2024-06-12 17:57:17 +0300

    add keys module

---
+++ hermes/bin/src/sign/keys.rs
@@ -0,0 +1,102 @@
+//! ED25519 public and private key implementation.
+
+use std::{
+    fmt::Display,
+    path::{Path, PathBuf},
+};
+
+use ed25519::pkcs8::DecodePublicKey;
+use ed25519_dalek::{pkcs8::DecodePrivateKey, SigningKey, VerifyingKey};
+
+/// Public or private key file open and read error.
+#[derive(thiserror::Error, Debug)]
+pub(crate) struct KeyFileError(PathBuf, Option<anyhow::Error>);
+impl Display for KeyFileError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let msg = format!("Cannot open and read key file at {0}.", self.0.display());
+        let err = self
+            .1
+            .as_ref()
+            .map(|msg| format!("error: {msg}\n"))
+            .unwrap_or_default();
+        write!(f, "{msg}\n{err}",)
+    }
+}
+
+/// Public or private key decoding from string error.
+#[derive(thiserror::Error, Debug)]
+#[error("Cannot decode key from string. Invalid PEM format.")]
+pub(crate) struct KeyDecodingError;
+
+/// Ed25519 private key
+pub(crate) struct PrivateKey {
+    /// `ed25519_dalek::SigningKey` key value
+    key: SigningKey,
+}
+
+impl PrivateKey {
+    /// Create new private key from file decoded in PEM format
+    pub(crate) fn from_file<P: AsRef<Path>>(path: P) -> anyhow::Result<Self> {
+        let str =
+            std::fs::read_to_string(&path).map_err(|_| KeyFileError(path.as_ref().into(), None))?;
+        Ok(Self::from_str(&str).map_err(|err| KeyFileError(path.as_ref().into(), Some(err)))?)
+    }
+
+    /// Create new private key from string decoded in PEM format
+    pub(crate) fn from_str(str: &str) -> anyhow::Result<Self> {
+        let key = SigningKey::from_pkcs8_pem(str).map_err(|_| KeyDecodingError)?;
+        Ok(Self { key })
+    }
+}
+
+/// Ed25519 public key
+pub(crate) struct PublicKey {
+    /// `ed25519_dalek::VerifyingKey` key value
+    key: VerifyingKey,
+}
+
+impl PublicKey {
+    /// Create new private key from file decoded in PEM format
+    pub(crate) fn from_file<P: AsRef<Path>>(path: P) -> anyhow::Result<Self> {
+        let str =
+            std::fs::read_to_string(&path).map_err(|_| KeyFileError(path.as_ref().into(), None))?;
+        Ok(Self::from_str(&str).map_err(|err| KeyFileError(path.as_ref().into(), Some(err)))?)
+    }
+
+    /// Create new private key from string decoded in PEM format
+    pub(crate) fn from_str(str: &str) -> anyhow::Result<Self> {
+        let key = VerifyingKey::from_public_key_pem(str).map_err(|_| KeyDecodingError)?;
+        Ok(Self { key })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use temp_dir::TempDir;
+
+    use super::*;
+
+    #[test]
+    fn private_key_from_file_test() {
+        let dir = TempDir::new().expect("cannot create temp dir");
+
+        let private_key_path = dir.path().join("private.pem");
+        let private_key = "-----BEGIN PRIVATE KEY-----\nMC4CAQAwBQYDK2VwBCIEIP1iI3LF7h89yY6QZmhDp4Y5FmTQ4oasbz2lEiaqqTzV\n-----END PRIVATE KEY-----";
+        std::fs::write(&private_key_path, private_key).expect("Cannot create private.pem file");
+
+        let _key =
+            PrivateKey::from_file(private_key_path).expect("Cannot create private key from file");
+    }
+
+    #[test]
+    fn public_key_from_file_test() {
+        let dir = TempDir::new().expect("cannot create temp dir");
+
+        let private_key_path = dir.path().join("private.pem");
+        let private_key = "-----BEGIN PUBLIC KEY-----\nMCowBQYDK2VwAyEAtFuCleJwHS28jUCT+ulLl5c1+MXhehhDz2SimOhmWaI=\n-----END PUBLIC KEY-----";
+        std::fs::write(&private_key_path, private_key).expect("Cannot create private.pem file");
+
+        let _key =
+            PublicKey::from_file(private_key_path).expect("Cannot create private key from file");
+    }
+}
--- hermes/bin/src/sign/mod.rs
+++ hermes/bin/src/sign/mod.rs
@@ -1,1 +1,5 @@
-//! Hermes signing module.
+//! Hermes signing procedure implementation.
+//! Follows documented spec `https://input-output-hk.github.io/hermes/architecture/08_concepts/hermes_signing_procedure`.
+
+#[allow(dead_code)]
+pub(crate) mod keys;
