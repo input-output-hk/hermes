865bebfdab2fcfa9fdf41b72c882373154090854
Apisit Ritreungroj <38898766+apskhem@users.noreply.github.com>
2024-06-18 19:10:23 +0700

    feat(sqlite): SQLite Module for Hermes (#248)
    * feat: initial commit
    
    * feat: complete setup
    
    * feat: init engine
    
    * feat: simple cases
    
    * feat: internal state
    
    * chore: remove println
    
    * chore: lintfix
    
    * feat: tests
    
    * feat: bench cases
    
    * fix: cspell
    
    * chore: fmt
    
    * refactor: split file
    
    * refactor: simplify functions
    
    * refactor: minor
    
    * chore: fmtfix
    
    * docs: readme
    
    * chore: mdfix
    
    * refactor: changing allocate to add
    
    * chore: cat-ci version dump
    
    * fix: update deny.toml
    
    * fix: echo
    
    * fix: comment
    
    * fix: ci new version
    
    * refactor: grouping test items
    
    * fix: docs and types

---
+++ wasm/integration-test/sqlite/src/lib.rs
@@ -0,0 +1,90 @@
+//! Hermes SQLite module integration test with WASM runtime.
+//! Generate `hermes.rs` with `earthly +gen-bindings` before writing the test.
+
+// Allow everything since this is generated code.
+#[allow(clippy::all, unused)]
+mod hermes;
+mod test;
+
+use hermes::{
+    exports::hermes::integration_test::event::TestResult,
+    hermes::{
+        cardano::api::{BlockSrc, CardanoBlock, CardanoBlockchainId, CardanoTxn},
+        cron::api::CronTagged,
+        kv_store::api::KvValues,
+        sqlite,
+    },
+    wasi::http::types::{IncomingRequest, ResponseOutparam},
+};
+use test::*;
+
+struct TestComponent;
+
+impl hermes::exports::hermes::integration_test::event::Guest for TestComponent {
+    fn test(test: u32, run: bool) -> Option<TestResult> {
+        TESTS.get(test as usize).map(|item| TestResult {
+            name: String::from(item.name),
+            status: {
+                if run {
+                    (item.executor)().is_ok()
+                } else {
+                    true
+                }
+            },
+        })
+    }
+
+    fn bench(test: u32, run: bool) -> Option<TestResult> {
+        BENCHES.get(test as usize).map(|item| TestResult {
+            name: String::from(item.name),
+            status: {
+                if run {
+                    (item.executor)().is_ok()
+                } else {
+                    true
+                }
+            },
+        })
+    }
+}
+
+impl hermes::exports::hermes::cardano::event_on_block::Guest for TestComponent {
+    fn on_cardano_block(_blockchain: CardanoBlockchainId, _block: CardanoBlock, _source: BlockSrc) {
+    }
+}
+
+impl hermes::exports::hermes::cardano::event_on_rollback::Guest for TestComponent {
+    fn on_cardano_rollback(_blockchain: CardanoBlockchainId, _slot: u64) {}
+}
+
+impl hermes::exports::hermes::cardano::event_on_txn::Guest for TestComponent {
+    fn on_cardano_txn(
+        _blockchain: CardanoBlockchainId,
+        _slot: u64,
+        _txn_index: u32,
+        _txn: CardanoTxn,
+    ) {
+    }
+}
+
+impl hermes::exports::hermes::cron::event::Guest for TestComponent {
+    fn on_cron(_event: CronTagged, _last: bool) -> bool {
+        false
+    }
+}
+
+impl hermes::exports::hermes::init::event::Guest for TestComponent {
+    fn init() -> bool {
+        true
+    }
+}
+
+impl hermes::exports::hermes::kv_store::event::Guest for TestComponent {
+    fn kv_update(_key: String, _value: KvValues) {}
+}
+
+impl hermes::exports::wasi::http::incoming_handler::Guest for TestComponent {
+    fn handle(_request: IncomingRequest, _response_out: ResponseOutparam) {}
+}
+
+hermes::export!(TestComponent with_types_in hermes);
