13e6fd3799de622de956f11fc5b3914bee3cd221
Mr-Leshiy <leshiy12345678@gmail.com>
2024-06-20 13:05:12 +0300

    add signing functionality to keys

--- hermes/bin/src/sign/keys.rs
+++ hermes/bin/src/sign/keys.rs
@@ -6,6 +6,7 @@ use std::{
 };
 
 use ed25519_dalek::{
+    ed25519::signature::Signer,
     pkcs8::{DecodePrivateKey, DecodePublicKey},
     SigningKey, VerifyingKey,
 };
@@ -53,6 +54,12 @@ impl PrivateKey {
     pub(crate) fn public_key(&self) -> PublicKey {
         PublicKey(self.0.verifying_key())
     }
+
+    /// Sign the message with the current private key.
+    /// Returns the signature bytes.
+    pub(crate) fn sign(&self, msg: &[u8]) -> Vec<u8> {
+        self.0.sign(msg).to_vec()
+    }
 }
 
 /// Ed25519 public key instance.
@@ -78,6 +85,21 @@ impl PublicKey {
     pub(crate) fn from_bytes(bytes: &[u8]) -> anyhow::Result<Self> {
         let key = VerifyingKey::from_bytes(bytes.try_into()?)?;
         Ok(Self(key))
+    }
+
+    /// Verify signature of the message with the current public key.
+    /// Returns `Ok(())` if the signature is valid, `Err` otherwise.
+    pub(crate) fn verify(&self, msg: &[u8], signature_bytes: &[u8]) -> anyhow::Result<()> {
+        let signature_bytes = signature_bytes.try_into().map_err(|_| {
+            anyhow::anyhow!(
+                "Invalid signature bytes size: expected {}, provided {}.",
+                ed25519_dalek::Signature::BYTE_SIZE,
+                signature_bytes.len()
+            )
+        })?;
+        let signature = ed25519_dalek::Signature::from_bytes(signature_bytes);
+        self.0.verify_strict(msg, &signature)?;
+        Ok(())
     }
 }
 
@@ -140,5 +162,29 @@ mod tests {
         let public_key = PublicKey::from_str(&public_key).expect("Cannot create public key");
 
         assert_eq!(private_key.public_key(), public_key);
+    }
+
+    #[test]
+    fn sign_test() {
+        let private_key = format!(
+            "{}\n{}\n{}",
+            "-----BEGIN PRIVATE KEY-----",
+            "MC4CAQAwBQYDK2VwBCIEIP1iI3LF7h89yY6QZmhDp4Y5FmTQ4oasbz2lEiaqqTzV",
+            "-----END PRIVATE KEY-----"
+        );
+        let public_key = format!(
+            "{}\n{}\n{}",
+            "-----BEGIN PUBLIC KEY-----",
+            "MCowBQYDK2VwAyEAtFuCleJwHS28jUCT+ulLl5c1+MXhehhDz2SimOhmWaI=",
+            "-----END PUBLIC KEY-----"
+        );
+
+        let private_key = PrivateKey::from_str(&private_key).expect("Cannot create private key");
+        let public_key = PublicKey::from_str(&public_key).expect("Cannot create public key");
+
+        let msg = b"test";
+
+        let signature = private_key.sign(msg);
+        assert!(public_key.verify(msg, &signature).is_ok());
     }
 }
