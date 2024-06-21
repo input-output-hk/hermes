16a914a0bf1831dcf0fe7112035010d980983c9c
Mr-Leshiy <leshiy12345678@gmail.com>
2024-06-21 16:00:47 +0300

    fix tests

--- hermes/bin/src/sign/hash.rs
+++ hermes/bin/src/sign/hash.rs
@@ -101,8 +101,8 @@ mod tests {
 
     #[test]
     fn hasher_test() {
-        let hasher = Blake2b256Hasher::new();
-        // hasher.update(b"test");
+        let mut hasher = Blake2b256Hasher::new();
+        hasher.update(b"test");
         let hash = hasher.finalize();
         assert_eq!(
             "928b20366943e2afd11ebc0eae2e53a93bf177a4fcf35bcc64d503704e65e202",
