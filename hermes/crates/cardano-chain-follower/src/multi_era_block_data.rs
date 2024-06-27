43851bb43c6f198d65888f7d918e1a63a72ed4be
Mr-Leshiy <leshiy12345678@gmail.com>
2024-06-27 15:35:16 +0300

    add test

--- hermes/crates/cardano-chain-follower/src/multi_era_block_data.rs
+++ hermes/crates/cardano-chain-follower/src/multi_era_block_data.rs
@@ -30,7 +30,7 @@ impl MultiEraBlock {
     /// # Errors
     ///
     /// If the given bytes cannot be decoded as a multi-era block, an error is returned.
-    pub fn new<'a>(raw_data: Vec<u8>) -> Result<Self, Error> {
+    pub fn new(raw_data: Vec<u8>) -> Result<Self, Error> {
         let builder = SelfReferencedMultiEraBlockBuilder {
             raw_data,
             block_builder: |raw_data| pallas::ledger::traverse::MultiEraBlock::decode(raw_data),
@@ -48,9 +48,36 @@ impl MultiEraBlock {
     ///
     /// If the data has changed between the creation of this `MultiEraBlockData` and now,
     /// it may panic.
+    #[must_use]
     pub fn decode(&self) -> &pallas::ledger::traverse::MultiEraBlock {
         #[allow(clippy::unwrap_used)]
         // We checked the block before during construction, so it is safe to unwrap.
         self.0.borrow_block().as_ref().unwrap()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn multi_era_block_test() {
+        let blocks_hex_bytes = vec![
+            include_str!("./../test_data/byron.block"),
+            include_str!("./../test_data/shelley.block"),
+            include_str!("./../test_data/mary.block"),
+            include_str!("./../test_data/allegra.block"),
+            include_str!("./../test_data/alonzo.block"),
+        ];
+
+        for block_hex_bytes in blocks_hex_bytes {
+            let block_bytes = hex::decode(block_hex_bytes).expect("Failed to decode hex block.");
+            let block = MultiEraBlock::new(block_bytes.clone()).expect("Failed to decode block.");
+            let pallas_block =
+                pallas::ledger::traverse::MultiEraBlock::decode(block_bytes.as_slice())
+                    .expect("Failed to decode pallas block.");
+
+            assert_eq!(block.decode().hash(), pallas_block.hash());
+        }
     }
 }
