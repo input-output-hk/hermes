8afd3d6bbff41f0574fa307c738aa57fd33d0952
cong-or <conchubhar.gannon@gmail.com>
2024-07-03 15:56:27 +0100

    ci(c bindings): c

--- wasm/stub-module/stub-module.c
+++ wasm/stub-module/stub-module.c
@@ -27,9 +27,10 @@ bool exports_hermes_cron_event_on_cron(exports_hermes_cron_event_cron_tagged_t *
 
 
 // Exported Functions from `hermes:http-gateway/event`
-exports_hermes_http_gateway_event_http_response_t exports_hermes_http_gateway_event_reply(exports_hermes_http_gateway_event_bstr_t *body, exports_hermes_http_gateway_event_headers_t *headers, hermes_string_t *path, hermes_string_t *method){
- return *ret;
-}
+bool exports_hermes_http_gateway_event_reply(exports_hermes_http_gateway_event_bstr_t *body, exports_hermes_http_gateway_event_headers_t *headers, hermes_string_t *path, hermes_string_t *method, exports_hermes_http_gateway_event_http_response_t *ret){
+  return false;
+};
+
 
 // Exported Functions from `hermes:init/event`
 bool exports_hermes_init_event_init(void) {
