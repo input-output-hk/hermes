bf7ea17b434d780b1a01686820dddad8151e7ec2
Mr-Leshiy <leshiy12345678@gmail.com>
2024-07-23 13:34:28 +0300

    update Run cli command

--- hermes/bin/src/cli/run.rs
+++ hermes/bin/src/cli/run.rs
@@ -2,6 +2,7 @@
 
 use std::path::PathBuf;
 
+use clap::Args;
 use console::Emoji;
 
 use crate::{
@@ -16,20 +17,30 @@ use crate::{
 };
 
 /// Run cli command
-pub(crate) struct Run;
+#[derive(Args)]
+pub(crate) struct Run {
+    /// Path to the Hermes application package to run
+    app_package: PathBuf,
+
+    /// Path to the trusted certificate
+    #[clap(name = "cert", short)]
+    certificates: Vec<PathBuf>,
+
+    /// Flag which disables package signature verification
+    #[clap(long, action = clap::ArgAction::SetTrue)]
+    untrusted: bool,
+}
 
 impl Run {
     /// Run the hermes application
-    pub(crate) fn exec(
-        app_package: PathBuf, certificates: Vec<PathBuf>, untrusted: bool,
-    ) -> anyhow::Result<()> {
-        for cert_path in certificates {
+    pub(crate) fn exec(self) -> anyhow::Result<()> {
+        for cert_path in self.certificates {
             let cert = Certificate::from_file(cert_path)?;
             certificate::storage::add_certificate(cert)?;
         }
 
-        let package = ApplicationPackage::from_file(app_package)?;
-        package.validate(untrusted)?;
+        let package = ApplicationPackage::from_file(self.app_package)?;
+        package.validate(self.untrusted)?;
 
         let app_name = package.get_metadata()?.get_name()?;
 
