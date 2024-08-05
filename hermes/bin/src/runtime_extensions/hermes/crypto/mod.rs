4ec16b03bb89304d80d53cd3d4fa23c09ce5ef2b
Mr-Leshiy <leshiy12345678@gmail.com>
2024-08-05 16:48:22 +0300

    refactor

--- hermes/bin/src/runtime_extensions/hermes/crypto/mod.rs
+++ hermes/bin/src/runtime_extensions/hermes/crypto/mod.rs
@@ -3,7 +3,6 @@
 mod bip32_ed25519;
 mod bip39;
 mod host;
-mod state;
 
 /// Advise Runtime Extensions of a new context
 pub(crate) fn new_context(_ctx: &crate::runtime_context::HermesRuntimeContext) {}
--- hermes/bin/src/runtime_extensions/resource_manager.rs
+++ hermes/bin/src/runtime_extensions/resource_manager.rs
@@ -1,7 +1,5 @@
 //! Generalized, type safe `wasmtime::component::Resource<T>` manager implementation.
 
-#![allow(dead_code)]
-
 use std::sync::atomic::{AtomicU32, Ordering};
 
 use dashmap::DashMap;
@@ -80,12 +78,8 @@ where WitType: 'static
 /// - `RustType` actually the type which is binded to the `WitType` and holds all the data
 ///   needed for the `WitType`.
 pub(crate) struct ApplicationResourceManager<WitType, RustType> {
-    /// Map of id to resource object.
-    state: DashMap<(ApplicationName, u32), RustType>,
-    /// Next available address id of the resource.
-    available_address: AtomicU32,
-    /// `WitType` type phantom.
-    _phantom: std::marker::PhantomData<WitType>,
+    /// Map of app name to resources.
+    state: DashMap<ApplicationName, ResourceManager<WitType, RustType>>,
 }
 
 impl<WitType, RustType> ApplicationResourceManager<WitType, RustType>
@@ -95,8 +89,6 @@ where WitType: 'static
     pub(crate) fn new() -> Self {
         Self {
             state: DashMap::new(),
-            available_address: AtomicU32::default(),
-            _phantom: std::marker::PhantomData,
         }
     }
 
@@ -105,18 +97,8 @@ where WitType: 'static
     pub(crate) fn create_resource(
         &self, app_name: ApplicationName, object: RustType,
     ) -> wasmtime::component::Resource<WitType> {
-        let mut available_address = self.available_address.load(Ordering::Acquire);
-        self.state.insert((app_name, available_address), object);
-
-        // Increment the value of the available address to 1,
-        // so that it can be used for the next resource.
-        // Under the assumption that `ResourceManager` will not handle too many resources at once,
-        // and will not hold resources for too long, saturating increment is safe.
-        available_address = available_address.saturating_add(1);
-        self.available_address
-            .store(available_address, Ordering::Release);
-
-        wasmtime::component::Resource::new_own(available_address)
+        let app_state = self.state.entry(app_name).or_insert(ResourceManager::new());
+        app_state.create_resource(object)
     }
 
     /// Creates a new owned resource from the given object.
@@ -125,10 +107,8 @@ where WitType: 'static
         &self, app_name: ApplicationName, resource: &wasmtime::component::Resource<WitType>,
     ) -> wasmtime::Result<RustType>
     where RustType: Clone {
-        self.state
-            .get(&(app_name, resource.rep()))
-            .map(|r| r.value().clone())
-            .ok_or(wasmtime::Error::msg("Resource not found"))
+        let app_state = self.state.entry(app_name).or_insert(ResourceManager::new());
+        app_state.get_object(resource)
     }
 
     /// Removes the resource from the resource manager.
@@ -138,6 +118,7 @@ where WitType: 'static
     pub(crate) fn delete_resource(
         &self, app_name: ApplicationName, resource: wasmtime::component::Resource<WitType>,
     ) {
-        self.state.remove(&(app_name, resource.rep()));
+        let app_state = self.state.entry(app_name).or_insert(ResourceManager::new());
+        app_state.delete_resource(resource);
     }
 }
