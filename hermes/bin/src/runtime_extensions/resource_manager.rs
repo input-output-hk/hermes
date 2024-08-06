714d185fffa3f51419ef45a8fc98cc203d05dfc7
Mr-Leshiy <leshiy12345678@gmail.com>
2024-08-06 17:05:29 +0300

    add resource manager tests, fix issue

--- hermes/bin/src/runtime_extensions/resource_manager.rs
+++ hermes/bin/src/runtime_extensions/resource_manager.rs
@@ -37,16 +37,15 @@ where WitType: 'static
     pub(crate) fn create_resource(
         &self, object: RustType,
     ) -> wasmtime::component::Resource<WitType> {
-        let mut available_address = self.available_address.load(Ordering::Acquire);
+        let available_address = self.available_address.load(Ordering::Acquire);
         self.state.insert(available_address, object);
 
         // Increment the value of the available address to 1,
         // so that it can be used for the next resource.
         // Under the assumption that `ResourceManager` will not handle too many resources at once,
         // and will not hold resources for too long, saturating increment is safe.
-        available_address = available_address.saturating_add(1);
         self.available_address
-            .store(available_address, Ordering::Release);
+            .store(available_address.saturating_add(1), Ordering::Release);
 
         wasmtime::component::Resource::new_own(available_address)
     }
@@ -140,5 +139,60 @@ where WitType: 'static
             .or_insert(ResourceManager::new())
             .downgrade();
         app_state.delete_resource(resource)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    struct WitType;
+
+    #[test]
+    #[allow(clippy::unwrap_used)]
+    fn test_resource_manager() {
+        let resource_manager = ResourceManager::<WitType, u32>::new();
+
+        let object = 100;
+        let resource = resource_manager.create_resource(object);
+        let copied_resource = wasmtime::component::Resource::new_borrow(resource.rep());
+
+        assert_eq!(resource_manager.get_object(&resource).unwrap(), object);
+        assert!(resource_manager.delete_resource(resource).is_ok());
+        assert!(resource_manager.get_object(&copied_resource).is_err());
+        assert!(resource_manager.delete_resource(copied_resource).is_err());
+    }
+
+    #[test]
+    #[allow(clippy::unwrap_used)]
+    fn test_app_resource_manager() {
+        let resource_manager = ApplicationResourceManager::<WitType, u32>::new();
+        let app_name_1 = ApplicationName("app_1".to_string());
+        let app_name_2 = ApplicationName("app_2".to_string());
+
+        let object = 100;
+        let resource = resource_manager.create_resource(app_name_1.clone(), object);
+        let copied_resource = wasmtime::component::Resource::new_borrow(resource.rep());
+
+        assert_eq!(
+            resource_manager
+                .get_object(app_name_1.clone(), &resource)
+                .unwrap(),
+            object
+        );
+        assert!(resource_manager
+            .get_object(app_name_2.clone(), &resource)
+            .is_err(),);
+
+        assert!(resource_manager
+            .delete_resource(app_name_1.clone(), resource)
+            .is_ok());
+
+        assert!(resource_manager
+            .get_object(app_name_1.clone(), &copied_resource)
+            .is_err());
+        assert!(resource_manager
+            .delete_resource(app_name_1.clone(), copied_resource)
+            .is_err());
     }
 }
