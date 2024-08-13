beb2c4329ae42c3021d7c152805d215fa29085bf
Mr-Leshiy <leshiy12345678@gmail.com>
2024-08-13 13:17:43 +0300

    fix input streams state initialization

--- hermes/bin/src/runtime_extensions/wasi/io/streams/mod.rs
+++ hermes/bin/src/runtime_extensions/wasi/io/streams/mod.rs
@@ -6,4 +6,6 @@ mod state;
 pub(crate) use state::get_intput_streams_state;
 
 /// Advise Runtime Extensions of a new context
-pub(crate) fn new_context(_ctx: &crate::runtime_context::HermesRuntimeContext) {}
+pub(crate) fn new_context(ctx: &crate::runtime_context::HermesRuntimeContext) {
+    get_intput_streams_state().add_app(ctx.app_name().clone());
+}
