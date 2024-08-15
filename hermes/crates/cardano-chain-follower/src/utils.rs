0b14842e0dad143f14676a72ba14f252b8839e34
bkioshn <bkioshn@gmail.com>
2024-08-15 12:53:42 +0700

    fix: update utils

--- hermes/crates/cardano-chain-follower/src/utils.rs
+++ hermes/crates/cardano-chain-follower/src/utils.rs
@@ -47,3 +47,12 @@ pub(crate) fn blake2b_256(value: &[u8]) -> anyhow::Result<[u8; 32]> {
     b.try_into()
         .map_err(|_| anyhow::anyhow!("Invalid length of blake2b_256, expected 32 got {}", b.len()))
 }
+
+#[allow(dead_code)]
+/// Convert the given value to `blake2b_128` array.
+pub(crate) fn blake2b_128(value: &[u8]) -> anyhow::Result<[u8; 16]> {
+    let h = Params::new().hash_length(16).hash(value);
+    let b = h.as_bytes();
+    b.try_into()
+        .map_err(|_| anyhow::anyhow!("Invalid length of blake2b_128, expected 16 got {}", b.len()))
+}
