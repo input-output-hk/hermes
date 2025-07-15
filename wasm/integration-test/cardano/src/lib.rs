2beb9249d5c7ce67fa88d00728bcef3e6f3da95d
bkioshn <bkioshn@gmail.com>
2025-07-15 15:08:35 +0700

    fix: cardano integration test
    Signed-off-by: bkioshn <bkioshn@gmail.com>

--- wasm/integration-test/cardano/src/lib.rs
+++ wasm/integration-test/cardano/src/lib.rs
@@ -1,26 +1,23 @@
-#[allow(clippy::all, unused)]
+#![allow(clippy::all, unused)]
 mod hermes;
 
-use pallas_traverse::tx;
-
-use crate::hermes::{exports::hermes::cardano::event_on_block::SubscriptionId, hermes::cardano};
+use crate::hermes::hermes::cardano;
 
 struct TestComponent;
 
 fn test_get_data() -> bool {
     let slot = 91210205;
     let index = 0;
-    let tx_hash = hex::encode("ef831f1d3ce6134628e39d288876408fac5f81840b9d74b9ba07f36e56090bb1")
-        .expect("Invalid transaction hash");
+    let tx_hash = "ef831f1d3ce6134628e39d288876408fac5f81840b9d74b9ba07f36e56090bb1";
 
     let Ok(network_resource) = cardano::api::Network::new(cardano::api::CardanoNetwork::Preprod)
     else {
         return false;
     };
-    let Some(block_resource) = network_resource.get_block(Some(slot), 0) else {
+    let Some(block_resource) = network_resource.get_block(Some(slot), index) else {
         return false;
     };
-    let Some(tx_resource) = block_resource.get_txn(0) else {
+    let Some(tx_resource) = block_resource.get_txn(index as u16) else {
         return false;
     };
     let hash = if let Some(tx) = tx_resource.get_txn_hash() {
@@ -29,7 +26,9 @@ fn test_get_data() -> bool {
         return false;
     };
 
-    let tx_check = tx_resource.get_metadata().is_some() && hash == tx_hash;
+    let encode_hash = hex::encode(hash);
+
+    let tx_check = tx_resource.get_metadata().is_some() && encode_hash == tx_hash;
     let network_check = network_resource.get_tips().is_some();
     let block_check = block_resource.is_immutable() == true
         && block_resource.get_slot() == slot
@@ -38,8 +37,13 @@ fn test_get_data() -> bool {
     network_check && block_check && tx_check
 }
 
-fn tuple_to_array(t: (u64, u64, u64, u64)) -> [u64; 4] {
-    [t.0, t.1, t.2, t.3]
+fn tuple_to_array(t: (u64, u64, u64, u64)) -> [u8; 32] {
+    let mut array = [0u8; 32];
+    array[0..8].copy_from_slice(&t.0.to_le_bytes());
+    array[8..16].copy_from_slice(&t.1.to_le_bytes());
+    array[16..24].copy_from_slice(&t.2.to_le_bytes());
+    array[24..32].copy_from_slice(&t.3.to_le_bytes());
+    array
 }
 
 fn test_subscribe_block() -> bool {
@@ -50,7 +54,10 @@ fn test_subscribe_block() -> bool {
     let Ok(id) = network_resource.subscribe_block(cardano::api::SyncSlot::Genesis) else {
         return false;
     };
-    id.get_network() == cardano::api::CardanoNetwork::Preprod
+    match id.get_network() {
+        cardano::api::CardanoNetwork::Preprod => true,
+        _ => false,
+    }
 }
 
 impl hermes::exports::hermes::integration_test::event::Guest for TestComponent {
@@ -117,8 +124,8 @@ impl hermes::exports::hermes::http_gateway::event::Guest for TestComponent {
     fn reply(
         body: hermes::exports::hermes::http_gateway::event::Bstr,
         headers: hermes::exports::hermes::http_gateway::event::Headers,
-        path: _rt::String,
-        method: _rt::String,
+        path: String,
+        method: String,
     ) -> Option<hermes::exports::hermes::http_gateway::event::HttpResponse> {
         None
     }
@@ -137,7 +144,7 @@ impl hermes::exports::hermes::ipfs::event::Guest for TestComponent {
 }
 
 impl hermes::exports::hermes::kv_store::event::Guest for TestComponent {
-    fn kv_update(key: _rt::String, value: hermes::exports::hermes::kv_store::event::KvValues) {}
+    fn kv_update(key: String, value: hermes::exports::hermes::kv_store::event::KvValues) {}
 }
 
 impl hermes::exports::wasi::http::incoming_handler::Guest for TestComponent {
