c363e22714e0d08398f599e5f44b19f44ca14142
no30bit <artur.helmanau@innowise.com>
2025-07-21 15:24:07 +0300

    remove done_with_code

--- hermes/bin/src/runtime_extensions/hermes/init/host.rs
+++ hermes/bin/src/runtime_extensions/hermes/init/host.rs
@@ -7,17 +7,8 @@ use crate::{
 };
 
 impl Host for HermesRuntimeContext {
-    /// Perform Hermes event queue shut down.
-    fn done(&mut self, status: Result<(), ()>) -> wasmtime::Result<()> {
-        if status.is_ok() {
-            event::queue::shutdown(ExitCode::SUCCESS)
-        } else {
-            event::queue::shutdown(ExitCode::FAILURE)
-        }
-    }
-
-    /// Perform Hermes event queue shutdown with a custom status code.
-    fn done_with_code(&mut self, status_code: u8) -> wasmtime::Result<()> {
+    /// Perform Hermes event queue shutdown.
+    fn done(&mut self, status_code: u8) -> wasmtime::Result<()> {
         event::queue::shutdown(ExitCode::from(status_code))
     }
 }
