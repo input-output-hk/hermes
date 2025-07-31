2fd148b62f5655d695e75dba4b7308e57b9a31a1
bkioshn <bkioshn@gmail.com>
2025-07-31 19:15:35 +0700

    test: update cardano test
    Signed-off-by: bkioshn <bkioshn@gmail.com>

--- wasm/examples/rust/cardano/src/lib.rs
+++ wasm/examples/rust/cardano/src/lib.rs
@@ -10,18 +10,23 @@ impl hermes::exports::hermes::cardano::event_on_block::Guest for TestComponent {
         subscription_id: &hermes::exports::hermes::cardano::event_on_block::SubscriptionId,
         block: &hermes::exports::hermes::cardano::event_on_block::Block,
     ) {
+        let mut txn_hash = None;
         let slot = block.get_slot();
         let is_immutable = block.is_immutable();
         let is_rollback = block.is_rollback();
         let network = subscription_id.get_network();
+        if let Ok(txn) = block.get_txn(0) {
+            txn_hash = txn.get_txn_hash();
+        }
+
         hermes::hermes::logging::api::log(
-            hermes::hermes::logging::api::Level::Trace,
+            hermes::hermes::logging::api::Level::Warn,
             None,
             None,
             None,
             None,
             None,
-            format!("✈️ - on_cardano_block event trigger - subscription ID: {subscription_id:?}, network: {network:?}, slot: {slot:?}, is rollback: {is_rollback}, is immutable: {is_immutable}").as_str(),
+            format!("✈️ - on_cardano_block event trigger - subscription ID: {subscription_id:?}, network: {network:?}, slot: {slot:?}, is rollback: {is_rollback:?}, is immutable: {is_immutable}, txn hash: {txn_hash:?}").as_str(),
             None,
         );
     }
@@ -46,6 +51,7 @@ impl hermes::exports::hermes::cardano::event_on_immutable_roll_forward::Guest fo
         );
     }
 }
+
 impl hermes::exports::hermes::init::event::Guest for TestComponent {
     fn init() -> bool {
         hermes::hermes::logging::api::log(
@@ -59,7 +65,7 @@ impl hermes::exports::hermes::init::event::Guest for TestComponent {
             None,
         );
 
-        let subscribe_from = hermes::hermes::cardano::api::SyncSlot::Genesis;
+        let subscribe_from = hermes::hermes::cardano::api::SyncSlot::Tip;
         let network = hermes::hermes::cardano::api::CardanoNetwork::Preview;
 
         let network_resource = hermes::hermes::cardano::api::Network::new(network).unwrap();
@@ -74,6 +80,7 @@ impl hermes::exports::hermes::init::event::Guest for TestComponent {
             format!("🎧 Network {network:?}, Subscribe to a block from {subscribe_from:?}, with subscription id: {subscription_id_resource:?}").as_str(),
             None,
         );
+
         true
     }
 }
