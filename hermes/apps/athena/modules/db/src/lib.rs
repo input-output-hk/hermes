861e1b0021220667303f07ea8380ee930ae684f8
rafal-ch <rafal.chabowski@iohk.io>
2025-08-12 12:17:24 +0200

    Clean-up DB module imports

--- hermes/apps/athena/modules/db/src/lib.rs
+++ hermes/apps/athena/modules/db/src/lib.rs
@@ -1,32 +1,31 @@
 //! # DB Component.
 
 #[allow(clippy::all, unused)]
-mod hermes;
-mod stub;
+mod db;
 
 /// Simple HTTP proxy component for demonstration purposes.
 struct DbComponent;
 
 fn log(
-    level: hermes::hermes::logging::api::Level,
+    level: db::hermes::logging::api::Level,
     message: &str,
 ) {
     let message = format!("DB Component: {}", message);
-    hermes::hermes::logging::api::log(level, None, None, None, None, None, &message, None);
+    db::hermes::logging::api::log(level, None, None, None, None, None, &message, None);
 }
 
-impl hermes::exports::hermes::init::event::Guest for DbComponent {
+impl db::exports::hermes::init::event::Guest for DbComponent {
     fn init() -> bool {
         log(
-            hermes::hermes::logging::api::Level::Info,
+            db::hermes::logging::api::Level::Info,
             "Initializing DB component...",
         );
 
-        let sqlite = match hermes::hermes::sqlite::api::open(false, false) {
+        let sqlite = match db::hermes::sqlite::api::open(false, false) {
             Ok(sqlite) => sqlite,
             Err(e) => {
                 log(
-                    hermes::hermes::logging::api::Level::Error,
+                    db::hermes::logging::api::Level::Error,
                     &format!("Failed to open database: {}", e),
                 );
                 return false;
@@ -34,7 +33,7 @@ impl hermes::exports::hermes::init::event::Guest for DbComponent {
         };
 
         log(
-            hermes::hermes::logging::api::Level::Info,
+            db::hermes::logging::api::Level::Info,
             "DB opened or created, creating table...",
         );
         let create_table_sql = r"
@@ -45,26 +44,26 @@ impl hermes::exports::hermes::init::event::Guest for DbComponent {
             );
         ";
         if let Err(e) = sqlite.execute(create_table_sql) {
-            log(hermes::hermes::logging::api::Level::Error, e.to_string().as_str());
+            log(db::hermes::logging::api::Level::Error, e.to_string().as_str());
         }
 
         log(
-            hermes::hermes::logging::api::Level::Info,
+            db::hermes::logging::api::Level::Info,
             "Putting data into table...",
         );
         let insert_sql = r#"
             INSERT INTO people (name, age) VALUES ('Athena', 1);
         "#;
         if let Err(e) = sqlite.execute(insert_sql) {
-            log(hermes::hermes::logging::api::Level::Error, e.to_string().as_str());
+            log(db::hermes::logging::api::Level::Error, e.to_string().as_str());
         }
 
         log(
-            hermes::hermes::logging::api::Level::Info,
+            db::hermes::logging::api::Level::Info,
             "Done and dusted!",
         );
         true
     }
 }
 
-hermes::export!(DbComponent with_types_in hermes);
+db::export!(DbComponent with_types_in db);
