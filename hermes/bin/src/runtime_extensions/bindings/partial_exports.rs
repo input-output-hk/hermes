c6426468c38299fa4df93337d2cb1345708dd321
no30bit <artur.helmanau@innowise.com>
2025-09-01 14:45:01 +0300

    qualify paths and run examples

--- hermes/bin/src/runtime_extensions/bindings/partial_exports.rs
+++ hermes/bin/src/runtime_extensions/bindings/partial_exports.rs
@@ -10,7 +10,10 @@ use crate::{
 };
 
 use super::exports::hermes::integration_test::event::TestResult;
-use wasmtime::component::{self, ComponentNamedList, Resource, TypedFunc};
+use wasmtime::{
+    component::{self, ComponentNamedList, Resource, TypedFunc},
+    AsContextMut,
+};
 
 pub type OnCardanoBlock = TypedFunc<(Resource<SubscriptionId>, Resource<Block>), ()>;
 
@@ -103,13 +106,20 @@ pub trait ComponentInstanceExt {
 fn get_typed_func<Params, Return>(
     instance: &component::Instance,
     store: &mut wasmtime::Store<HermesRuntimeContext>,
-    wit_name: &str,
+    wit_interface_name: &str,
+    wit_func_name: &str,
 ) -> Result<TypedFunc<Params, Return>, Error>
 where
     Params: ComponentNamedList + component::Lower,
     Return: ComponentNamedList + component::Lift,
 {
-    let Some(untyped) = instance.get_func(&mut *store, wit_name) else {
+    let Some(untyped) = instance
+        .get_export_index(store.as_context_mut(), None, wit_interface_name)
+        .and_then(|interface_idx| {
+            instance.get_export_index(store.as_context_mut(), Some(&interface_idx), wit_func_name)
+        })
+        .and_then(|func_idx| instance.get_func(store.as_context_mut(), func_idx))
+    else {
         return Err(Error::NotExported);
     };
     untyped.typed(store).map_err(|_| Error::InvalidSignature)
@@ -120,69 +130,79 @@ impl ComponentInstanceExt for &component::Instance {
         self,
         store: &mut wasmtime::Store<HermesRuntimeContext>,
     ) -> Result<OnCardanoBlock, Error> {
-        get_typed_func(self, store, "TODO")
+        get_typed_func(
+            self,
+            store,
+            "hermes:cardano/event-on-block",
+            "on-cardano-block",
+        )
     }
 
     fn hermes_cardano_event_on_immutable_roll_forward_on_cardano_immutable_roll_forward(
         self,
         store: &mut wasmtime::Store<HermesRuntimeContext>,
     ) -> Result<OnCardanoImmutableRollForward, Error> {
-        get_typed_func(self, store, "TODO")
+        get_typed_func(
+            self,
+            store,
+            "hermes:cardano/event-on-immutable-roll-forward",
+            "on-cardano-immutable-roll-forward",
+        )
     }
 
     fn hermes_cron_event_on_cron<'p>(
         self,
         store: &mut wasmtime::Store<HermesRuntimeContext>,
     ) -> Result<OnCron<'p>, Error> {
-        get_typed_func(self, store, "TODO")
+        get_typed_func(self, store, "hermes:cron/event", "on-cron")
     }
 
     fn hermes_init_event_init(
         self,
         store: &mut wasmtime::Store<HermesRuntimeContext>,
     ) -> Result<Init, Error> {
-        get_typed_func(self, store, "TODO")
+        get_typed_func(self, store, "hermes:init/event", "init")
     }
 
     fn hermes_ipfs_event_on_topic<'p>(
         self,
         store: &mut wasmtime::Store<HermesRuntimeContext>,
     ) -> Result<OnTopic<'p>, Error> {
-        get_typed_func(self, store, "TODO")
+        get_typed_func(self, store, "hermes:ipfs/event", "on-topic")
     }
 
     fn hermes_kv_store_event_kv_update<'p>(
         self,
         store: &mut wasmtime::Store<HermesRuntimeContext>,
     ) -> Result<KvUpdate<'p>, Error> {
-        get_typed_func(self, store, "TODO")
+        get_typed_func(self, store, "hermes:kv-store/event", "kv-update")
     }
 
     fn hermes_integration_test_event_test(
         self,
         store: &mut wasmtime::Store<HermesRuntimeContext>,
     ) -> Result<Test, Error> {
-        get_typed_func(self, store, "TODO")
+        get_typed_func(self, store, "hermes:integration-test/event", "test")
     }
 
     fn hermes_integration_test_event_bench(
         self,
         store: &mut wasmtime::Store<HermesRuntimeContext>,
     ) -> Result<Bench, Error> {
-        get_typed_func(self, store, "TODO")
+        get_typed_func(self, store, "hermes:integration-test/event", "bench")
     }
 
     fn hermes_http_gateway_event_reply<'p>(
         self,
         store: &mut wasmtime::Store<HermesRuntimeContext>,
     ) -> Result<Reply<'p>, Error> {
-        get_typed_func(self, store, "TODO")
+        get_typed_func(self, store, "hermes:http-gateway/event", "reply")
     }
 
     fn hermes_http_request_event_on_http_response<'p>(
         self,
         store: &mut wasmtime::Store<HermesRuntimeContext>,
     ) -> Result<OnHttpResponse<'p>, Error> {
-        get_typed_func(self, store, "TODO")
+        get_typed_func(self, store, "hermes:http-request/event", "on-http-response")
     }
 }
