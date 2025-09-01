15963c885b1f39a7e269351191052fd7316ae3da
bkioshn <bkioshn@gmail.com>
2025-09-01 20:13:16 +0700

    fix: refactor insert binding
    Signed-off-by: bkioshn <bkioshn@gmail.com>

--- hermes/apps/athena/modules/rbac-registration/src/database/insert.rs
+++ hermes/apps/athena/modules/rbac-registration/src/database/insert.rs
@@ -2,7 +2,7 @@ use serde_json::json;
 
 use crate::{
     database::{
-        data::{RbacDbData, RbacStakeDbData},
+        data::{rbac_db::RbacDbData, rbac_stake_db::RbacStakeDbData},
         SQLITE,
     },
     hermes::{
@@ -28,7 +28,7 @@ pub(crate) const RBAC_INSERT_STAKE_ADDRESS: &str = r#"
     VALUES(?, ?, ?, ?);
 "#;
 
-pub(crate) fn prepare_insert_rbac_registration() -> anyhow::Result<Statement> {
+pub(crate) fn prepare_insert_rbac_registration() -> Option<Statement> {
     const FUNCTION_NAME: &str = "prepare_insert_rbac_registration";
     log_info(
         FILE_NAME,
@@ -37,16 +37,18 @@ pub(crate) fn prepare_insert_rbac_registration() -> anyhow::Result<Statement> {
         &format!("Prepare insert 🍊"),
         None,
     );
-    SQLITE.prepare(RBAC_INSERT_RBAC_REGISTRATION).map_err(|e| {
-        log_error(
-            FILE_NAME,
-            FUNCTION_NAME,
-            "hermes::sqlite::api::prepare",
-            &format!("🚨 Failed to prepare insert statement: {e}"),
-            None,
-        );
-        anyhow::anyhow!(e)
-    })
+    SQLITE
+        .prepare(RBAC_INSERT_RBAC_REGISTRATION)
+        .map_err(|e| {
+            log_error(
+                FILE_NAME,
+                FUNCTION_NAME,
+                "hermes::sqlite::api::prepare",
+                &format!("🚨 Failed to prepare insert statement: {e}"),
+                None,
+            );
+        })
+        .ok()
 }
 
 pub(crate) fn insert_rbac_registration(
@@ -54,7 +56,7 @@ pub(crate) fn insert_rbac_registration(
     data: RbacDbData,
 ) {
     const FUNCTION_NAME: &str = "insert_rbac_registration";
-    log_info(FILE_NAME, FUNCTION_NAME, "", &format!("Insert 🍊"), None);
+    log_error(FILE_NAME, FUNCTION_NAME, "", &format!("Insert 🍊"), None);
 
     bind_rbac_registration(stmt, data);
     if let Err(e) = stmt.step() {
@@ -83,88 +85,49 @@ fn bind_rbac_registration(
 ) {
     const FUNCTION_NAME: &str = "bind_rbac_registration";
 
-    bind_with_log(stmt, FUNCTION_NAME, 1, &Value::Blob(data.txn_id), "txn_id");
+    bind_with_log(stmt, FUNCTION_NAME, 1, &data.txn_id.into(), "txn_id");
     bind_with_log(
-        &stmt,
+        stmt,
         FUNCTION_NAME,
         2,
-        &data
-            .catalyst_id
-            .as_ref()
-            .map(|id| Value::Text(id.clone()))
-            .unwrap_or(Value::Null),
+        &data.catalyst_id.into(),
         "catalyst_id",
     );
-    // FIXME - Properly handle conversion
+    bind_with_log(stmt, FUNCTION_NAME, 3, &data.slot.into(), "slot");
+    bind_with_log(stmt, FUNCTION_NAME, 4, &data.txn_idx.into(), "txn_idx");
     bind_with_log(
-        &stmt,
-        FUNCTION_NAME,
-        3,
-        &Value::Int64(data.slot as i64),
-        "slot",
-    );
-
-    bind_with_log(
-        &stmt,
-        FUNCTION_NAME,
-        4,
-        &Value::Int32(data.txn_idx as i32),
-        "txn_idx",
-    );
-
-    bind_with_log(
-        &stmt,
+        stmt,
         FUNCTION_NAME,
         5,
-        &data
-            .prv_txn_id
-            .as_ref()
-            .map(|prv_txn_id| Value::Blob(prv_txn_id.clone()))
-            .unwrap_or(Value::Null),
+        &data.prv_txn_id.into(),
         "prv_txn_id",
     );
-
+    bind_with_log(stmt, FUNCTION_NAME, 6, &data.purpose.into(), "purpose");
     bind_with_log(
-        &stmt,
-        FUNCTION_NAME,
-        6,
-        &data
-            .purpose
-            .as_ref()
-            .map(|p| Value::Text(p.clone()))
-            .unwrap_or(Value::Null),
-        "purpose",
-    );
-
-    bind_with_log(
-        &stmt,
+        stmt,
         FUNCTION_NAME,
         7,
-        &data
-            .problem_report
-            .as_ref()
-            .map(|p| Value::Text(p.clone()))
-            .unwrap_or(Value::Null),
+        &data.problem_report.into(),
         "problem_report",
     );
 }
 
 pub(crate) fn prepare_insert_rbac_stake_address() -> anyhow::Result<Statement> {
     const FUNCTION_NAME: &str = "prepare_insert_rbac_stake_address";
-    log_info(
-        FILE_NAME,
-        FUNCTION_NAME,
-        "",
-        &format!("Prepare insert 🍊"),
-        None,
-    );
+    // log_info(
+    //     FILE_NAME,
+    //     FUNCTION_NAME,
+    //     "",
+    //     &format!("Prepare insert 🍊"),
+    //     None,
+    // );
 
     SQLITE.prepare(RBAC_INSERT_STAKE_ADDRESS).map_err(|e| {
         log_error(
             FILE_NAME,
             FUNCTION_NAME,
             "hermes::sqlite::api::prepare",
-            &format!("🚨 Failed to prepare insert statement: {e}"),
+            &format!("🚨 Failed to prepare insert: {e}"),
             None,
         );
         anyhow::anyhow!(e)
@@ -176,7 +139,7 @@ pub(crate) fn insert_rbac_stake_address(
     data: RbacStakeDbData,
 ) {
     const FUNCTION_NAME: &str = "insert_rbac_stake_address";
-    log_info(FILE_NAME, FUNCTION_NAME, "", &format!("Insert 🍊"), None);
+    // log_info(FILE_NAME, FUNCTION_NAME, "", &format!("Insert 🍊"), None);
 
     bind_rbac_stake_address(stmt, data);
     if let Err(e) = stmt.step() {
@@ -208,37 +171,21 @@ fn bind_rbac_stake_address(
         stmt,
         FUNCTION_NAME,
         1,
-        &Value::Blob(data.stake_address),
+        &data.stake_address.into(),
         "stake_address",
     );
-    // FIXME - Properly handle conversion
+    bind_with_log(stmt, FUNCTION_NAME, 2, &data.slot.into(), "slot");
+    bind_with_log(stmt, FUNCTION_NAME, 3, &data.txn_idx.into(), "txn_idx");
     bind_with_log(
-        &stmt,
-        FUNCTION_NAME,
-        2,
-        &Value::Int64(data.slot as i64),
-        "slot",
-    );
-    bind_with_log(
-        &stmt,
-        FUNCTION_NAME,
-        3,
-        &Value::Int32(data.txn_idx as i32),
-        "txn_idx",
-    );
-    bind_with_log(
-        &stmt,
+        stmt,
         FUNCTION_NAME,
         4,
-        &data
-            .catalyst_id
-            .as_ref()
-            .map(|id| Value::Text(id.clone()))
-            .unwrap_or(Value::Null),
+        &data.catalyst_id.into(),
         "catalyst_id",
     );
 }
 
+// --------------- Binding helper -------------------
 fn bind_with_log(
     stmt: &Statement,
     func_name: &str,
