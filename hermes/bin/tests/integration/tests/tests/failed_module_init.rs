78ed6e60616b2569916c6b70769fbedd20f4bbf1
rafal-ch <rafal.chabowski@iohk.io>
2025-09-03 10:36:42 +0200

    Add integration test for failed module initialization

---
+++ hermes/bin/tests/integration/tests/tests/failed_module_init.rs
@@ -0,0 +1,36 @@
+use temp_dir::TempDir;
+
+use crate::utils;
+
+#[test]
+fn do_not_load_app_when_module_initialization_fails() {
+    const COMPONENT: &str = "failed_init";
+
+    let temp_dir = TempDir::new().unwrap();
+
+    utils::component::build(COMPONENT, &temp_dir).expect("failed to build component");
+    let app_file_name = utils::packaging::package(&temp_dir).expect("failed to package app");
+
+    // TODO[RC]: Build hermes just once for all tests
+    utils::hermes::build();
+
+    // TODO[RC]: Currently, when a module fails to initialize, the whole app fails to run.
+    // In future, hermes will not bail on app initialization.
+    utils::hermes::run_app(&temp_dir, &app_file_name).expect_err("should fail to run hermes app");
+
+    assert!(utils::assert::app_logs_contain(
+        &temp_dir,
+        "module explicitly failing to initialize"
+    ));
+
+    assert!(utils::assert::app_logs_contain(
+        &temp_dir,
+        "WASM module init function returned false"
+    ));
+   
+
+    // Uncomment the line below if you want to inspect the details
+    // available in the temp directory.
+    //
+    // utils::debug_sleep(&temp_dir);
+}
--- hermes/bin/tests/integration/tests/tests/mod.rs
+++ hermes/bin/tests/integration/tests/tests/mod.rs
@@ -1,1 +1,2 @@
 mod http_request_rte;
+mod failed_module_init;
