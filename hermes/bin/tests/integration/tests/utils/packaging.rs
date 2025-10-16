45d8588ca0f658d30ce10fdb26f649c8653d44d7
Uladzislau Borbut <vladislav.borbut@iohk.io>
2025-10-16 12:37:21 +0200

    chore(hermes): remove merge changes from PR

--- hermes/bin/tests/integration/tests/utils/packaging.rs
+++ hermes/bin/tests/integration/tests/utils/packaging.rs
@@ -1,4 +1,3 @@
-use serde::{Deserialize, Serialize};
 use std::process::Command;
 
 use serde::{Deserialize, Serialize};
@@ -56,9 +55,11 @@ fn replace_app_manifest_with_modules(
     let mut app_manifest: AppManifest = serde_json::from_str(&manifest_content)?;
     let new_modules: Vec<ModuleEntry> = modules
         .iter()
-        .map(|module| ModuleEntry {
-            package: format!("{module}.hmod"),
-            name: module.to_string(),
+        .map(|module| {
+            ModuleEntry {
+                package: format!("{module}.hmod"),
+                name: module.to_string(),
+            }
         })
         .collect();
     app_manifest.modules = new_modules;
