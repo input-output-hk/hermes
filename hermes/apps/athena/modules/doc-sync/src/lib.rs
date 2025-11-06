dd05824bf14e79bbfc824279408636a3e65396f4
no30bit <artur.helmanau@innowise.com>
2025-11-06 14:45:53 +0300

    athena doc sync module stub

--- hermes/apps/athena/modules/doc-sync/src/lib.rs
+++ hermes/apps/athena/modules/doc-sync/src/lib.rs
@@ -1,3 +1,4 @@
+#![allow(missing_docs)]
 //! Doc Sync Module
 
 shared::bindings_generate!({
@@ -10,20 +11,20 @@ shared::bindings_generate!({
             include wasi:cli/imports@0.2.6;
             import hermes:doc-sync/api;
             import hermes:logging/api;
-            import hermes:init/api;
 
             export hermes:init/event;
             export hermes:doc-sync/event;
         }
     ",
-    share: ["hermes:logging"],
+    share: ["hermes:logging", "hermes:doc-sync"],
 });
 
 export!(Component);
 
-use shared::utils::log::{self, warn};
-
-use crate::exports::hermes::doc_sync::event::{ChannelName, DocData};
+use shared::{
+    bindings::hermes::doc_sync::api::{ChannelName, DocData, SyncChannel},
+    utils::log::{self, info, warn},
+};
 
 /// Doc Sync component.
 struct Component;
@@ -31,7 +32,9 @@ struct Component;
 impl exports::hermes::init::event::Guest for Component {
     fn init() -> bool {
         log::init(log::LevelFilter::Trace);
-        warn!(target: "doc_sync::init", "Unimplemented");
+        info!(target: "doc_sync::init", "💫 Opening channel...");
+        let _chan = SyncChannel::new("documents");
+        info!(target: "doc_sync::init", "💫 Channel opened");
         true
     }
 }
@@ -42,6 +45,6 @@ impl exports::hermes::doc_sync::event::Guest for Component {
         doc: DocData,
     ) {
         log::init(log::LevelFilter::Trace);
-        warn!(target: "doc_sync::on_new_doc", channel:%, doc_byte_length = doc.len(); "Unimplemented");
+        warn!(target: "doc_sync::on_new_doc", channel:%, doc_byte_length = doc.len(); "Unimplemented!");
     }
 }
