dee73bcf26c0ac4c8cd84bc0fd18b9e29dc18af3
Vladislav Borbut <vladislav.borbut@iohk.io>
2025-11-27 11:49:00 +0100

    feat(hermes): add ipfs connection opening to doc_sync (#685)
    * feat(hermes): add ipfs connection opening to doc_sync
    
    * chore(hermes): rafactoring
    
    * feat(hermes): add doc-sync opening test
    
    * fix(CI): fix spelling
    
    * fix(hermes): fix test
    
    * fix(CI): ignore some lints
    
    * chore(hermes): update lock file
    
    * fix(hermes): change document id to cbor encoded CID
    
    * chore(hermes): remove unused imports
    
    * fix(hermes): forbid usage of sync channel resource after close
    
    * chore(hermes): fix lints
    
    * chore(hermes): update code so that it uses 4 byte hash
    
    * chore(hermes): add comment

--- hermes/bin/src/runtime_extensions/hermes/doc_sync/host.rs
+++ hermes/bin/src/runtime_extensions/hermes/doc_sync/host.rs
@@ -1,22 +1,79 @@
 //! Doc Sync host module.
 
+use cardano_chain_follower::pallas_codec::minicbor::{self, Encode, Encoder, data::Tag};
+use stringzilla::stringzilla::Sha256;
 use wasmtime::component::Resource;
 
 use crate::{
+    ipfs::hermes_ipfs_subscribe,
     runtime_context::HermesRuntimeContext,
-    runtime_extensions::bindings::hermes::doc_sync::api::{
-        ChannelName, DocData, DocLoc, DocProof, Errno, Host, HostSyncChannel, ProverId, SyncChannel,
+    runtime_extensions::{
+        bindings::hermes::doc_sync::api::{
+            ChannelName, DocData, DocLoc, DocProof, Errno, Host, HostSyncChannel, ProverId,
+            SyncChannel,
+        },
+        hermes::doc_sync::DOC_SYNC_STATE,
     },
 };
+
+/// CBOR multicodec identifier.
+///
+/// See: <https://github.com/multiformats/multicodec/blob/master/table.csv>
+const CBOR_CODEC: u64 = 0x51;
+
+/// SHA2-256 multihash code.
+const SHA2_256_CODE: u64 = 0x12;
+
+/// CBOR tag for IPLD CID (Content Identifier).
+///
+/// See: <https://github.com/ipld/cid-cbor/>
+const CID_CBOR_TAG: u64 = 42;
+
+/// Wrapper for `hermes_ipfs::Cid` to implement `minicbor::Encode` for it.
+struct Cid(hermes_ipfs::Cid);
+
+impl minicbor::Encode<()> for Cid {
+    fn encode<W: minicbor::encode::Write>(
+        &self,
+        e: &mut Encoder<W>,
+        _ctx: &mut (),
+    ) -> Result<(), minicbor::encode::Error<W::Error>> {
+        // Encode as tag(42) containing the CID bytes
+        e.tag(Tag::new(CID_CBOR_TAG))?;
+        e.bytes(&self.0.to_bytes())?;
+        Ok(())
+    }
+}
 
 #[allow(clippy::todo)]
 impl Host for HermesRuntimeContext {
     /// Get the Document ID for the given Binary Document
+    ///
+    /// See: <https://docs.dev.projectcatalyst.io/hermes/main/architecture/08_concepts/document_sync/protocol_spec/#cidv1-binary-encoding-poc-focus>
+    ///
+    /// # Note
+    ///
+    /// We expect to receive doc as cbor bytes.
     fn id_for(
         &mut self,
-        _doc: DocData,
+        doc: DocData,
     ) -> wasmtime::Result<Vec<u8>> {
-        todo!()
+        // Compute SHA2-256 hash
+        let mut hasher = Sha256::new();
+        hasher.update(&doc);
+        let hash_digest = hasher.digest();
+
+        // Create multihash from digest using the wrap() API
+        // The generic parameter <64> is the max digest size we support
+        let multihash = multihash::Multihash::<64>::wrap(SHA2_256_CODE, &hash_digest)?;
+
+        // Create CID v1 with CBOR codec
+        let cid = hermes_ipfs::Cid::new_v1(CBOR_CODEC, multihash);
+
+        let mut e = minicbor::Encoder::new(Vec::new());
+        Cid(cid).encode(&mut e, &mut ())?;
+
+        Ok(e.into_writer())
     }
 }
 
@@ -35,9 +92,43 @@ impl HostSyncChannel for HermesRuntimeContext {
     /// - `error(create-network-error)`: If creating network resource failed.
     fn new(
         &mut self,
-        _name: ChannelName,
+        name: ChannelName,
     ) -> wasmtime::Result<Resource<SyncChannel>> {
-        todo!()
+        let hash = blake2b_simd::Params::new()
+            .hash_length(4)
+            .hash(name.as_bytes());
+
+        // The digest is a 64-byte array ([u8; 64]) for 512-bit output.
+        // Take the first 4 bytes to use them as resource id.
+        //
+        // Assumption:
+        // Number of channels is way more less then u32, so collisions are
+        // acceptable but unlikely in practice. We use the first 4 bytes of
+        // the cryptographically secure Blake2b hash as a fast, 32-bit ID
+        // to minimize lock contention when accessing state via DOC_SYNC_STATE.
+        let prefix_bytes: &[u8; 4] = hash.as_bytes().try_into().map_err(|err| {
+            wasmtime::Error::msg(format!("BLAKE2b hash output length must be 4 bytes: {err}"))
+        })?;
+
+        let resource: u32 = u32::from_be_bytes(*prefix_bytes);
+
+        // Code block is used to minimize locking scope.
+        {
+            let entry = DOC_SYNC_STATE.entry(resource).or_insert(name.clone());
+            if &name != entry.value() {
+                return Err(wasmtime::Error::msg(format!(
+                    "Collision occurred with previous value = {} and new one = {name}",
+                    entry.value()
+                )));
+            }
+        }
+
+        if let Err(err) = hermes_ipfs_subscribe(self.app_name(), name) {
+            DOC_SYNC_STATE.remove(&resource);
+            return Err(wasmtime::Error::msg(format!("Subscription failed: {err}",)));
+        }
+
+        Ok(wasmtime::component::Resource::new_own(resource))
     }
 
     /// Close Doc Sync Channel
@@ -46,7 +137,7 @@ impl HostSyncChannel for HermesRuntimeContext {
     /// (and all docs stored are released)
     /// Close itself should be deferred until all running WASM modules with an open
     /// `sync-channel` resource have terminated.
-    ///  
+    ///
     /// **Parameters**
     ///
     /// None
@@ -57,14 +148,13 @@ impl HostSyncChannel for HermesRuntimeContext {
     /// - `error(<something>)`: If it gets an error closing.
     fn close(
         &mut self,
-        _self_: Resource<SyncChannel>,
-        _name: ChannelName,
+        self_: Resource<SyncChannel>,
     ) -> wasmtime::Result<Result<bool, Errno>> {
-        todo!()
+        inner_close(self, self_)
     }
 
     /// Post the document to a channel
-    ///  
+    ///
     /// **Parameters**
     ///
     /// None
@@ -82,7 +172,7 @@ impl HostSyncChannel for HermesRuntimeContext {
     }
 
     /// Prove a document is stored in the provers
-    ///  
+    ///
     /// **Parameters**
     ///
     /// loc : Location ID of the document to prove storage of.
@@ -104,7 +194,7 @@ impl HostSyncChannel for HermesRuntimeContext {
     }
 
     /// Disprove a document is stored in the provers
-    ///  
+    ///
     /// **Parameters**
     ///
     /// loc : Location ID of the document to prove storage of.
@@ -126,7 +216,7 @@ impl HostSyncChannel for HermesRuntimeContext {
     }
 
     /// Prove a document is stored in the provers
-    ///  
+    ///
     /// **Parameters**
     ///
     /// None
@@ -146,8 +236,22 @@ impl HostSyncChannel for HermesRuntimeContext {
     /// Wasmtime resource drop callback.
     fn drop(
         &mut self,
-        _rep: Resource<SyncChannel>,
+        res: Resource<SyncChannel>,
     ) -> wasmtime::Result<()> {
-        todo!()
+        inner_close(self, res)??;
+
+        Ok(())
     }
+}
+
+/// This function is required cause reusage of `self.close`
+/// inside drop causes invalid behavior during codegen.
+#[allow(clippy::unnecessary_wraps)]
+fn inner_close(
+    _ctx: &mut HermesRuntimeContext,
+    _res: Resource<SyncChannel>,
+) -> wasmtime::Result<Result<bool, Errno>> {
+    // TODO(anyone): Here we should clean up the state, since we would have a map that
+    // associates app_name with app's subscriptions.
+    Ok(Ok(true))
 }
