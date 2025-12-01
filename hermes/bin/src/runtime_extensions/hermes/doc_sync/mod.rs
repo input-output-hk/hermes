f4f27fc3f658f467c8e02ecfc97e78b9eff6b354
no30bit <artur.helmanau@innowise.com>
2025-12-01 09:58:37 +0300

    update name mappings

--- hermes/bin/src/runtime_extensions/hermes/doc_sync/mod.rs
+++ hermes/bin/src/runtime_extensions/hermes/doc_sync/mod.rs
@@ -18,8 +18,8 @@ pub(crate) use event::OnNewDocEvent;
 /// - Topic is not valid doc sync channel.
 fn map_ipfs_topic_to_channel_name(topic: &'_ str) -> anyhow::Result<Cow<'_, str>> {
     match topic {
-        "" => Err(anyhow!("Not a Doc Sync IPFS topic")),
-        _ => Ok("documents".into()),
+        "doc-sync/documents" => Ok("documents".into()),
+        _ => Err(anyhow!("Not a Doc Sync IPFS topic")),
     }
 }
 
@@ -30,7 +30,7 @@ fn map_ipfs_topic_to_channel_name(topic: &'_ str) -> anyhow::Result<Cow<'_, str>
 /// - Topic is not valid doc sync channel.
 fn map_channel_name_to_ipfs_topic(channel_name: &'_ str) -> anyhow::Result<Cow<'_, str>> {
     match channel_name {
-        "documents" => Ok("documents".into()),
+        "documents" => Ok("doc-sync/documents".into()),
         _ => Err(anyhow!("Not a Doc Sync channel")),
     }
 }
