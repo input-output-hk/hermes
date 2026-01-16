7d6532238ddca4cbd03c1830f2b2264279ab0e8f
Uladzislau Borbut <vladislav.borbut@iohk.io>
2026-01-16 14:10:49 +0100

    chore(hermes): fix lints

--- hermes/bin/src/cli/run.rs
+++ hermes/bin/src/cli/run.rs
@@ -109,7 +109,7 @@ impl Run {
         // TODO[RC]: Currently, when a module fails to initialize, the whole app fails to run.
         reactor::load_app(app)?;
 
-        let _ = reactor::initialize_smt(app_name);
+        reactor::initialize_smt(app_name)?;
 
         let exit = if let Some(timeout_ms) = self.rt_config.timeout_ms {
             exit_lock.wait_timeout(Duration::from_millis(timeout_ms))
